0ac6c142525d285f47a6e3d5f5b8c4dd0db1df27
Veeral Patel <vpatel98@gmail.com>
2019-08-17 15:01:53 -0700

    move compareUsers method into a new file

--- frontend/src/presentational/cases/CasesTableP.tsx
+++ frontend/src/presentational/cases/CasesTableP.tsx
@@ -4,7 +4,7 @@ import ICase from "../../ts/interfaces/ICase";
 import PriorityTagP from "../shared/tags/PriorityTagP";
 import StatusTagP from "../shared/tags/StatusTagP";
 import ListOfTagsP from "../shared/tags/ListOfTagsP";
-import IUser from "../../ts/interfaces/IUser";
+import compareUsers from "../../utils/compareUsers";
 
 const { Column } = Table;
 
@@ -16,21 +16,6 @@ interface ICasesTableProps {
 const rowSelection = {
   onChange: (selectedRowKeys: string[] | number[], selectedRows: ICase[]) => {}
 };
-
-function compareUsers(u1: IUser | null, u2: IUser | null): number {
-  // sort unassigned cases before assigned cases
-  if (!u1 && u2) return -1;
-  else if (u1 && !u2) return 1;
-  // if two cases are unassigned, sort them equally
-  else if (!u1 && !u2) return 0;
-  // if both cases are assigned
-  else {
-    // then sort based on the assigned user's username
-    if (u1 && u2) return u1.username.localeCompare(u2.username);
-    // this should never happen
-    else return 0;
-  }
-}
 
 const CasesTableP: React.FC<ICasesTableProps> = props => {
   const { dataSource } = props;
