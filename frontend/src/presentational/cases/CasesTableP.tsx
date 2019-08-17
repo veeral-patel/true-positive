8741cf4daae68c5cce2befa10a16861a96cc8f20
Veeral Patel <vpatel98@gmail.com>
2019-08-16 22:57:11 -0700

    add row selection

--- frontend/src/presentational/cases/CasesTableP.tsx
+++ frontend/src/presentational/cases/CasesTableP.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Table, Tag } from "antd";
+import { Table, Tag, Row } from "antd";
 import ICase from "../../ts/interfaces/ICase";
 import PriorityTagP from "../shared/tags/PriorityTagP";
 import StatusTagP from "../shared/tags/StatusTagP";
+import { TableRowSelection } from "antd/lib/table";
 
 const { Column } = Table;
 
@@ -10,49 +11,55 @@ interface ICasesTableProps {
   dataSource: ICase[]; // list of case objects
 }
 
+const rowSelection = {
+  onChange: (selectedRowKeys: string[] | number[], selectedRows: ICase[]) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  }
+};
+
 const CasesTableP: React.FC<ICasesTableProps> = props => {
   const { dataSource } = props;
   return (
-    <div>
-      <Table dataSource={dataSource}>
-        <Column title="Name" dataIndex="name" key="name" />
-        <Column
-          title="Status"
-          dataIndex="status.name"
-          key="status"
-          render={(statusName: string) => (
-            <StatusTagP statusName={statusName} />
-          )}
-        />
-        <Column
-          title="Priority"
-          dataIndex="priority.name"
-          key="priority"
-          render={(priorityName: string) => (
-            <PriorityTagP priorityName={priorityName} />
-          )}
-        />
-        <Column
-          title="Assigned To"
-          dataIndex="assignedTo.username"
-          key="assigned_to"
-        />
-        <Column
-          title="Tags"
-          dataIndex="tags"
-          key="tags"
-          render={tags => (
-            <span>
-              {tags.map((tag: string) => (
-                <Tag color="blue" key={tag}>
-                  {tag}
-                </Tag>
-              ))}
-            </span>
-          )}
-        />
-      </Table>
-    </div>
+    <Table dataSource={dataSource} rowSelection={rowSelection}>
+      <Column title="Name" dataIndex="name" key="name" />
+      <Column
+        title="Status"
+        dataIndex="status.name"
+        key="status"
+        render={(statusName: string) => <StatusTagP statusName={statusName} />}
+      />
+      <Column
+        title="Priority"
+        dataIndex="priority.name"
+        key="priority"
+        render={(priorityName: string) => (
+          <PriorityTagP priorityName={priorityName} />
+        )}
+      />
+      <Column
+        title="Assigned To"
+        dataIndex="assignedTo.username"
+        key="assigned_to"
+      />
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        render={tags => (
+          <span>
+            {tags.map((tag: string) => (
+              <Tag color="blue" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </span>
+        )}
+      />
+    </Table>
   );
 };
 
