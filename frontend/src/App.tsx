6d183fd5309f8b5d13bd1be24d9d573de9e43971
Veeral Patel <vpatel98@gmail.com>
2019-08-17 11:10:33 -0700

    add reach router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AllCasesPage from "./pages/cases/AllCasesPage";
+import { Router } from "@reach/router";
 
 const App: React.FC = () => {
-  return <AllCasesPage />;
+  return (
+    <Router>
+      <AllCasesPage path="/" />
+      <AllCasesPage path="/cases" />
+    </Router>
+  );
 };
 
 export default App;
--- frontend/src/pages/cases/AllCasesPage.tsx
+++ frontend/src/pages/cases/AllCasesPage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import CasesTable from "../../container/cases/CasesTable";
+import { RouteComponentProps } from "@reach/router";
 
-const AllCasesPage: React.FC = () => (
+const AllCasesPage: React.FC<RouteComponentProps> = () => (
   <div style={{ margin: "3%" }}>
     <CasesTable />
   </div>
