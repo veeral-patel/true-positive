b2d0e00607bf29b96404a3a7d96532e397ed22af
Veeral Patel <vpatel98@gmail.com>
2019-08-16 18:20:43 -0700

    remove some boilerplate code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,8 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hello World</h2>
     </div>
   );
 };
