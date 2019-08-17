4f37d612bbff4769cd53f9f094b116d98c2fe6eb
Veeral Patel <vpatel98@gmail.com>
2019-08-17 12:38:13 -0700

    add new case button

--- frontend/src/pages/cases/AllCasesPage.tsx
+++ frontend/src/pages/cases/AllCasesPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import CasesTable from "../../container/cases/CasesTable";
 import { RouteComponentProps } from "@reach/router";
+import { PageHeader, Button } from "antd";
 
 const AllCasesPage: React.FC<RouteComponentProps> = () => (
   <div>
-    <h2>Cases</h2>
-    <br />
+    <PageHeader
+      title={<h2>Cases</h2>}
+      backIcon={false}
+      style={{ paddingLeft: 0 }}
+      extra={[<Button type="primary">Create Case</Button>]}
+    />
     <CasesTable />
   </div>
 );
