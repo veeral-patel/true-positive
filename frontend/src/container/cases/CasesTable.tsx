7a538af999cb6bed07e808571a793e85a7e1fcbb
Veeral Patel <vpatel98@gmail.com>
2019-08-16 20:27:04 -0700

    load data from API

--- frontend/src/container/cases/CasesTable.tsx
+++ frontend/src/container/cases/CasesTable.tsx
@@ -3,21 +3,6 @@ import CasesTableP from "../../presentational/cases/CasesTableP";
 import { useQuery } from "react-apollo-hooks";
 import gql from "graphql-tag";
 
-const dataSource = [
-  {
-    key: "1",
-    name: "Mike",
-    age: 32,
-    address: "10 Downing Street"
-  },
-  {
-    key: "2",
-    name: "John",
-    age: 42,
-    address: "10 Downing Street"
-  }
-];
-
 const GET_CASES = gql`
   query {
     cases {
@@ -29,9 +14,14 @@ const GET_CASES = gql`
 
 const CasesTable: React.SFC = () => {
   const { data, error, loading } = useQuery(GET_CASES);
-  console.log(data);
 
-  return <CasesTableP dataSource={dataSource} />;
+  if (error) {
+    alert("Error!");
+  } else if (loading) {
+    return <h3>Loading...</h3>;
+  }
+
+  return <CasesTableP dataSource={data.cases} />;
 };
 
 export default CasesTable;
--- frontend/src/presentational/cases/CasesTableP.tsx
+++ frontend/src/presentational/cases/CasesTableP.tsx
@@ -12,16 +12,6 @@ const columns: ColumnProps<ICase>[] = [
     title: "Name",
     dataIndex: "name",
     key: "name"
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age"
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address"
   }
 ];
 
