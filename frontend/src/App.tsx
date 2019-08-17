aa7e9013e79a2b8eab4c04832a01b3002a0e202b
Veeral Patel <vpatel98@gmail.com>
2019-08-17 10:50:20 -0700

    create a pages/ directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import CasesTable from "./container/cases/CasesTable";
+import AllCasesPage from "./pages/AllCasesPage";
 
 const App: React.FC = () => {
-  return (
-    <div style={{ margin: "3%" }}>
-      <CasesTable />
-    </div>
-  );
+  return <AllCasesPage />;
 };
 
 export default App;
---
+++ frontend/src/pages/AllCasesPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CasesTable from "../container/cases/CasesTable";
+
+const AllCasesPage: React.FC = () => (
+  <div style={{ margin: "3%" }}>
+    <CasesTable />
+  </div>
+);
+
+export default AllCasesPage;
