b1fe971d8c7c2a46a6b66c031aa8a3693f14050c
Veeral Patel <vpatel98@gmail.com>
2019-08-16 19:18:03 -0700

    work on table component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import CasesTable from "./presentational/cases/CasesTable";
+
+const dataSource = [
+  {
+    key: "1",
+    name: "Mike",
+    age: 32,
+    address: "10 Downing Street"
+  },
+  {
+    key: "2",
+    name: "John",
+    age: 42,
+    address: "10 Downing Street"
+  }
+];
 
 const App: React.FC = () => {
   return (
     <div>
-      <h2>Hello World</h2>
+      <CasesTable dataSource={dataSource} />
     </div>
   );
 };
---
+++ frontend/src/presentational/cases/CasesTable.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Table } from "antd";
+
+interface ICasesTableProps {
+  dataSource: any;
+}
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age"
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address"
+  }
+];
+
+const CasesTable: React.FC<ICasesTableProps> = props => {
+  const { dataSource } = props;
+  return (
+    <div>
+      <Table columns={columns} dataSource={dataSource} />
+    </div>
+  );
+};
+
+export default CasesTable;
