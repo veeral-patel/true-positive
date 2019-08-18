09486149b830b7d14998501d7bd3f718135ab645
Veeral Patel <vpatel98@gmail.com>
2019-08-17 23:03:47 -0700

    convert created_at to an ISO datetime

--- backend/app/graphql/types/case_type.rb
+++ backend/app/graphql/types/case_type.rb
@@ -5,7 +5,7 @@ module Types
         # required
         field :id, ID, null: false
         field :name, String, null: false
-        field :created_at, String, null: false
+        field :created_at, GraphQL::Types::ISO8601DateTime, null: false
         field :created_by, Types::UserType, null: false
         field :status, Types::StatusType, null: false
         field :priority, Types::PriorityType, null: false
@@ -15,10 +15,5 @@ module Types
         field :assigned_to, Types::UserType, null: true
         field :tags, [String], null: false
         field :tasks, [Types::TaskType], null: false
-
-        def created_at
-            # use our `formatted_created_at` method in our case model
-            object.formatted_created_at
-        end
     end
 end
