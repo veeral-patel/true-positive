934d21ec1d3b4feabf8e3efa3bc1cc4944bf1572
Veeral Patel <vpatel98@gmail.com>
2019-08-18 09:38:29 -0700

    adjust margins again

--- frontend/src/presentational/cases/ExpandedRowP.tsx
+++ frontend/src/presentational/cases/ExpandedRowP.tsx
@@ -11,7 +11,7 @@ interface IExpandedRowProps {
 }
 
 const ExpandedRow: React.FC<IExpandedRowProps> = ({ description, tasks }) => (
-  <div style={{ marginTop: "60px", marginBottom: "60px", width: "75%" }}>
+  <div style={{ marginTop: "40px", marginBottom: "80px", width: "75%" }}>
     <Paragraph style={{ margin: "0px" }}>{description}</Paragraph>
     <br />
     <div>
