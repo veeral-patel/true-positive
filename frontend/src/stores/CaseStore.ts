8dca8be99f233f15f6450e1aa89a35f41f100ff3
Veeral Patel <vpatel98@gmail.com>
2019-08-18 22:12:38 -0700

    improve typing for CaseStore

--- frontend/src/stores/CaseStore.ts
+++ frontend/src/stores/CaseStore.ts
@@ -1,21 +1,29 @@
 import { notification } from "antd";
+import { ApolloQueryResult } from "apollo-boost";
 import client from "createApolloClient";
 import { action, observable } from "mobx";
 import GET_CASES from "queries/getCases";
 import ICase from "ts/interfaces/ICase";
 
+interface ICaseData {
+  cases: ICase[];
+}
+
 class CaseStore {
   @observable cases: ICase[] = [];
   @observable casesAreLoading: boolean = false;
 
-  @action
+  @action.bound
   loadCases() {
     this.casesAreLoading = true;
     client
-      .query({
+      .query<ICaseData>({
         query: GET_CASES
       })
-      .then(response => (this.cases = response.data.cases))
+      .then(
+        (response: ApolloQueryResult<ICaseData>) =>
+          (this.cases = response.data.cases)
+      )
       .catch(error =>
         this.showErrorNotification(
           "An error occurred while fetching cases",
