2af9e36ef3d3cd7a5d3f6974d73cba29e1e3ac88
Veeral Patel <vpatel98@gmail.com>
2019-08-18 21:16:26 -0700

    enable strict mode

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,6 +1,12 @@
+import { configure } from "mobx";
 import CaseStore from "store/CaseStore";
 
 const caseStore = new CaseStore();
+
+// only let me mutate state within actions
+configure({
+  enforceActions: true
+});
 
 export default {
   caseStore
