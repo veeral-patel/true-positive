85ef7defd47febd0a61b96318952c3193aa48965
Veeral Patel <vpatel98@gmail.com>
2019-08-18 22:23:13 -0700

    can load cases just like before!

--- frontend/src/container/cases/CasesTable.tsx
+++ frontend/src/container/cases/CasesTable.tsx
@@ -1,4 +1,5 @@
 import { inject, observer } from "mobx-react";
+import CasesTableP from "presentational/cases/CasesTableP";
 import React from "react";
 import CaseStore from "stores/CaseStore";
 
@@ -9,11 +10,18 @@ interface ICasesTableProps {
 export default inject("caseStore")(
   observer(
     class CasesTable extends React.Component<ICasesTableProps> {
-      render() {
+      componentDidMount() {
         if (this.props.caseStore) {
           this.props.caseStore.loadCases();
         }
-        return <h3>Loading cases now!</h3>;
+      }
+
+      render() {
+        if (this.props.caseStore) {
+          if (this.props.caseStore.casesAreLoading) return <h3>Loading...</h3>;
+          else return <CasesTableP dataSource={this.props.caseStore.cases} />;
+        }
+        return <h3>An error occurred.</h3>;
       }
     }
   )
