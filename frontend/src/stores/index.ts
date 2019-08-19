7f264fbe2bd857c2501e6d3c66453b7358bc1d9d
Veeral Patel <vpatel98@gmail.com>
2019-08-18 22:12:01 -0700

    disable enforceActions for now

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -1,12 +1,11 @@
-import { configure } from "mobx";
 import CaseStore from "stores/CaseStore";
 
 const caseStore = new CaseStore();
 
 // only let me mutate state within actions
-configure({
-  enforceActions: "always"
-});
+// configure({
+//   enforceActions: "always"
+// });
 
 export default {
   caseStore
