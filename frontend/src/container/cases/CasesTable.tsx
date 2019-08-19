e5d700bf8a5d8e1eec2e949fc972fb283361e840
Veeral Patel <vpatel98@gmail.com>
2019-08-18 22:47:18 -0700

    fix performance issue

--- frontend/src/container/cases/CasesTable.tsx
+++ frontend/src/container/cases/CasesTable.tsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { notification, Table } from "antd";
 import { inject, observer } from "mobx-react";
 import CasesTableP from "presentational/cases/CasesTableP";
 import React from "react";
@@ -14,6 +14,11 @@ export default inject("caseStore")(
       componentDidMount() {
         if (this.props.caseStore) {
           this.props.caseStore.loadCases();
+        } else {
+          notification.error({
+            message: "An error occurred while fetching cases",
+            description: "caseStore is undefined"
+          });
         }
       }
 
@@ -23,7 +28,7 @@ export default inject("caseStore")(
             return <Table loading={true} />;
           else return <CasesTableP dataSource={this.props.caseStore.cases} />;
         }
-        return <h3>An error occurred.</h3>;
+        return <h3>An error occurred: caseStore is undefined</h3>;
       }
     }
   )
