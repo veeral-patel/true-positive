e8e4ce45f658508ee817b4476ed570bd12b83e63
Veeral Patel <vpatel98@gmail.com>
2019-08-19 18:44:26 -0700

    move Actions dropdown to the right

--- frontend/src/pages/cases/AllCasesPage.tsx
+++ frontend/src/pages/cases/AllCasesPage.tsx
@@ -1,8 +1,19 @@
 import { RouteComponentProps } from "@reach/router";
-import { Button, PageHeader } from "antd";
+import { Button, Dropdown, Icon, Menu, PageHeader } from "antd";
 import CasesTable from "container/cases/CasesTable";
 import FilterInput from "container/cases/FilterInput";
 import React from "react";
+
+const menu = (
+  <Menu>
+    <Menu.Item key="add_tags">Add Tags</Menu.Item>
+    <Menu.Item key="remove_tags">Remove Tags</Menu.Item>
+    <Menu.Item key="add_comment">Add Comment</Menu.Item>
+    <Menu.Item key="change_status">Change Status</Menu.Item>
+    <Menu.Item key="change_priority">Change Priority</Menu.Item>
+    <Menu.Item key="assign">Assign</Menu.Item>
+  </Menu>
+);
 
 const AllCasesPage: React.FC<RouteComponentProps> = () => (
   <div>
@@ -12,9 +23,18 @@ const AllCasesPage: React.FC<RouteComponentProps> = () => (
       style={{ paddingLeft: 0 }}
       extra={[<Button type="primary">Create Case</Button>]}
     />
-    <div style={{ marginBottom: "30px", width: "40%" }}>
+    <span
+      style={{ marginBottom: "30px", width: "40%", display: "inline-block" }}
+    >
       <FilterInput />
-    </div>
+    </span>
+    <span style={{ float: "right", paddingRight: "24px" }}>
+      <Dropdown overlay={menu}>
+        <Button>
+          Actions <Icon type="down" />
+        </Button>
+      </Dropdown>
+    </span>
     <CasesTable />
   </div>
 );
