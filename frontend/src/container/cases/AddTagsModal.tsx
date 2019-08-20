1fae1a33ccd111760ba2a26a3f549d9aaae272bd
Veeral Patel <vpatel98@gmail.com>
2019-08-19 22:58:07 -0700

    add an info message to our modal

--- frontend/src/container/cases/AddTagsModal.tsx
+++ frontend/src/container/cases/AddTagsModal.tsx
@@ -1,8 +1,10 @@
-import { Modal } from "antd";
+import { Alert, Modal, Select, Typography } from "antd";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import CaseStore from "stores/CaseStore";
 import UIStore from "stores/UIStore";
+
+const { Paragraph } = Typography;
 
 interface IAddTagsModalProps {
   uiStore?: UIStore;
@@ -24,7 +26,16 @@ export default inject("uiStore", "caseStore")(
             onCancel={() => uiStore!.closeModal()}
             okText="Add Tags"
           >
-            <p>Some contents here</p>
+            <Alert message="If a selected case already has a tag that you enter, the tag won't be added again to the case." type="info" showIcon />
+            <br />
+            <Paragraph>
+              Simply type in the tags you'd like to add to the selected case(s).
+            </Paragraph>
+            <Select
+              mode="tags"
+              style={{ width: "100%" }}
+              placeholder="Enter tags"
+            />
           </Modal>
         );
       }
