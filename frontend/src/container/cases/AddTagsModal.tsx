bddaf4195e7ac433b806f80d4d23492cf965bd86
Veeral Patel <vpatel98@gmail.com>
2019-08-19 22:43:37 -0700

    improve title and okText of modal

--- frontend/src/container/cases/AddTagsModal.tsx
+++ frontend/src/container/cases/AddTagsModal.tsx
@@ -1,23 +1,28 @@
 import { Modal } from "antd";
 import { inject, observer } from "mobx-react";
 import React from "react";
+import CaseStore from "stores/CaseStore";
 import UIStore from "stores/UIStore";
 
 interface IAddTagsModalProps {
   uiStore?: UIStore;
+  caseStore?: CaseStore;
 }
 
-export default inject("uiStore")(
+export default inject("uiStore", "caseStore")(
   observer(
     class AddTagsModal extends React.Component<IAddTagsModalProps> {
       render() {
-        const { uiStore } = this.props;
+        const { uiStore, caseStore } = this.props;
         return (
           <Modal
-            title="Add Tags"
+            title={
+              "Add Tags to " + caseStore!.numberOfSelectedCases + " Case(s)"
+            }
             visible={uiStore!.openModal === "ADD_TAGS_TO_CASE_MODAL"}
             onOk={() => uiStore!.closeModal()}
             onCancel={() => uiStore!.closeModal()}
+            okText="Add Tags"
           >
             <p>Some contents here</p>
           </Modal>
