8e2a9bbe57064b99601ae409ca6c7dbbca9d3972
Veeral Patel <vpatel98@gmail.com>
2019-08-19 20:33:53 -0700

    only display actions dropdown if a case is selected

--- frontend/src/pages/cases/AllCasesPage.tsx
+++ frontend/src/pages/cases/AllCasesPage.tsx
@@ -3,26 +3,47 @@ import { Button, PageHeader } from "antd";
 import ActionsDropdown from "container/cases/ActionsDropdown";
 import CasesTable from "container/cases/CasesTable";
 import FilterInput from "container/cases/FilterInput";
+import { inject, observer } from "mobx-react";
 import React from "react";
+import CaseStore from "stores/CaseStore";
 
-const AllCasesPage: React.FC<RouteComponentProps> = () => (
-  <div>
-    <PageHeader
-      title={<h2>Cases</h2>}
-      backIcon={false}
-      style={{ paddingLeft: 0 }}
-      extra={[<Button type="primary">Create Case</Button>]}
-    />
-    <span
-      style={{ marginBottom: "30px", width: "40%", display: "inline-block" }}
-    >
-      <FilterInput />
-    </span>
-    <span style={{ float: "right", paddingRight: "24px" }}>
-      <ActionsDropdown />
-    </span>
-    <CasesTable />
-  </div>
+interface IAllCasesPageProps extends RouteComponentProps {
+  caseStore?: CaseStore;
+}
+
+export default inject("caseStore")(
+  observer(
+    class AllCasesPage extends React.Component<IAllCasesPageProps> {
+      render() {
+        const { caseStore } = this.props;
+        return (
+          <div>
+            <PageHeader
+              title={<h2>Cases</h2>}
+              backIcon={false}
+              style={{ paddingLeft: 0 }}
+              extra={[<Button type="primary">Create Case</Button>]}
+            />
+            <span
+              style={{
+                marginBottom: "30px",
+                width: "40%",
+                display: "inline-block"
+              }}
+            >
+              <FilterInput />
+            </span>
+            {caseStore!.numberOfSelectedCases === 0 ? (
+              <span />
+            ) : (
+              <span style={{ float: "right", paddingRight: "24px" }}>
+                <ActionsDropdown />
+              </span>
+            )}
+            <CasesTable />
+          </div>
+        );
+      }
+    }
+  )
 );
-
-export default AllCasesPage;
