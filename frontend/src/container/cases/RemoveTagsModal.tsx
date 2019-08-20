fdefdfd5eb38f67ea3f3e7c3b3219d208eb18104
Veeral Patel <vpatel98@gmail.com>
2019-08-20 00:27:45 -0700

    remove tags modal now opens

--- frontend/src/container/cases/RemoveTagsModal.tsx
+++ frontend/src/container/cases/RemoveTagsModal.tsx
@@ -1,10 +1,26 @@
 import { Modal } from "antd";
+import { inject, observer } from "mobx-react";
 import React from "react";
+import CaseStore from "stores/CaseStore";
+import UIStore from "stores/UIStore";
 
-class RemoveTagsModal extends React.Component {
-  render() {
-    return <Modal title="Remove Tags" visible={false} />;
-  }
+interface IRemoveTagsModalProps {
+  uiStore?: UIStore;
+  caseStore?: CaseStore;
 }
 
-export default RemoveTagsModal;
+export default inject("uiStore", "caseStore")(
+  observer(
+    class RemoveTagsModal extends React.Component<IRemoveTagsModalProps> {
+      render() {
+        const { uiStore, caseStore } = this.props;
+        return (
+          <Modal
+            title={`Remove Tags from ${caseStore!.numberOfSelectedCases} Cases`}
+            visible={uiStore!.openModal === "REMOVE_TAGS_FROM_CASE_MODAL"}
+          />
+        );
+      }
+    }
+  )
+);
