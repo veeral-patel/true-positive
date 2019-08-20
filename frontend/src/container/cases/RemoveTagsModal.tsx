790ebe17babf72b936c9c483497d1666de361402
Veeral Patel <vpatel98@gmail.com>
2019-08-20 00:36:01 -0700

    add Alert to our remove tags modal

--- frontend/src/container/cases/RemoveTagsModal.tsx
+++ frontend/src/container/cases/RemoveTagsModal.tsx
@@ -1,8 +1,10 @@
-import { Modal } from "antd";
+import { Alert, Modal, Select, Typography } from "antd";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import CaseStore from "stores/CaseStore";
 import UIStore from "stores/UIStore";
+
+const { Paragraph } = Typography;
 
 interface IRemoveTagsModalProps {
   uiStore?: UIStore;
@@ -21,7 +23,23 @@ export default inject("uiStore", "caseStore")(
             onOk={() => uiStore!.closeModal()}
             onCancel={() => uiStore!.closeModal()}
             okText="Remove Tags"
-          />
+          >
+            <Alert
+              message="If a selected case doesn't have all of the tags below, it's fine! We'll just remove the tags below that it does have."
+              type="info"
+              showIcon
+            />
+            <br />
+            <Paragraph>
+              Simply type in the tags you'd like to remove from all of the
+              selected case(s).
+            </Paragraph>
+            <Select
+              mode="tags"
+              style={{ width: "100%" }}
+              placeholder="Enter tags"
+            />
+          </Modal>
         );
       }
     }
