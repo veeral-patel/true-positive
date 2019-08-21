d5fa1f21c73e3c22272f181fe46c65665f46bd23
Veeral Patel <vpatel98@gmail.com>
2019-08-20 21:39:41 -0700

    add comma as a token separator

--- frontend/src/presentational/cases/CreateCaseForm.tsx
+++ frontend/src/presentational/cases/CreateCaseForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Input } from "antd";
+import { Form, Input, Select } from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import React from "react";
 
@@ -6,6 +6,9 @@ const CreateCaseForm: React.FC = () => (
   <Form colon={false}>
     <Form.Item label="Name" required>
       <Input placeholder="Found Ryuk" autoFocus />
+    </Form.Item>
+    <Form.Item label="Tags">
+      <Select mode="tags" placeholder="Enter tags" tokenSeparators={[","]} />
     </Form.Item>
     <Form.Item label="Description">
       <TextArea placeholder="Enter description here" rows={5} />
