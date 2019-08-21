5cebd3341db35df25e8ffaaba05f21f222445fb5
Veeral Patel <vpatel98@gmail.com>
2019-08-20 21:47:47 -0700

    make status and priority required

--- frontend/src/presentational/cases/CreateCaseForm.tsx
+++ frontend/src/presentational/cases/CreateCaseForm.tsx
@@ -2,10 +2,34 @@ import { Form, Input, Select } from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import React from "react";
 
+const { Option } = Select;
+
 const CreateCaseForm: React.FC = () => (
   <Form colon={false}>
     <Form.Item label="Name" required>
       <Input placeholder="Found Ryuk" autoFocus />
+    </Form.Item>
+    <Form.Item label="Status" required>
+      <Select
+        showSearch
+        placeholder="Choose a status"
+        optionFilterProp="children"
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="tom">Tom</Option>
+      </Select>
+    </Form.Item>
+    <Form.Item label="Priority" required>
+      <Select
+        showSearch
+        placeholder="Choose a priority"
+        optionFilterProp="children"
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="tom">Tom</Option>
+      </Select>
     </Form.Item>
     <Form.Item label="Tags">
       <Select mode="tags" placeholder="Enter tags" tokenSeparators={[","]} />
