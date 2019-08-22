66253197d66db71f45c9cb2091f4952733d4ee22
Veeral Patel <vpatel98@gmail.com>
2019-08-21 23:37:16 -0700

    finish refactoring sider

--- frontend/src/pages/cases/CasePage.tsx
+++ frontend/src/pages/cases/CasePage.tsx
@@ -1,13 +1,33 @@
 import { RouteComponentProps } from "@reach/router";
+import { Breadcrumb, Layout } from "antd";
 import CaseSider from "container/cases/CaseSider";
 import React from "react";
+
+const { Content } = Layout;
 
 interface ICasePageProps extends RouteComponentProps {}
 
 const CasePage: React.FC<ICasePageProps> = () => (
-  <div>
+  <Layout>
     <CaseSider />
-  </div>
+    <Layout style={{ padding: "0 24px 24px" }}>
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Content
+        style={{
+          background: "#fff",
+          padding: 24,
+          margin: 0,
+          minHeight: 280
+        }}
+      >
+        Content
+      </Content>
+    </Layout>
+  </Layout>
 );
 
 export default CasePage;
--- frontend/src/presentational/cases/CaseSiderP.tsx
+++ frontend/src/presentational/cases/CaseSiderP.tsx
@@ -1,8 +1,8 @@
-import { Breadcrumb, Icon, Layout, Menu } from "antd";
+import { Icon, Layout, Menu } from "antd";
 import { CollapseType } from "antd/lib/layout/Sider";
 import React from "react";
 
-const { Content, Sider } = Layout;
+const { Sider } = Layout;
 
 interface ICaseSiderProps {
   collapsed: boolean;
@@ -13,60 +13,41 @@ const CaseSiderP: React.FC<ICaseSiderProps> = ({
   collapsed,
   handleCollapse
 }) => (
-  <Layout>
-    <Sider
-      width={200}
-      style={{ background: "#fff" }}
-      collapsible
-      collapsed={collapsed}
-      onCollapse={handleCollapse}
-      theme="light"
+  <Sider
+    width={200}
+    style={{ background: "#fff" }}
+    collapsible
+    collapsed={collapsed}
+    onCollapse={handleCollapse}
+    theme="light"
+  >
+    <Menu
+      mode="inline"
+      defaultSelectedKeys={["info"]}
+      style={{ height: "100%", borderRight: 0 }}
     >
-      <Menu
-        mode="inline"
-        defaultSelectedKeys={["info"]}
-        style={{ height: "100%", borderRight: 0 }}
-      >
-        <Menu.Item key="info">
-          <Icon type="info-circle" />
-          <span>Info</span>
-        </Menu.Item>
-        <Menu.Item key="tasks">
-          <Icon type="check-square" />
-          <span>Tasks</span>
-        </Menu.Item>
-        <Menu.Item key="indicators">
-          <Icon type="security-scan" />
-          <span>Indicators</span>
-        </Menu.Item>
-        <Menu.Item key="tree">
-          <Icon type="apartment" />
-          <span>Tree</span>
-        </Menu.Item>
-        <Menu.Item key="members">
-          <Icon type="user" />
-          <span>Members</span>
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout style={{ padding: "0 24px 24px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <Content
-        style={{
-          background: "#fff",
-          padding: 24,
-          margin: 0,
-          minHeight: 280
-        }}
-      >
-        Content
-      </Content>
-    </Layout>
-  </Layout>
+      <Menu.Item key="info">
+        <Icon type="info-circle" />
+        <span>Info</span>
+      </Menu.Item>
+      <Menu.Item key="tasks">
+        <Icon type="check-square" />
+        <span>Tasks</span>
+      </Menu.Item>
+      <Menu.Item key="indicators">
+        <Icon type="security-scan" />
+        <span>Indicators</span>
+      </Menu.Item>
+      <Menu.Item key="tree">
+        <Icon type="apartment" />
+        <span>Tree</span>
+      </Menu.Item>
+      <Menu.Item key="members">
+        <Icon type="user" />
+        <span>Members</span>
+      </Menu.Item>
+    </Menu>
+  </Sider>
 );
 
 export default CaseSiderP;
