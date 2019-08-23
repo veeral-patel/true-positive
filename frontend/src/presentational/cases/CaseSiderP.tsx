688c721c41ca4fea88f2da56379da21f75ba8ca7
Veeral Patel <vpatel98@gmail.com>
2019-08-22 21:24:29 -0700

    change route when clicking on menu items

--- frontend/src/presentational/cases/CaseSiderP.tsx
+++ frontend/src/presentational/cases/CaseSiderP.tsx
@@ -1,3 +1,4 @@
+import { navigate } from "@reach/router";
 import { Icon, Layout, Menu } from "antd";
 import { CollapseType } from "antd/lib/layout/Sider";
 import React from "react";
@@ -26,23 +27,23 @@ const CaseSiderP: React.FC<ICaseSiderProps> = ({
       defaultSelectedKeys={["info"]}
       style={{ height: "100%", borderRight: 0 }}
     >
-      <Menu.Item key="info">
+      <Menu.Item key="info" onClick={() => navigate("info")}>
         <Icon type="info-circle" />
         <span>Info</span>
       </Menu.Item>
-      <Menu.Item key="tasks">
+      <Menu.Item key="tasks" onClick={() => navigate("tasks")}>
         <Icon type="check-square" />
         <span>Tasks</span>
       </Menu.Item>
-      <Menu.Item key="indicators">
+      <Menu.Item key="indicators" onClick={() => navigate("indicators")}>
         <Icon type="security-scan" />
         <span>Indicators</span>
       </Menu.Item>
-      <Menu.Item key="tree">
+      <Menu.Item key="tree" onClick={() => navigate("tree")}>
         <Icon type="apartment" />
         <span>Tree</span>
       </Menu.Item>
-      <Menu.Item key="members">
+      <Menu.Item key="members" onClick={() => navigate("members")}>
         <Icon type="user" />
         <span>Members</span>
       </Menu.Item>
