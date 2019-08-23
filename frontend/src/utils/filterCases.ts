21e96d555bf5aa01e923c648007c73ee18dea397
Veeral Patel <vpatel98@gmail.com>
2019-08-22 20:13:21 -0700

    refactor filter logic

--- frontend/src/utils/filterCases.ts
+++ frontend/src/utils/filterCases.ts
@@ -1,22 +1,46 @@
 import ICase from "ts/interfaces/ICase";
 
-// Returns true iff the case matches the given filter value
-function matchesFilter(filterValue: string, thecase: ICase) {
-  const assignedToMatches = thecase.assignedTo
+function nameMatches(filterValue: string, thecase: ICase) {
+  return thecase.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1;
+}
+
+function statusMatches(filterValue: string, thecase: ICase) {
+  return (
+    thecase.status.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
+  );
+}
+
+function priorityMatches(filterValue: string, thecase: ICase) {
+  return (
+    thecase.priority.name.toLowerCase().indexOf(filterValue.toLowerCase()) !==
+    -1
+  );
+}
+
+function assignedToMatches(filterValue: string, thecase: ICase) {
+  return thecase.assignedTo
     ? thecase.assignedTo.username
         .toLowerCase()
         .indexOf(filterValue.toLowerCase()) !== -1
     : false;
+}
+
+function createdByMatches(filterValue: string, thecase: ICase) {
   return (
-    thecase.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1 ||
-    thecase.status.name.toLowerCase().indexOf(filterValue.toLowerCase()) !==
-      -1 ||
-    thecase.priority.name.toLowerCase().indexOf(filterValue.toLowerCase()) !==
-      -1 ||
     thecase.createdBy.username
       .toLowerCase()
-      .indexOf(filterValue.toLowerCase()) !== -1 ||
-    assignedToMatches
+      .indexOf(filterValue.toLowerCase()) !== -1
+  );
+}
+
+// Returns true iff the case matches the given filter value
+function matchesFilter(filterValue: string, thecase: ICase) {
+  return (
+    nameMatches(filterValue, thecase) ||
+    statusMatches(filterValue, thecase) ||
+    priorityMatches(filterValue, thecase) ||
+    createdByMatches(filterValue, thecase) ||
+    assignedToMatches(filterValue, thecase)
   );
 }
 
