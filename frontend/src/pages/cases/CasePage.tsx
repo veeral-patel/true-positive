49d877dc98d11d9bff87fd63b408cf02eae94fb8
Veeral Patel <vpatel98@gmail.com>
2019-08-22 22:11:58 -0700

    make components presentational

--- frontend/src/pages/cases/CasePage.tsx
+++ frontend/src/pages/cases/CasePage.tsx
@@ -1,10 +1,10 @@
 import { RouteComponentProps, Router } from "@reach/router";
 import { Layout } from "antd";
 import CaseSider from "container/one_case/CaseSider";
-import Indicators from "container/one_case/Indicators";
-import Info from "container/one_case/Info";
-import Members from "container/one_case/Members";
-import Tasks from "container/one_case/Tasks";
+import IndicatorsP from "presentational/one_case/IndicatorsP";
+import InfoP from "presentational/one_case/InfoP";
+import MembersP from "presentational/one_case/MembersP";
+import TasksP from "presentational/one_case/TasksP";
 import React from "react";
 
 interface ICasePageProps extends RouteComponentProps {}
@@ -14,11 +14,11 @@ const CasePage: React.FC<ICasePageProps> = () => (
     <CaseSider />
     <Layout style={{ padding: "0 24px 24px" }}>
       <Router>
-        <Info path="/" />
-        <Info path="/info" />
-        <Members path="/members" />
-        <Indicators path="/indicators" />
-        <Tasks path="/tasks" />
+        <InfoP path="/" />
+        <InfoP path="/info" />
+        <MembersP path="/members" />
+        <IndicatorsP path="/indicators" />
+        <TasksP path="/tasks" />
       </Router>
     </Layout>
   </Layout>
