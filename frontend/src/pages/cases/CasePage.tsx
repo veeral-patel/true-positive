ed14496aca974e54e50f3c5e1684f78431c24bb9
Veeral Patel <vpatel98@gmail.com>
2019-08-24 12:13:29 -0700

    set activeCaseId when CasePage loads

--- frontend/src/pages/cases/CasePage.tsx
+++ frontend/src/pages/cases/CasePage.tsx
@@ -1,6 +1,7 @@
 import { RouteComponentProps, Router } from "@reach/router";
 import { Layout } from "antd";
 import CaseSider from "container/one_case/CaseSider";
+import { inject, observer } from "mobx-react";
 import Page404 from "pages/shared/Page404";
 import IndicatorsP from "presentational/one_case/IndicatorsP";
 import InfoP from "presentational/one_case/InfoP";
@@ -8,26 +9,37 @@ import MembersP from "presentational/one_case/MembersP";
 import TasksP from "presentational/one_case/TasksP";
 import TreeP from "presentational/one_case/TreeP";
 import React from "react";
+import ActiveCaseStore from "stores/ActiveCaseStore";
 
 interface ICasePageProps extends RouteComponentProps {
   caseId?: number;
+  activeCaseStore?: ActiveCaseStore;
 }
 
-const CasePage: React.FC<ICasePageProps> = ({ caseId }) => (
-  <Layout>
-    <CaseSider />
-    <Layout style={{ padding: "0 24px 24px" }}>
-      <Router>
-        <InfoP path="/" />
-        <InfoP path="/info" />
-        <TreeP path="/tree" />
-        <MembersP path="/members" />
-        <IndicatorsP path="/indicators" />
-        <TasksP path="/tasks" />
-        <Page404 default showBackButton={false} />
-      </Router>
-    </Layout>
-  </Layout>
-);
+class CasePage extends React.Component<ICasePageProps> {
+  componentDidMount() {
+    const { activeCaseStore, caseId } = this.props;
+    if (caseId) activeCaseStore!.setActiveCaseId(caseId);
+  }
 
-export default CasePage;
+  render() {
+    return (
+      <Layout>
+        <CaseSider />
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Router>
+            <InfoP path="/" />
+            <InfoP path="/info" />
+            <TreeP path="/tree" />
+            <MembersP path="/members" />
+            <IndicatorsP path="/indicators" />
+            <TasksP path="/tasks" />
+            <Page404 default showBackButton={false} />
+          </Router>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default inject("activeCaseStore")(observer(CasePage));
