b857c19f776b63ab834df2fc00916e03249f84e0
Veeral Patel <vpatel98@gmail.com>
2019-08-24 12:08:21 -0700

    add setter for activeCaseId

--- frontend/src/stores/ActiveCaseStore.ts
+++ frontend/src/stores/ActiveCaseStore.ts
@@ -1,7 +1,12 @@
-import { observable } from "mobx";
+import { action, observable } from "mobx";
 
 class ActiveCaseStore {
   @observable activeCaseId: number | null = null;
+
+  @action.bound
+  setActiveCaseId(activeCaseId: number) {
+    this.activeCaseId = activeCaseId;
+  }
 }
 
 export default ActiveCaseStore;
