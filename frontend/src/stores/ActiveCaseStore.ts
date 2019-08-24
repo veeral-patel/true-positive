6b9e31876d6b32ff367b2c7c4c3fd149afb5d155
Veeral Patel <vpatel98@gmail.com>
2019-08-24 14:23:27 -0700

    add an autorun function to clear related variables when activeCaseId becomes null

--- frontend/src/stores/ActiveCaseStore.ts
+++ frontend/src/stores/ActiveCaseStore.ts
@@ -1,7 +1,22 @@
-import { action, observable } from "mobx";
+import { action, autorun, observable, runInAction } from "mobx";
+import ICase from "ts/interfaces/ICase";
 
 class ActiveCaseStore {
   @observable activeCaseId: number | null = null;
+  @observable activeCase: ICase | null = null;
+  @observable activeCaseIsLoading: boolean = false;
+
+  constructor() {
+    // when you leave a case page, clear its derived variables
+    autorun(() => {
+      if (this.activeCaseId === null) {
+        runInAction(() => {
+          this.activeCase = null;
+          this.activeCaseIsLoading = false;
+        });
+      }
+    });
+  }
 
   @action.bound
   setActiveCaseId(activeCaseId: number | null) {
