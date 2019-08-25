ae70cf93ffad55db8359597219de511c6c8d27dd
Veeral Patel <vpatel98@gmail.com>
2019-08-25 15:14:49 -0700

    use flexbox

--- frontend/src/presentational/one_case/InfoP.tsx
+++ frontend/src/presentational/one_case/InfoP.tsx
@@ -18,88 +18,84 @@ interface InfoProps {
 const Info: React.FC<InfoProps> = ({ activeCase }) => (
   <div>
     <OneCaseBreadcrumb caseName={activeCase.name} tabName="Info" />
-    <Content
-      style={{
-        background: "#fff",
-        padding: 24,
-        margin: 0,
-        minHeight: 280,
-        width: "70%",
-        float: "left"
-      }}
-    >
-      <h2>Info</h2>
-      <section style={{ lineHeight: 3 }}>
-        <Row>
-          <Col span={24}>
-            <Divider orientation="left">Details</Divider>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={4}>Status:</Col>
-          <Col span={8}>
-            <StatusTagP statusName={activeCase.status.name} />
-          </Col>
-          <Col span={4}>Created:</Col>
-          <Col span={8}>
-            {`${activeCase.formattedCreatedAt} by ${
-              activeCase.createdBy.username
-            }`}
-          </Col>
-        </Row>
-        <Row>
-          <Col span={4}>Priority:</Col>
-          <Col span={8}>
-            <PriorityTagP priorityName={activeCase.priority.name} />
-          </Col>
-          <Col span={4}>Assigned To:</Col>
-          <Col span={8}>
-            {activeCase.assignedTo ? activeCase.assignedTo.username : "N/A"}
-          </Col>
-        </Row>
-        <Row>
-          <Col span={4}>Tags:</Col>
-          <Col span={20}>{<ListOfTagsP tags={activeCase.tags} />}</Col>
-        </Row>
-      </section>
-      <section>
-        <Row>
-          <Col span={24}>
-            <Divider orientation="left">Description</Divider>
-            <DescriptionP description={activeCase.description} />
-          </Col>
-        </Row>
-      </section>
-      <section>
-        <Row>
-          <Col span={24}>
-            <Divider orientation="left">
-              Comments ({activeCase.comments.length})
-            </Divider>
-            <CommentList comments={activeCase.comments} />
-          </Col>
-        </Row>
-      </section>
-    </Content>
-    <Content
-      style={{
-        width: "28%",
-        float: "right",
-        backgroundColor: "#fff",
-        padding: 24,
-        minHeight: 700,
-        marginLeft: 24
-      }}
-    >
-      <Text type="secondary" style={{ textTransform: "uppercase" }}>
-        Activity
-      </Text>
-      <br />
-      <br />
-      <br />
-      <br />
-      <Empty />
-    </Content>
+    <div style={{ display: "flex" }}>
+      <Content
+        style={{
+          background: "#fff",
+          padding: 24,
+          margin: 0,
+          minHeight: 280,
+          flex: 3
+        }}
+      >
+        <h2>Info</h2>
+        <section style={{ lineHeight: 3 }}>
+          <Row>
+            <Col span={24}>
+              <Divider orientation="left">Details</Divider>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={4}>Status:</Col>
+            <Col span={8}>
+              <StatusTagP statusName={activeCase.status.name} />
+            </Col>
+            <Col span={4}>Created:</Col>
+            <Col span={8}>
+              {`${activeCase.formattedCreatedAt} by ${
+                activeCase.createdBy.username
+              }`}
+            </Col>
+          </Row>
+          <Row>
+            <Col span={4}>Priority:</Col>
+            <Col span={8}>
+              <PriorityTagP priorityName={activeCase.priority.name} />
+            </Col>
+            <Col span={4}>Assigned To:</Col>
+            <Col span={8}>
+              {activeCase.assignedTo ? activeCase.assignedTo.username : "N/A"}
+            </Col>
+          </Row>
+          <Row>
+            <Col span={4}>Tags:</Col>
+            <Col span={8}>{<ListOfTagsP tags={activeCase.tags} />}</Col>
+          </Row>
+        </section>
+        <section>
+          <Row>
+            <Col span={24}>
+              <Divider orientation="left">Description</Divider>
+              <DescriptionP description={activeCase.description} />
+            </Col>
+          </Row>
+        </section>
+        <section>
+          <Row>
+            <Col span={24}>
+              <Divider orientation="left">
+                Comments ({activeCase.comments.length})
+              </Divider>
+              <CommentList comments={activeCase.comments} />
+            </Col>
+          </Row>
+        </section>
+      </Content>
+      <Content
+        style={{
+          backgroundColor: "#fff",
+          padding: 24,
+          minHeight: 700,
+          marginLeft: 24,
+          flex: 1
+        }}
+      >
+        <Text type="secondary" style={{ textTransform: "uppercase" }}>
+          Activity
+        </Text>
+        <Empty />
+      </Content>
+    </div>
   </div>
 );
 
