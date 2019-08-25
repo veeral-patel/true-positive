a174959bdc37d0e157cc50cb35a095fc54fbbd03
Veeral Patel <vpatel98@gmail.com>
2019-08-25 13:09:29 -0700

    test that created_by is validated on Comment

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :commentable, polymorphic: true
+    belongs_to :created_by, :class_name => 'User', optional: false
 
     validates :comment, presence: true
+    validates :created_by, presence: true
 end
--- backend/spec/models/comment_spec.rb
+++ backend/spec/models/comment_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
     describe 'validations' do
         it { should validate_presence_of(:comment) }
+        it { should validate_presence_of(:created_by) }
     end
 end
 
