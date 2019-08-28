faa2193ccc98a84e80781b895beb48ce1a2ca3ac
Veeral Patel <vpatel98@gmail.com>
2019-08-27 22:17:23 -0700

    add unique, required email to user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true
+
     has_many :case_members
     has_many :accessible_cases, :through => :case_members
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_034148) do
+ActiveRecord::Schema.define(version: 2019_08_28_050927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2019_08_28_034148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "email"
   end
 
   add_foreign_key "case_members", "cases"
