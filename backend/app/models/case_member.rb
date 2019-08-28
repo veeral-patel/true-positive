e8a955dcca61f0cad917507e354f7a3e9a275c1f
Veeral Patel <vpatel98@gmail.com>
2019-08-27 22:41:21 -0700

    Cannot have duplicate members in a case.

--- backend/app/models/case_member.rb
+++ backend/app/models/case_member.rb
@@ -2,5 +2,7 @@ class CaseMember < ApplicationRecord
   belongs_to :case
   belongs_to :member, :class_name => 'User', :foreign_key => 'user_id'
 
+  validates_uniqueness_of :user_id, :scope => :case_id, :message => "Cannot have duplicate members in a case."
+
   enum role: { CAN_VIEW: 1, CAN_EDIT: 2 }
 end
