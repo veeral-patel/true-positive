dff456d1d51f87667b3cdd33aafa298ee925ea07
Veeral Patel <vpatel98@gmail.com>
2019-08-27 22:47:27 -0700

    require role

--- backend/app/models/case_member.rb
+++ backend/app/models/case_member.rb
@@ -1,8 +1,9 @@
 class CaseMember < ApplicationRecord
+  validates_uniqueness_of :user_id, :scope => :case_id, :message => "Cannot have duplicate members in a case."
+  validates :role, presence: true
+
   belongs_to :case
   belongs_to :member, :class_name => 'User', :foreign_key => 'user_id'
-
-  validates_uniqueness_of :user_id, :scope => :case_id, :message => "Cannot have duplicate members in a case."
 
   enum role: { CAN_VIEW: 1, CAN_EDIT: 2 }
 end
