b42c6c230cf3031301a5ca4fed71087a6a8df931
Veeral Patel <vpatel98@gmail.com>
2019-08-27 19:56:28 -0700

    add list to members page

--- frontend/src/presentational/one_case/MembersP.tsx
+++ frontend/src/presentational/one_case/MembersP.tsx
@@ -1,9 +1,32 @@
-import { Layout } from "antd";
+import { Avatar, Icon, Layout, List, Select } from "antd";
 import React from "react";
 
 const { Content } = Layout;
+const { Option } = Select;
 
 interface MembersProps {}
+
+const data = [
+  {
+    username: "rob",
+    email: "rob@example.org"
+  },
+  {
+    username: "john",
+    email: "john@example.org"
+  }
+];
+
+const RoleSelect = () => (
+  <Select defaultValue="lucy" style={{ width: 120 }}>
+    <Option value="jack">Jack</Option>
+    <Option value="lucy">Lucy</Option>
+    <Option value="disabled" disabled>
+      Disabled
+    </Option>
+    <Option value="Yiminghe">yiminghe</Option>
+  </Select>
+);
 
 const Members: React.FC<MembersProps> = () => (
   <Content
@@ -15,6 +38,27 @@ const Members: React.FC<MembersProps> = () => (
     }}
   >
     <h2>Members</h2>
+    <List
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={member => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title={member.username}
+            description={member.email}
+          />
+          <div>
+            <div style={{ marginRight: "30px", display: "inline-block" }}>
+              <RoleSelect />
+            </div>
+            <Icon type="cross" />
+          </div>
+        </List.Item>
+      )}
+    />
   </Content>
 );
 
