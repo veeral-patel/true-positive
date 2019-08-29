6cdadebb2a59e9e7fa4d168bd595969db5a5db19
Veeral Patel <vpatel98@gmail.com>
2019-08-29 00:27:12 -0700

    add loadActiveTask method

--- frontend/src/stores/ActiveTaskStore.ts
+++ frontend/src/stores/ActiveTaskStore.ts
@@ -1,5 +1,12 @@
+import { notification } from "antd";
+import { ApolloError, ApolloQueryResult } from "apollo-boost";
+import client from "createApolloClient";
 import { action, observable, runInAction } from "mobx";
 import ITask from "ts/interfaces/ITask";
+
+interface ITaskDatum {
+  task: ITask;
+}
 
 class ActiveTaskStore {
   @observable activeTaskId: number | null = null;
@@ -15,6 +22,25 @@ class ActiveTaskStore {
     } else {
       this.loadActiveTask();
     }
+  }
+
+  @action.bound
+  loadActiveTask() {
+    this.activeTaskIsLoading = true;
+    client
+      .query<ITaskDatum>({
+        query: GET_ONE_TASK,
+        variables: { id: this.activeTaskId }
+      })
+      .then((response: ApolloQueryResult<ITaskDatum>) => {
+        runInAction(() => (this.activeTask = response.data.task));
+      })
+      .catch((error: ApolloError) => {
+        notification.error({
+          message: "An error occurred while fetching the task",
+          description: error.message
+        });
+      });
   }
 
   @action.bound
