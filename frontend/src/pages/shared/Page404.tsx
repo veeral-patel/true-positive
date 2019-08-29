07bb9a906d8f5b142b4f88cf016d379b93ef7cdc
Veeral Patel <vpatel98@gmail.com>
2019-08-29 00:06:21 -0700

    add a white background to 404

--- frontend/src/pages/shared/Page404.tsx
+++ frontend/src/pages/shared/Page404.tsx
@@ -1,7 +1,9 @@
 import { navigate, RouteComponentProps } from "@reach/router";
-import { Button, Result } from "antd";
+import { Button, Layout, Result } from "antd";
 import React from "react";
 import { paths } from "utils/constants";
+
+const { Content } = Layout;
 
 interface Page404Props extends RouteComponentProps {
   showBackButton?: boolean;
@@ -11,20 +13,29 @@ const Page404: React.FC<Page404Props> = ({ showBackButton }) => {
   const showBackButtonReal =
     showBackButton !== undefined ? showBackButton : true;
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        showBackButtonReal ? (
-          <Button type="primary" onClick={() => navigate(paths.ROOT_PATH)}>
-            Back Home
-          </Button>
-        ) : (
-          <span />
-        )
-      }
-    />
+    <Content
+      style={{
+        backgroundColor: "#fff",
+        padding: 24,
+        marginLeft: 24,
+        height: "100%"
+      }}
+    >
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          showBackButtonReal ? (
+            <Button type="primary" onClick={() => navigate(paths.ROOT_PATH)}>
+              Back Home
+            </Button>
+          ) : (
+            <span />
+          )
+        }
+      />
+    </Content>
   );
 };
 
