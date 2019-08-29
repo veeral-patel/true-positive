bdd995e97ddc486c639945b1c94d9a1244123bbf
Veeral Patel <vpatel98@gmail.com>
2019-08-29 01:07:11 -0700

    add componentDidMount to OneTask

--- frontend/src/container/one_task/OneTask.tsx
+++ frontend/src/container/one_task/OneTask.tsx
@@ -1,13 +1,30 @@
 import { RouteComponentProps } from "@reach/router";
+import { notification } from "antd";
 import { inject, observer } from "mobx-react";
 import OneTaskP from "presentational/one_task/OneTaskP";
 import React from "react";
+import ActiveTaskStore from "stores/ActiveTaskStore";
 
-interface OneTaskProps extends RouteComponentProps {}
+interface OneTaskProps extends RouteComponentProps {
+  activeTaskStore?: ActiveTaskStore;
+  taskId?: number;
+}
 
 export default inject("activeTaskStore")(
   observer(
     class OneTask extends React.Component<OneTaskProps> {
+      componentDidMount() {
+        const { activeTaskStore, taskId } = this.props;
+        if (taskId) {
+          activeTaskStore!.setActiveTaskId(taskId);
+        } else {
+          notification.error({
+            message: "Unable to extract the task's ID from the URL",
+            description: "Ensure you're on a valid URL"
+          });
+        }
+      }
+
       render() {
         return <OneTaskP />;
       }
