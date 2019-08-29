557d49075575b0a6569ec799ccd6d6c2fcc4ae91
Veeral Patel <vpatel98@gmail.com>
2019-08-29 01:01:55 -0700

    make OneTask a class-based component

--- frontend/src/container/one_task/OneTask.tsx
+++ frontend/src/container/one_task/OneTask.tsx
@@ -1,11 +1,16 @@
 import { RouteComponentProps } from "@reach/router";
+import { inject, observer } from "mobx-react";
 import OneTaskP from "presentational/one_task/OneTaskP";
 import React from "react";
 
 interface OneTaskProps extends RouteComponentProps {}
 
-const OneTask: React.FC<OneTaskProps> = () => {
-  return <OneTaskP />;
-};
-
-export default OneTask;
+export default inject("activeTaskStore")(
+  observer(
+    class OneTask extends React.Component<OneTaskProps> {
+      render() {
+        return <OneTaskP />;
+      }
+    }
+  )
+);
