8692fad8cb0e6d56e12c2a2abf5a227f2d752cea
Veeral Patel <vpatel98@gmail.com>
2019-08-29 00:02:26 -0700

    make height of activity pane 100%

--- frontend/src/presentational/one_case/ActivityPaneP.tsx
+++ frontend/src/presentational/one_case/ActivityPaneP.tsx
@@ -9,7 +9,8 @@ const ActivityPaneP: React.FC = () => (
     style={{
       backgroundColor: "#fff",
       padding: 24,
-      marginLeft: 24
+      marginLeft: 24,
+      height: "100%"
     }}
   >
     <Text type="secondary" style={{ textTransform: "uppercase" }}>
