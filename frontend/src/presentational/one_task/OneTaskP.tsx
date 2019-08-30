3461b1c148e82816c8db89aadee17754a5de4902
Veeral Patel <vpatel98@gmail.com>
2019-08-29 20:08:41 -0700

    add description to task page

--- frontend/src/presentational/one_task/OneTaskP.tsx
+++ frontend/src/presentational/one_task/OneTaskP.tsx
@@ -1,6 +1,7 @@
 import { navigate } from "@reach/router";
-import { Layout, PageHeader } from "antd";
+import { Col, Divider, Layout, PageHeader, Row } from "antd";
 import DetailsP from "presentational/one_task/DetailsSectionP";
+import DescriptionP from "presentational/shared/description/DescriptionP";
 import React from "react";
 import ICase from "ts/interfaces/ICase";
 import ITask from "ts/interfaces/ITask";
@@ -26,7 +27,17 @@ const OneTaskP: React.FC<OneTaskProps> = ({ activeCase, activeTask }) => (
       onBack={() => navigate(getPathToCaseTasks(activeCase.id))}
       title={activeTask.name}
     />
+
     <DetailsP activeTask={activeTask} />
+
+    <section>
+      <Row>
+        <Col span={24}>
+          <Divider orientation="left">Description</Divider>
+          <DescriptionP description={activeTask.description} />
+        </Col>
+      </Row>
+    </section>
   </Content>
 );
 
