e34d977e01c5fc7cdbd92fb34c2ef18be3b02d5d
Veeral Patel <vpatel98@gmail.com>
2019-08-31 10:37:48 -0700

    add indicator model

---
+++ backend/app/models/indicator.rb
@@ -0,0 +1,10 @@
+class Case < ApplicationRecord
+    validates :name, presence: true
+    validates :created_by, presence: true
+    validates :task, presence: true
+
+    belongs_to :created_by, :class_name => 'User'
+    belongs_to :task
+
+    has_many :comments, as: :commentable
+end
