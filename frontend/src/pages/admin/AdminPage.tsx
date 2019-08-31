5f125e844350fde6c0f194a004c39f302094b31f
Veeral Patel <vpatel98@gmail.com>
2019-08-30 20:53:15 -0700

    lay out admin page

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,9 +1,25 @@
 import { RouteComponentProps } from "@reach/router";
+import { Tabs } from "antd";
 import React from "react";
+
+const { TabPane } = Tabs;
 
 class AdminPage extends React.Component<RouteComponentProps> {
   render() {
-    return <h2>Admin</h2>;
+    return (
+      <div>
+        <h2>Admin</h2>
+        <div style={{ display: "flex" }}>
+          <div style={{ flex: 1 }}>
+            <Tabs tabPosition="left">
+              <TabPane tab="Customize Statuses" key="statuses"></TabPane>
+              <TabPane tab="Customize Priorities" key="priorities"></TabPane>
+            </Tabs>
+          </div>
+          <div style={{ flex: 5 }}>Content</div>
+        </div>
+      </div>
+    );
   }
 }
 
