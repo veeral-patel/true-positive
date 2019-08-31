c3fba123b183cdc7134c82b12d9b2082f9a46089
Veeral Patel <vpatel98@gmail.com>
2019-08-30 21:08:02 -0700

    add helper text

--- frontend/src/presentational/admin/AdminTabsP.tsx
+++ frontend/src/presentational/admin/AdminTabsP.tsx
@@ -11,12 +11,25 @@ const AdminTabsP: React.FC = () => {
         <Text>
           <h4>Customize Statuses</h4>
           <Paragraph>
-            You can customize the default list of statuses to fit your
-            processes. For example, you may switch to a P0/P1/P2 scheme.
+            You can customize the default list of statuses to fit your workflow.
+          </Paragraph>
+          <Paragraph>
+            You might add a "In Review" or a "Blocked" status, for example.
           </Paragraph>
         </Text>
       </TabPane>
-      <TabPane tab="Customize Priorities" key="priorities"></TabPane>
+      <TabPane tab="Customize Priorities" key="priorities">
+        <Text>
+          <h4>Customize Priorities</h4>
+          <Paragraph>
+            You can customize the default list of priorities to fit your
+            workflow.
+          </Paragraph>
+          <Paragraph>
+            You could choose a P0/P1/P2 scheme, for example.
+          </Paragraph>
+        </Text>
+      </TabPane>
     </Tabs>
   );
 };
