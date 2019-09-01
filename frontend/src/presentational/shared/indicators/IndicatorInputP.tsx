9b4091bcb948c48baf0848d0a4db5841adb7c715
Veeral Patel <vpatel98@gmail.com>
2019-09-01 12:38:11 -0700

    adjust suffix icon based on status

--- frontend/src/presentational/shared/indicators/IndicatorInputP.tsx
+++ frontend/src/presentational/shared/indicators/IndicatorInputP.tsx
@@ -1,13 +1,35 @@
 import { Icon, Input } from "antd";
 import React from "react";
 
-class IndicatorInputP extends React.Component {
+interface IndicatorInputProps {}
+
+interface IndicatorInputState {
+  status: "READY" | "LOADING";
+}
+
+class IndicatorInputP extends React.Component<
+  IndicatorInputProps,
+  IndicatorInputState
+> {
+  constructor(props: IndicatorInputProps) {
+    super(props);
+    this.state = {
+      status: "READY"
+    };
+  }
+
   render() {
+    const { status } = this.state;
+
+    let suffix;
+    if (status === "READY") suffix = <Icon type="arrow-right" />;
+    else if (status === "LOADING") suffix = <Icon type="loading" />;
+
     return (
       <Input
         placeholder="Enter an IP, file hash, domain, or URL to add it"
         prefix={<Icon type="plus" />}
-        suffix={<Icon type="arrow-right" />}
+        suffix={suffix}
       />
     );
   }
