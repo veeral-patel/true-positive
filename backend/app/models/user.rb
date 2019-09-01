5d7eeefe49f0da70c03dbcd33f0351be81bd5e3e
Veeral Patel <vpatel98@gmail.com>
2019-08-31 21:17:11 -0700

    users must input an email

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true, presence: true
 
     def to_s
         self.username
