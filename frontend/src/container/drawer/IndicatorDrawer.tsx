bad6c7c3c44eefac3fc89c6b5409de007a05773c
Veeral Patel <vpatel98@gmail.com>
2019-09-01 18:19:43 -0700

    add error handling to IndicatorDrawer

--- frontend/src/container/drawer/IndicatorDrawer.tsx
+++ frontend/src/container/drawer/IndicatorDrawer.tsx
@@ -1,17 +1,35 @@
-import { Drawer } from "antd";
+import { Drawer, notification } from "antd";
 import { inject, observer } from "mobx-react";
 import React from "react";
+import ActiveCaseStore from "stores/ActiveCaseStore";
 import UIStore from "stores/UIStore";
+import ErrorP from "presentational/shared/errors/ErrorP";
 
 interface IndicatorDrawerProps {
   uiStore?: UIStore;
+  activeCaseStore?: ActiveCaseStore;
 }
 
-export default inject("uiStore")(
+export default inject("uiStore", "activeCaseStore")(
   observer(
     class IndicatorDrawer extends React.Component<IndicatorDrawerProps> {
       render() {
-        const { uiStore } = this.props;
+        const { uiStore, activeCaseStore } = this.props;
+        const taskId = uiStore!.indicatorDrawer.taskId;
+        const indicatorId = uiStore!.indicatorDrawer.indicatorId;
+
+        if (!taskId || !indicatorId) {
+          notification.error({
+            message: "Cannot fetch indicator",
+            description: "Either the task id or the indicator id is null"
+          })
+          return null;
+        }
+
+        const activeIndicator = activeCaseStore!.getIndicator(
+          taskId,
+          indicatorId
+        );
         return <Drawer visible={uiStore!.indicatorDrawer.status === "OPEN"} />;
       }
     }
