2d7ae5d26a6f6c0de833d6abf67da8cd6161e61a
Veeral Patel <vpatel98@gmail.com>
2019-09-01 22:32:15 -0700

    highlight indicator item on hover

--- frontend/src/presentational/shared/indicators/IndicatorListP.tsx
+++ frontend/src/presentational/shared/indicators/IndicatorListP.tsx
@@ -1,4 +1,5 @@
 import { Icon, List } from "antd";
+import "presentational/shared/indicators/IndicatorListP.css";
 import React from "react";
 import IIndicator from "ts/interfaces/IIndicator";
 
@@ -30,6 +31,7 @@ const IndicatorListP: React.FC<IndicatorListProps> = ({
       <List.Item
         actions={[<NumberOfComments count={indicator.comments.length} />]}
         onClick={() => openIndicatorDrawer(indicator.id)}
+        className="indicator_list_item"
       >
         {indicator.name}
       </List.Item>
