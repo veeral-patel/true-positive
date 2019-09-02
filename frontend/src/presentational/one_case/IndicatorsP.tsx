bb62a54ee3fc761a9d24c604603c4e8033d806be
Veeral Patel <vpatel98@gmail.com>
2019-09-01 23:38:09 -0700

    extract an indicator table component

--- frontend/src/presentational/one_case/IndicatorsP.tsx
+++ frontend/src/presentational/one_case/IndicatorsP.tsx
@@ -1,9 +1,7 @@
-import { Layout, Table } from "antd";
-import Column from "antd/lib/table/Column";
-import ListOfTagsP from "presentational/shared/tags/ListOfTagsP";
+import { Layout } from "antd";
+import IndicatorTableP from "presentational/shared/indicators/IndicatorTableP";
 import React from "react";
 import IIndicator from "ts/interfaces/IIndicator";
-import ITag from "ts/interfaces/ITag";
 const { Content } = Layout;
 
 interface IndicatorsProps {
@@ -20,28 +18,7 @@ const IndicatorsP: React.FC<IndicatorsProps> = ({ indicators }) => (
     }}
   >
     <h2>Indicators</h2>
-    <Table
-      dataSource={indicators}
-      rowKey={record => record.id.toString()}
-      pagination={{ hideOnSinglePage: true }}
-    >
-      <Column
-        title="Name"
-        dataIndex="name"
-        key="name"
-        sorter={(a: IIndicator, b: IIndicator) => a.name.localeCompare(b.name)}
-      />
-      <Column
-        title="Tags"
-        dataIndex="tags"
-        key="tags"
-        render={(tags: ITag[]) => (
-          <div style={{ lineHeight: 2.0 }}>
-            <ListOfTagsP tags={tags} />
-          </div>
-        )}
-      />
-    </Table>
+    <IndicatorTableP indicators={indicators} />
   </Content>
 );
 
