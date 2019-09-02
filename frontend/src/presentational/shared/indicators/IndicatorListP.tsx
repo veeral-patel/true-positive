cec91a9b3b9c147db48be851ba847bc87c9ccfd8
Veeral Patel <vpatel98@gmail.com>
2019-09-02 12:55:07 -0700

    scroll when list item overflows

--- frontend/src/presentational/shared/indicators/IndicatorListP.tsx
+++ frontend/src/presentational/shared/indicators/IndicatorListP.tsx
@@ -1,18 +1,7 @@
-import { Icon, List } from "antd";
+import { List } from "antd";
 import ListItemP from "presentational/shared/indicators/ListItemP";
 import React from "react";
 import IIndicator from "ts/interfaces/IIndicator";
-
-interface NumberofCommentsProps {
-  count: number;
-}
-
-const NumberOfComments: React.FC<NumberofCommentsProps> = ({ count }) => (
-  <span>
-    <Icon type="message" />
-    <span style={{ marginLeft: "5px" }}>{count}</span>
-  </span>
-);
 
 interface IndicatorListProps {
   indicators: IIndicator[];
--- frontend/src/presentational/shared/indicators/ListItemP.tsx
+++ frontend/src/presentational/shared/indicators/ListItemP.tsx
@@ -1,7 +1,20 @@
-import { List } from "antd";
+import { Icon, List, Typography } from "antd";
 import "presentational/shared/indicators/ListItem.css";
 import React from "react";
 import IIndicator from "ts/interfaces/IIndicator";
+
+const { Text } = Typography;
+
+interface NumberofCommentsProps {
+  count: number;
+}
+
+const NumberOfComments: React.FC<NumberofCommentsProps> = ({ count }) => (
+  <span>
+    <Icon type="message" />
+    <span style={{ marginLeft: "5px" }}>{count}</span>
+  </span>
+);
 
 interface ListItemProps {
   numberOfComments: number;
