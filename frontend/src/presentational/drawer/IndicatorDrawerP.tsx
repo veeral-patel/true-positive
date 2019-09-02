6387381ab53153e924464e7e5c62d78e4a5e8ceb
Veeral Patel <vpatel98@gmail.com>
2019-09-01 20:09:26 -0700

    render indicator description

--- frontend/src/presentational/drawer/IndicatorDrawerP.tsx
+++ frontend/src/presentational/drawer/IndicatorDrawerP.tsx
@@ -1,4 +1,5 @@
-import { Drawer } from "antd";
+import { Divider, Drawer } from "antd";
+import DescriptionP from "presentational/shared/description/DescriptionP";
 import React from "react";
 import IIndicator from "ts/interfaces/IIndicator";
 
@@ -13,8 +14,12 @@ const IndicatorDrawerP: React.FC<IndicatorDrawerProps> = ({
   visible,
   handleClose
 }) => (
-  <Drawer visible={visible} onClose={handleClose}>
-    <h4>{activeIndicator.name}</h4>
+  <Drawer visible={visible} onClose={handleClose} width={700}>
+    <div>
+      <h3>{activeIndicator.name}</h3>
+      <Divider orientation="left">Description</Divider>
+      <DescriptionP description={activeIndicator.description} />
+    </div>
   </Drawer>
 );
 
--- frontend/src/queries/getOneCase.ts
+++ frontend/src/queries/getOneCase.ts
@@ -67,6 +67,7 @@ const GET_ONE_CASE = gql`
         indicators {
           id
           name
+          description
           comments {
             comment
           }
