1828edefe0041334779b6b204a65bdb8d59be3e1
Veeral Patel <vpatel98@gmail.com>
2019-09-01 18:26:34 -0700

    reorganize_code

--- frontend/src/container/drawer/IndicatorDrawer.tsx
+++ frontend/src/container/drawer/IndicatorDrawer.tsx
@@ -3,7 +3,6 @@ import { inject, observer } from "mobx-react";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
 import UIStore from "stores/UIStore";
-import ErrorP from "presentational/shared/errors/ErrorP";
 
 interface IndicatorDrawerProps {
   uiStore?: UIStore;
@@ -15,22 +14,27 @@ export default inject("uiStore", "activeCaseStore")(
     class IndicatorDrawer extends React.Component<IndicatorDrawerProps> {
       render() {
         const { uiStore, activeCaseStore } = this.props;
-        const taskId = uiStore!.indicatorDrawer.taskId;
-        const indicatorId = uiStore!.indicatorDrawer.indicatorId;
+        if (uiStore!.indicatorDrawer.status === "OPEN") {
+          const taskId = uiStore!.indicatorDrawer.taskId;
+          const indicatorId = uiStore!.indicatorDrawer.indicatorId;
 
-        if (!taskId || !indicatorId) {
-          notification.error({
-            message: "Cannot fetch indicator",
-            description: "Either the task id or the indicator id is null"
-          })
-          return null;
+          if (!taskId || !indicatorId) {
+            notification.error({
+              message: "Cannot fetch indicator",
+              description: "Either the task id or the indicator id is null"
+            });
+
+            return null;
+          }
+
+          const activeIndicator = activeCaseStore!.getIndicator(
+            taskId,
+            indicatorId
+          );
+
+          return <Drawer visible={true} />;
         }
-
-        const activeIndicator = activeCaseStore!.getIndicator(
-          taskId,
-          indicatorId
-        );
-        return <Drawer visible={uiStore!.indicatorDrawer.status === "OPEN"} />;
+        return null;
       }
     }
   )
