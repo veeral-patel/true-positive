4af082ea7633ab7349ce1bc5506433d805c1dcae
Veeral Patel <vpatel98@gmail.com>
2019-09-02 16:15:44 -0700

    can now delete statuses

---
+++ backend/app/graphql/mutations/delete_status.rb
@@ -0,0 +1,18 @@
+class Mutations::DeleteStatus < Mutations::BaseMutation
+    argument :id, ID, required: true
+
+    # ID of the deleted status 
+    field :id, ID, null: false
+
+    def resolve(id:)
+        status = find_status_or_throw_execution_error(status_id: id)
+
+        if status.destroy
+            {
+                "id": id
+            }
+        else
+            raise GraphQL::ExecutionError, status.errors.full_messages.join(" | ")
+        end
+    end
+end
--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,8 +2,12 @@ module Types
   class MutationType < Types::BaseObject
       description "Mutations are used for creating, updating, or deleting data."
 
-      field :create_status, mutation: Mutations::CreateStatus
       field :create_priority, mutation: Mutations::CreatePriority
       field :create_case, mutation: Mutations::CreateCase
+
+      # status ----------
+      field :create_status, mutation: Mutations::CreateStatus
+      field :delete_status, mutation: Mutations::DeleteStatus
+
   end
 end
