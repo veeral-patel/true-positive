d3f2f9a8da66dd8301570012aca8f65d0613ffeb
Veeral Patel <vpatel98@gmail.com>
2019-09-02 19:26:42 -0700

    add renameCase mutation to our GraphQL API

---
+++ backend/app/graphql/mutations/rename_case.rb
@@ -0,0 +1,22 @@
+class Mutations::RenameCase < Mutations::BaseMutation
+    argument :id, ID, required: true
+    argument :name, String, required: true # new case name
+
+    # updated case
+    field :case, Types::CaseType, null: false
+
+    def resolve(id:, name:)
+        # find and update the case
+        thecase = find_case_or_throw_execution_error(case_id: id)
+        thecase.name = name
+
+        # and save it
+        if thecase.save
+            {
+                "case": thecase
+            }
+        else
+            raise GraphQL::ExecutionError, thecase.errors.full_messages.join(" | ")
+        end
+    end
+end
--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,7 +2,9 @@ module Types
   class MutationType < Types::BaseObject
       description "Mutations are used for creating, updating, or deleting data."
 
+      # case ------------
       field :create_case, mutation: Mutations::CreateCase
+      field :rename_case, mutation: Mutations::RenameCase
 
       # status ----------
       field :create_status, mutation: Mutations::CreateStatus
