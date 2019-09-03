fb506043929347f09b054c8f9e629bbd64a6a1c4
Veeral Patel <vpatel98@gmail.com>
2019-09-02 17:36:36 -0700

    remove edit priority button

--- frontend/src/presentational/admin/ListofPrioritiesP.tsx
+++ frontend/src/presentational/admin/ListofPrioritiesP.tsx
@@ -1,6 +1,8 @@
-import { List } from "antd";
+import { List, Typography } from "antd";
 import React from "react";
 import IPriority from "ts/interfaces/IPriority";
+
+const { Text } = Typography;
 
 interface IListofPrioritiesProps {
   priorities: IPriority[];
@@ -14,10 +16,8 @@ const ListofPrioritiesP: React.FC<IListofPrioritiesProps> = ({
       itemLayout="horizontal"
       dataSource={priorities}
       renderItem={priority => (
-        <List.Item
-          actions={[<a>Rename</a>, <a style={{ color: "red" }}>Remove</a>]}
-        >
-          <List.Item.Meta title={priority.name} />
+        <List.Item actions={[<a style={{ color: "red" }}>Remove</a>]}>
+          <List.Item.Meta title={<Text editable>{priority.name}</Text>} />
         </List.Item>
       )}
     />
