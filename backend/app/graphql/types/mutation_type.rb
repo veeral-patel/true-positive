a75d530d93284130aef7663ca37df5ca2337b20e
Veeral Patel <vpatel98@gmail.com>
2019-09-02 17:14:25 -0700

    move around code

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,12 +2,13 @@ module Types
   class MutationType < Types::BaseObject
       description "Mutations are used for creating, updating, or deleting data."
 
-      field :create_priority, mutation: Mutations::CreatePriority
       field :create_case, mutation: Mutations::CreateCase
 
       # status ----------
       field :create_status, mutation: Mutations::CreateStatus
       field :delete_status, mutation: Mutations::DeleteStatus
 
+      # priority --------
+      field :create_priority, mutation: Mutations::CreatePriority
   end
 end
