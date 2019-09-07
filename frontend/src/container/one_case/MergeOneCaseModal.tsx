942d19a551891b96111e9f64993b29e13a070b3c
Veeral Patel <vpatel98@gmail.com>
2019-09-07 14:51:08 -0700

    add helper text to the MergeOneCaseModal component

--- frontend/src/container/one_case/MergeOneCaseModal.tsx
+++ frontend/src/container/one_case/MergeOneCaseModal.tsx
@@ -1,7 +1,9 @@
-import { Modal } from "antd";
+import { Alert, Modal, Typography } from "antd";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import UIStore from "stores/UIStore";
+
+const { Paragraph, Text } = Typography;
 
 interface MergeOneCaseModalProps {
   uiStore?: UIStore;
@@ -18,7 +20,17 @@ export default inject("uiStore")(
             visible={uiStore!.openModal === "MERGE_ONE_CASE_MODAL"}
             onCancel={() => uiStore!.closeModal()}
             okText="Merge"
-          />
+          >
+            <Alert
+              type="info"
+              showIcon
+              message="Merging this case will simply mark this case as merged. It will
+              not modify this case, its indicators, or its tasks at all."
+            />
+            <div style={{ marginTop: "8px" }}>
+              <Paragraph>Choose a case to merge this case into:</Paragraph>
+            </div>
+          </Modal>
         );
       }
     }
