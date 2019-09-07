77de543fd53cc4fc0fc1cb6ebb0d6b2260a75306
Veeral Patel <vpatel98@gmail.com>
2019-09-07 11:30:51 -0700

    can filter priorities

--- frontend/src/container/shared/priorities/ListAndFilterPriorities.tsx
+++ frontend/src/container/shared/priorities/ListAndFilterPriorities.tsx
@@ -9,15 +9,25 @@ interface ListAndFilterPrioritiesProps {
   priorityStore?: PriorityStore;
 }
 
+interface ListAndFilterPrioritiesState {
+  filterValue: string;
+}
+
 export default inject("priorityStore")(
   observer(
     class ListAndFilterPriorities extends React.Component<
-      ListAndFilterPrioritiesProps
+      ListAndFilterPrioritiesProps,
+      ListAndFilterPrioritiesState
     > {
+      state = {
+        filterValue: ""
+      };
+
       render() {
         const { priorityStore } = this.props;
         const prioritiesAreLoading = priorityStore!.prioritiesAreLoading;
         const priorities = priorityStore!.priorities;
+        const { filterValue } = this.state;
 
         return (
           <div>
@@ -25,6 +35,10 @@ export default inject("priorityStore")(
               <Input
                 placeholder="Filter priorities"
                 prefix={<Icon type="search" />}
+                value={filterValue}
+                onChange={event =>
+                  this.setState({ filterValue: event.currentTarget.value })
+                }
               />
             </div>
 
@@ -33,7 +47,12 @@ export default inject("priorityStore")(
             ) : (
               <div style={{ maxHeight: "20vh", overflow: "scroll" }}>
                 <List<IPriority>
-                  dataSource={priorities}
+                  dataSource={priorities.filter(
+                    priority =>
+                      priority.name
+                        .toLowerCase()
+                        .indexOf(filterValue.toLowerCase()) !== -1
+                  )}
                   renderItem={priority => (
                     <List.Item
                       className="hoverable_item"
