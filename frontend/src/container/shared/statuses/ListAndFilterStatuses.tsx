c0d6c3c8e8c350936f51b93a0b26ac619757fc00
Veeral Patel <vpatel98@gmail.com>
2019-09-07 02:06:29 -0700

    restrict height of our list of statuses

--- frontend/src/container/shared/statuses/ListAndFilterStatuses.tsx
+++ frontend/src/container/shared/statuses/ListAndFilterStatuses.tsx
@@ -1,7 +1,8 @@
-import { Input } from "antd";
+import { Input, List, Spin } from "antd";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import StatusStore from "stores/StatusStore";
+import IStatus from "ts/interfaces/IStatus";
 
 interface ListAndFilterStatusesProps {
   statusStore?: StatusStore;
@@ -13,9 +14,26 @@ export default inject("statusStore")(
       ListAndFilterStatusesProps
     > {
       render() {
+        const { statusStore } = this.props;
+        const statusesAreLoading = statusStore!.statusesAreLoading;
+        const statuses = statusStore!.statuses;
+
         return (
           <div>
-            <Input placeholder="Filter statuses" />
+            <div style={{ marginBottom: "10px" }}>
+              <Input placeholder="Filter statuses" />
+            </div>
+
+            {statusesAreLoading ? (
+              <Spin />
+            ) : (
+              <div style={{ height: "20vh", overflow: "scroll" }}>
+                <List<IStatus>
+                  dataSource={statuses}
+                  renderItem={status => <List.Item>{status.name}</List.Item>}
+                />
+              </div>
+            )}
           </div>
         );
       }
