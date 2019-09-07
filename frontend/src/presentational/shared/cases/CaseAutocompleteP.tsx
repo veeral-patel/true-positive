92f28a8cd1fb2798c6f9142c85952080d2538230
Veeral Patel <vpatel98@gmail.com>
2019-09-07 15:26:32 -0700

    add creation time to case autocomplete

--- frontend/src/presentational/shared/cases/CaseAutocompleteP.tsx
+++ frontend/src/presentational/shared/cases/CaseAutocompleteP.tsx
@@ -1,6 +1,7 @@
 import { AutoComplete, Icon, Input } from "antd";
 import React from "react";
 import ICase from "ts/interfaces/ICase";
+import { formatDateOnly } from "utils/formatISO8601";
 const { Option } = AutoComplete;
 
 interface CaseAutocompleteProps {
@@ -9,7 +10,12 @@ interface CaseAutocompleteProps {
 
 const CaseAutocompleteP: React.FC<CaseAutocompleteProps> = ({ cases }) => {
   const options = cases.map(thecase => (
-    <Option key={thecase.id}>{thecase.name}</Option>
+    <Option key={thecase.id}>
+      {thecase.name}
+      <span style={{ position: "absolute", right: "16px" }}>
+        Created {formatDateOnly(thecase.createdAt)}
+      </span>
+    </Option>
   ));
 
   return (
--- frontend/src/utils/formatISO8601.ts
+++ frontend/src/utils/formatISO8601.ts
@@ -4,4 +4,9 @@ function formatISO8601(timestamp: string): string {
   return moment.utc(timestamp).format("MM/DD/YY HH:mm");
 }
 
+function formatDateOnly(timestamp: string): string {
+  return moment.utc(timestamp).format("MM/DD/YY");
+}
+
 export default formatISO8601;
+export { formatDateOnly };
