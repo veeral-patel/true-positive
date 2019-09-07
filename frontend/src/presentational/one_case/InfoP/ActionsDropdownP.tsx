5d4154832e84f0c85dc3cf0fee013cc9c8e15d7e
Veeral Patel <vpatel98@gmail.com>
2019-09-07 14:10:31 -0700

    add merge button

--- frontend/src/presentational/one_case/InfoP/ActionsDropdownP.tsx
+++ frontend/src/presentational/one_case/InfoP/ActionsDropdownP.tsx
@@ -1,9 +1,7 @@
 import { Button, Dropdown, Icon, Menu } from "antd";
 import { ClickParam } from "antd/lib/menu";
 import React from "react";
-import { DELETE_CASE } from "utils/constants";
-
-// ----
+import { DELETE_CASE, MERGE_CASE } from "utils/constants";
 
 interface ActionsDropdownProps {
   handleMenuClick: (click: ClickParam) => void;
@@ -13,12 +11,10 @@ const ActionsDropdownP: React.FC<ActionsDropdownProps> = ({
   handleMenuClick
 }) => {
   const menu = (
-    <Menu>
-      <Menu.Item
-        key={DELETE_CASE}
-        style={{ color: "red" }}
-        onClick={handleMenuClick}
-      >
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key={MERGE_CASE}>Merge</Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key={DELETE_CASE} style={{ color: "red" }}>
         Delete Case
       </Menu.Item>
     </Menu>
