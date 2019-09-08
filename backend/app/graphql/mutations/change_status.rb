7289ef6b667134e727583a7950149eece010a7c7
Veeral Patel <vpatel98@gmail.com>
2019-09-07 21:10:22 -0700

    can update the status of a case

--- backend/app/graphql/mutations/change_status.rb
+++ backend/app/graphql/mutations/change_status.rb
@@ -5,9 +5,25 @@ class Mutations::ChangeStatus < Mutations::BaseMutation
     # the ID of the new status
     argument :status_id, ID, required: true
 
+    # type of the object we're updating (either case or task)
+    argument :type, Types::HasStatusEnum, required: true
+
     # the ID of the updated case or task
     field :id, ID, null: false
 
-    def resolve(object_id:, status_id:)
+    def resolve(object_id:, status_id:, type:)
+        new_status =  find_status_or_throw_execution_error(status_id: status_id)
+        if type === "CASE"
+            the_case = find_case_or_throw_execution_error(case_id: object_id)
+            the_case.status = new_status
+
+            if the_case.save
+                {
+                    "id": object_id
+                }
+            else
+                raise GraphQL::ExecutionError, the_case.errors.full_messages.join(" | ")
+            end
+        end
     end
 end
---
+++ backend/app/graphql/types/has_status_enum.rb
@@ -0,0 +1,4 @@
+class Types::HasStatusEnum < Types::BaseEnum
+    value "CASE"
+    value "TASK"
+end
