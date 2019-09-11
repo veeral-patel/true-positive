11efa237bc8808425fb198d2881e0781d990ccbe
Veeral Patel <vpatel98@gmail.com>
2019-09-10 20:48:04 -0700

    install mobx-state-tree root store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,21 @@
 import "antd/dist/antd.css";
 import App from "App";
 import { Provider } from "mobx-react";
+import { RootStore, StoreContext } from "models";
+import { createHttpClient } from "mst-gql";
 import React from "react";
 import ReactDOM from "react-dom";
 import "react-quill/dist/quill.snow.css";
 import store from "stores";
 import * as serviceWorker from "./serviceWorker";
+
+const gqlHttpClient = createHttpClient(
+  `${process.env.REACT_APP_API_ENDPOINT}/graphql`
+);
+
+const rootStore = RootStore.create(undefined, {
+  gqlHttpClient
+});
 
 ReactDOM.render(
   <Provider
@@ -17,7 +27,9 @@ ReactDOM.render(
     userStore={store.userStore}
     tagStore={store.tagStore}
   >
-    <App />
+    <StoreContext.Provider value={rootStore}>
+      <App />
+    </StoreContext.Provider>
   </Provider>,
   document.getElementById("root")
 );
