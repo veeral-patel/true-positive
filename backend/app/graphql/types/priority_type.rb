6839b4cdd02e14feb1417295bf49db5e93ed3317
Veeral Patel <vpatel98@gmail.com>
2019-09-14 22:34:07 -0700

    annotate priority

--- backend/app/graphql/types/priority_type.rb
+++ backend/app/graphql/types/priority_type.rb
@@ -2,8 +2,16 @@ module Types
     class PriorityType < Types::BaseObject
         description "Describes the severity level of a case or task."
 
-        field :id, ID, null: false
-        field :name, String, null: false
-        field :description, String, null: true
+        field :id, ID, null: false do
+            description "An unique integer identifying this priority."
+        end
+
+        field :name, String, null: false do
+            description "This priority's name (such as 'Critical')."
+        end
+
+        field :description, String, null: true do
+            description "Some optional text describing this priority."
+        end
     end
 end
