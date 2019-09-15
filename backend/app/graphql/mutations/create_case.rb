4c089d2dbcd95a906ff66487f303df2dcddfcb00
Veeral Patel <vpatel98@gmail.com>
2019-09-15 11:31:58 -0700

    annotate create case

--- backend/app/graphql/mutations/create_case.rb
+++ backend/app/graphql/mutations/create_case.rb
@@ -3,15 +3,37 @@ require_relative '../helpers'
 class Mutations::CreateCase < Mutations::BaseMutation
     description "Creates a new case."
 
-    argument :name, String, required: true
-    argument :created_by_id, ID, required: true
-    argument :status_id, ID, required: true
-    argument :priority_id, ID, required: true
-    argument :description, String, required: false
-    argument :assigned_to_id, ID, required: false
-    argument :tags, [String], required: false
+    argument :name, String, required: true do
+        description "The name of the new case."
+    end
 
-    field :case, Types::CaseType, null: false
+    argument :created_by_id, ID, required: true do
+        description "The ID (not the username) of the user who created this case."
+    end
+
+    argument :status_id, ID, required: true do
+        description "The ID of this case's status."
+    end
+
+    argument :priority_id, ID, required: true do
+        description "The ID of this case's priority."
+    end
+
+    argument :description, String, required: false do
+        description "Optional text describing this case."
+    end
+
+    argument :assigned_to_id, ID, required: false do
+        description "The ID of the user who this case is assigned to. Optional."
+    end
+
+    argument :tags, [String], required: false do
+        description "A optional list of tags to add to this case."
+    end
+
+    field :case, Types::CaseType, null: false do
+        description "The newly created case."
+    end
 
     def resolve(name:, created_by_id:, status_id:, priority_id:, description: nil, assigned_to_id: nil, tags: nil)
         status = find_status_or_throw_execution_error(status_id: status_id)
