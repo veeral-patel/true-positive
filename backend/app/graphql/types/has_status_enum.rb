e1dc3867399108dd5d3ff1e6a27c23a879a70add
Veeral Patel <vpatel98@gmail.com>
2019-09-15 11:38:01 -0700

    annotate has_status_enum

--- backend/app/graphql/types/has_status_enum.rb
+++ backend/app/graphql/types/has_status_enum.rb
@@ -1,4 +1,11 @@
 class Types::HasStatusEnum < Types::BaseEnum
-    value "CASE"
-    value "TASK"
+    description "Lists the types of objects that have a status."
+
+    value "CASE" do
+        description "A case."
+    end
+
+    value "TASK" do
+        description "A task."
+    end
 end
