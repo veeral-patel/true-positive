5b15adbb52c5b81b3fb7ee6f6049cc8ea372d943
Veeral Patel <vpatel98@gmail.com>
2019-09-14 22:54:18 -0700

    annotate user

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -2,8 +2,16 @@ module Types
     class UserType < Types::BaseObject
         description "Represents an user"
 
-        field :id, ID, null: false
-        field :username, String, null: false
-        field :email, String, null: false
+        field :id, ID, null: false do
+            description "An unique integer identifying this user."
+        end
+
+        field :username, String, null: false do
+            description "The user's unique username."
+        end
+
+        field :email, String, null: false do
+            description "The user's email address."
+        end
     end
 end
