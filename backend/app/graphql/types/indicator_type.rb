fd050459c4aaaf60b54a0d2d240b768120a2eed7
Veeral Patel <vpatel98@gmail.com>
2019-09-14 23:26:04 -0700

    annotate indicator

--- backend/app/graphql/types/indicator_type.rb
+++ backend/app/graphql/types/indicator_type.rb
@@ -7,21 +7,27 @@ module Types
         end
 
         field :name, String, null: false do
+            description "The file hash, IP address, domain name, etc. this indicator describes."
         end
 
         field :description, String, null: true do
+            description "This indicator's description."
         end
 
         field :created_at, GraphQL::Types::ISO8601DateTime, null: false do
+            description "When this indicator was created (in ISO8601 format)."
         end
 
         field :created_by, Types::UserType, null: false do
+            description "The user who created this indicator."
         end
 
         field :comments, [Types::CommentType], null: false do
+            description "The comments on this indicator."
         end
 
         field :tags, [Types::TagType], null: false do
+            description "This indicator's tags."
         end
     end
 end
