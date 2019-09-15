be0605ddbf30b478c02cdac974fa84b6ba69a4da
Veeral Patel <vpatel98@gmail.com>
2019-09-14 23:22:10 -0700

    start annotating indicators

--- backend/app/graphql/types/indicator_type.rb
+++ backend/app/graphql/types/indicator_type.rb
@@ -2,12 +2,26 @@ module Types
     class IndicatorType < Types::BaseObject
         description "A file hash, IP address, domain name, or another indicator of compromise."
 
-        field :id, ID, null: false
-        field :name, String, null: false
-        field :description, String, null: true
-        field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-        field :created_by, Types::UserType, null: false
-        field :comments, [Types::CommentType], null: false
-        field :tags, [Types::TagType], null: false
+        field :id, ID, null: false do
+            description "An unique integer identifying this indicator."
+        end
+
+        field :name, String, null: false do
+        end
+
+        field :description, String, null: true do
+        end
+
+        field :created_at, GraphQL::Types::ISO8601DateTime, null: false do
+        end
+
+        field :created_by, Types::UserType, null: false do
+        end
+
+        field :comments, [Types::CommentType], null: false do
+        end
+
+        field :tags, [Types::TagType], null: false do
+        end
     end
 end
