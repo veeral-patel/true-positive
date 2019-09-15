96bac10bdf66496e5cc1904bf3352754c1b12da8
Veeral Patel <vpatel98@gmail.com>
2019-09-15 00:49:13 -0700

    annotate comment type

--- backend/app/graphql/types/comment_type.rb
+++ backend/app/graphql/types/comment_type.rb
@@ -2,10 +2,24 @@ module Types
     class CommentType < Types::BaseObject
         description "Represents a comment on a case, task, indicator, or another object."
 
-        field :id, ID, null: false
-        field :comment, String, null: false
-        field :created_by, Types::UserType, null: false
-        field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-        field :formatted_created_at, String, null: false
+        field :id, ID, null: false do
+            description "An unique integer identifying this comment."
+        end
+
+        field :comment, String, null: false do
+            description "The comment itself."
+        end
+
+        field :created_by, Types::UserType, null: false do
+            description "The user who wrote this comment."
+        end
+
+        field :created_at, GraphQL::Types::ISO8601DateTime, null: false do
+            description "When this comment was created (in ISO8601)"
+        end
+
+        field :formatted_created_at, String, null: false do
+            description "When this comment was created (nicely formatted)"
+        end
     end
 end
