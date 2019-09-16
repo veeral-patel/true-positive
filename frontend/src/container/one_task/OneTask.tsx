9d382ea5abff2289dc42adcf1de4bb84ea39d50c
Veeral Patel <vpatel98@gmail.com>
2019-09-15 19:10:00 -0700

    combine OneTaskP into OneTask

--- frontend/src/container/one_task/OneTask.tsx
+++ frontend/src/container/one_task/OneTask.tsx
@@ -1,10 +1,27 @@
-import { RouteComponentProps } from "@reach/router";
-import { notification } from "antd";
+import { navigate, RouteComponentProps } from "@reach/router";
+import {
+  Avatar,
+  Comment,
+  Divider,
+  Layout,
+  notification,
+  PageHeader,
+  Typography
+} from "antd";
+import ActionsDropdown from "container/one_task/ActionsDropdown";
 import { inject, observer } from "mobx-react";
-import OneTaskP from "presentational/one_task/OneTaskP";
+import AddCommentFormP from "presentational/one_case/InfoP/AddCommentFormP";
+import DetailsP from "presentational/one_task/DetailsSectionP";
+import CommentListP from "presentational/shared/comments/CommentListP";
+import DescriptionP from "presentational/shared/description/DescriptionP";
 import ErrorP from "presentational/shared/errors/ErrorP";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
+import { getPathToCaseTasks } from "utils/pathHelpers";
+import sortCommentsByCreatedAt from "utils/sortCommentsByCreatedAt";
+
+const { Content } = Layout;
+const { Text } = Typography;
 
 interface OneTaskProps extends RouteComponentProps {
   activeCaseStore?: ActiveCaseStore;
@@ -51,13 +68,62 @@ export default inject("activeCaseStore")(
         // above, in a HOC
         if (activeCase)
           return (
-            <OneTaskP
-              activeTask={activeTask}
-              activeCase={activeCase}
-              renameTask={activeCaseStore!.renameTask}
-              changeTaskStatus={activeCaseStore!.changeTaskStatus}
-              changeTaskPriority={activeCaseStore!.changeTaskPriority}
-            />
+            <Content
+              style={{
+                backgroundColor: "#fff",
+                padding: 24,
+                height: "100%"
+              }}
+            >
+              <PageHeader
+                onBack={() => navigate(getPathToCaseTasks(activeCase.id))}
+                title={
+                  <Text
+                    style={{ fontSize: 18, paddingTop: "5px" }}
+                    editable={{
+                      onChange: (newText: string) =>
+                        activeCaseStore!.renameTask(activeTask.id, newText)
+                    }}
+                  >
+                    {activeTask.name}
+                  </Text>
+                }
+                extra={
+                  <ActionsDropdown
+                    taskId={activeTask.id}
+                    caseId={activeCase.id}
+                  />
+                }
+              />
+
+              <DetailsP
+                activeTask={activeTask}
+                changeTaskStatus={activeCaseStore!.changeTaskStatus}
+                changeTaskPriority={activeCaseStore!.changeTaskPriority}
+              />
+
+              <section>
+                <Divider orientation="left">Description</Divider>
+                <DescriptionP description={activeTask.description} />
+              </section>
+
+              <section>
+                <Divider orientation="left">
+                  Comments ({activeTask.comments.length})
+                </Divider>
+                {activeTask.comments.length > 0 && (
+                  <CommentListP
+                    comments={sortCommentsByCreatedAt(activeTask.comments)}
+                  />
+                )}
+                <div style={{ width: "70%" }}>
+                  <Comment
+                    content={<AddCommentFormP />}
+                    avatar={<Avatar icon="user" />}
+                  />
+                </div>
+              </section>
+            </Content>
           );
       }
     }
