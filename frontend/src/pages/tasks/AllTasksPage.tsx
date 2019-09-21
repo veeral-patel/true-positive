116343a2c0453b791466fcd032b97168d339857f
Veeral Patel <vpatel98@gmail.com>
2019-09-21 12:48:18 -0700

    add filter input to all tasks page

--- frontend/src/pages/tasks/AllTasksPage.tsx
+++ frontend/src/pages/tasks/AllTasksPage.tsx
@@ -1,5 +1,5 @@
 import { RouteComponentProps } from "@reach/router";
-import { Typography } from "antd";
+import { Divider, Icon, Input, Radio, Typography } from "antd";
 import React from "react";
 const { Paragraph } = Typography;
 
@@ -9,10 +9,32 @@ class AllTasksPage extends React.Component<Props> {
   render() {
     return (
       <div>
-        <h2>Tasks</h2>
-        <Paragraph>
-          These are all the tasks in all the cases you're a member of.
-        </Paragraph>
+        <section>
+          <h2>Tasks</h2>
+          <Paragraph>
+            These are all the tasks from the cases you're a member of.
+          </Paragraph>
+        </section>
+        <section>
+          <span
+            style={{
+              marginBottom: "30px",
+              width: "40%",
+              display: "inline-block"
+            }}
+          >
+            <Input placeholder="Filter tasks" prefix={<Icon type="search" />} />
+          </span>
+          <span style={{ marginLeft: "8px", marginRight: "8px" }}>
+            <Divider type="vertical" />
+          </span>
+          <span>
+            <Radio.Group defaultValue="assigned">
+              <Radio.Button value="assigned">Assigned</Radio.Button>
+              <Radio.Button value="all">All</Radio.Button>
+            </Radio.Group>
+          </span>
+        </section>
       </div>
     );
   }
