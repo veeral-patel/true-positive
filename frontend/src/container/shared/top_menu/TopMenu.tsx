1c67ea66a829fa961c6e20b483002824804b8261
Veeral Patel <vpatel98@gmail.com>
2019-09-21 12:27:54 -0700

    add tasks to the top menu (and refactor the top menu)

--- frontend/src/container/shared/top_menu/TopMenu.tsx
+++ frontend/src/container/shared/top_menu/TopMenu.tsx
@@ -1,7 +1,10 @@
+import { navigate } from "@reach/router";
+import { Icon, Menu } from "antd";
+import SubMenu from "antd/lib/menu/SubMenu";
 import { inject, observer } from "mobx-react";
-import TopMenuP from "presentational/shared/top_menu/TopMenuP";
 import React from "react";
 import UIStore from "stores/UIStore";
+import { paths } from "utils/constants";
 
 interface TopMenuProps {
   uiStore?: UIStore;
@@ -12,7 +15,35 @@ export default inject("uiStore")(
     class TopMenu extends React.Component<TopMenuProps> {
       render() {
         const { uiStore } = this.props;
-        return <TopMenuP openCreateCaseModal={uiStore!.openCreateCaseModal} />;
+        return (
+          <Menu theme="dark" mode="horizontal" selectedKeys={[]}>
+            <Menu.Item
+              onClick={() => navigate(paths.CASES_PATH)}
+              style={{ float: "left" }}
+            >
+              Cases
+            </Menu.Item>
+            <Menu.Item
+              onClick={() => navigate(paths.ADMIN_PATH)}
+              style={{ float: "left" }}
+            >
+              Admin
+            </Menu.Item>
+            <Menu.Item style={{ float: "left" }}>Tasks</Menu.Item>
+            <SubMenu
+              title={
+                <span>
+                  New <Icon type="down" />
+                </span>
+              }
+              style={{ float: "right" }}
+            >
+              <Menu.Item onClick={() => uiStore!.openCreateCaseModal()}>
+                Case
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        );
       }
     }
   )
