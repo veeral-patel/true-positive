22aef03d11e4f43646c2d5df97f794d735a2c832
Veeral Patel <vpatel98@gmail.com>
2019-09-20 19:52:04 -0700

    complete getActiveSiderItem

--- frontend/src/utils/getActiveSiderItem.ts
+++ frontend/src/utils/getActiveSiderItem.ts
@@ -1,10 +1,22 @@
-import { CASE_SIDER_INFO, CASE_SIDER_TASKS } from "utils/constants";
+import {
+  CASE_SIDER_INDICATORS,
+  CASE_SIDER_INFO,
+  CASE_SIDER_MEMBERS,
+  CASE_SIDER_TASKS,
+  CASE_SIDER_TREE
+} from "utils/constants";
 
 function getActiveSiderItem(path: string): string[] {
   if (path.endsWith("/info") || path.endsWith("/info/")) {
     return [CASE_SIDER_INFO];
   } else if (path.endsWith("/tasks") || path.endsWith("/tasks/")) {
     return [CASE_SIDER_TASKS];
+  } else if (path.endsWith("/indicators") || path.endsWith("/indicators/")) {
+    return [CASE_SIDER_INDICATORS];
+  } else if (path.endsWith("/tree") || path.endsWith("/tree/")) {
+    return [CASE_SIDER_TREE];
+  } else if (path.endsWith("/members") || path.endsWith("/members/")) {
+    return [CASE_SIDER_MEMBERS];
   }
   return [];
 }
