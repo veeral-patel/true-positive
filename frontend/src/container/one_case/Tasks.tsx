e62aa48b92d67726b84abb0631858f8730f31e6e
Veeral Patel <vpatel98@gmail.com>
2019-09-21 12:20:18 -0700

    describe what a task is

--- frontend/src/container/one_case/Tasks.tsx
+++ frontend/src/container/one_case/Tasks.tsx
@@ -1,11 +1,13 @@
 import { navigate, RouteComponentProps } from "@reach/router";
-import { Icon, Input, Layout } from "antd";
+import { Icon, Input, Layout, Typography } from "antd";
 import TasksTable from "container/one_case/TasksTable";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
 import { getPathToATask } from "utils/pathHelpers";
+
 const { Content } = Layout;
+const { Paragraph } = Typography;
 
 interface TaskProps extends RouteComponentProps {
   activeCaseStore?: ActiveCaseStore;
@@ -32,9 +34,13 @@ export default inject("activeCaseStore")(
             >
               <div>
                 <h2>Tasks ({activeCase.tasks.length})</h2>
+                <Paragraph>
+                  Create a task for every piece of work to be completed in a
+                  case.
+                </Paragraph>
               </div>
 
-              <div style={{ marginTop: "30px", marginBottom: "30px" }}>
+              <div style={{ marginBottom: "20px" }}>
                 <Input
                   placeholder="Create a task"
                   prefix={<Icon type="plus" />}
