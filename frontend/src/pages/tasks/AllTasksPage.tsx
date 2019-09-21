477faede7e29d8904b93ff880700df0147d35cd7
Veeral Patel <vpatel98@gmail.com>
2019-09-21 12:40:33 -0700

    add copy

--- frontend/src/pages/tasks/AllTasksPage.tsx
+++ frontend/src/pages/tasks/AllTasksPage.tsx
@@ -1,11 +1,20 @@
 import { RouteComponentProps } from "@reach/router";
+import { Typography } from "antd";
 import React from "react";
+const { Paragraph } = Typography;
 
 interface Props extends RouteComponentProps {}
 
 class AllTasksPage extends React.Component<Props> {
   render() {
-    return <h2>Tasks</h2>;
+    return (
+      <div>
+        <h2>Tasks</h2>
+        <Paragraph>
+          These are all the tasks in all the cases you're a member of.
+        </Paragraph>
+      </div>
+    );
   }
 }
 
