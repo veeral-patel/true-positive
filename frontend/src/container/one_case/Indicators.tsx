dcfa09526d36e5f466b1fe346d9f7dbc21817a04
Veeral Patel <vpatel98@gmail.com>
2019-09-21 17:14:29 -0700

    add options for adding indicators

--- frontend/src/container/one_case/Indicators.tsx
+++ frontend/src/container/one_case/Indicators.tsx
@@ -1,8 +1,11 @@
 import { RouteComponentProps } from "@reach/router";
+import { Button, Divider, Layout } from "antd";
 import { inject, observer } from "mobx-react";
-import IndicatorsP from "presentational/one_case/IndicatorsP";
+import IndicatorInputP from "presentational/shared/indicators/IndicatorInputP";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
+
+const { Content } = Layout;
 
 interface IndicatorsProps extends RouteComponentProps {
   activeCaseStore?: ActiveCaseStore;
@@ -17,7 +20,33 @@ export default inject("activeCaseStore")(
 
         // should always render, since we're catching errors and showing spinner above this component
         if (activeCase)
-          return <IndicatorsP indicators={activeCase.indicators} />;
+          return (
+            <Content
+              style={{
+                background: "#fff",
+                padding: 24,
+                margin: 0,
+                minHeight: 280
+              }}
+            >
+              <section>
+                <h2>Indicators ({activeCase.indicators.length})</h2>
+              </section>
+              <section>
+                <div style={{ float: "right", marginBottom: "6px" }}>
+                  <Button type="link" style={{ paddingLeft: 0 }}>
+                    Add file indicator
+                  </Button>
+                  <Button type="link">Add textual indicator</Button>
+                  <Divider type="vertical" />
+                  <Button type="link">Bulk import</Button>
+                </div>
+                <div>
+                  <IndicatorInputP />
+                </div>
+              </section>
+            </Content>
+          );
       }
     }
   )
