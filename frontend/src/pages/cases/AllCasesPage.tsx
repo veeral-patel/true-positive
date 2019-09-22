3854a3474b7f732895f3906f629d86e7a67dab52
Veeral Patel <vpatel98@gmail.com>
2019-09-22 12:53:47 -0700

    add empty state to Cases

--- frontend/src/pages/cases/AllCasesPage.tsx
+++ frontend/src/pages/cases/AllCasesPage.tsx
@@ -1,20 +1,22 @@
 import { RouteComponentProps } from "@reach/router";
-import { Button, Divider, Radio } from "antd";
+import { Button, Divider, Empty, Radio } from "antd";
 import CasesTable from "container/all_cases/CasesTable";
 import FilterInput from "container/all_cases/FilterInput";
 import { inject, observer } from "mobx-react";
 import React from "react";
+import AllCasesStore from "stores/AllCasesStore";
 import UIStore from "stores/UIStore";
 
 interface IAllCasesPageProps extends RouteComponentProps {
   uiStore?: UIStore;
+  allCasesStore?: AllCasesStore;
 }
 
 export default inject("allCasesStore", "uiStore")(
   observer(
     class AllCasesPage extends React.Component<IAllCasesPageProps> {
       render() {
-        const { uiStore } = this.props;
+        const { uiStore, allCasesStore } = this.props;
         return (
           <div>
             <div>
@@ -30,29 +32,47 @@ export default inject("allCasesStore", "uiStore")(
                 </Button>
               </span>
             </div>
-            <div>
-              <span
-                style={{
-                  marginBottom: "30px",
-                  width: "40%",
-                  display: "inline-block"
-                }}
-              >
-                <FilterInput />
-              </span>
-              <span style={{ marginLeft: "8px", marginRight: "8px" }}>
-                <Divider type="vertical" />
-              </span>
-              <span>
-                <Radio.Group defaultValue="assigned">
-                  <Radio.Button value="assigned">Assigned</Radio.Button>
-                  <Radio.Button value="all">All</Radio.Button>
-                </Radio.Group>
-              </span>
-            </div>
-            <div>
-              <CasesTable />
-            </div>
+            {allCasesStore!.cases.length > 0 ? (
+              <section>
+                <div>
+                  <span
+                    style={{
+                      marginBottom: "2em",
+                      width: "40%",
+                      display: "inline-block"
+                    }}
+                  >
+                    <FilterInput />
+                  </span>
+                  <span style={{ marginLeft: "8px", marginRight: "8px" }}>
+                    <Divider type="vertical" />
+                  </span>
+                  <span>
+                    <Radio.Group defaultValue="assigned">
+                      <Radio.Button value="assigned">Assigned</Radio.Button>
+                      <Radio.Button value="all">All</Radio.Button>
+                    </Radio.Group>
+                  </span>
+                </div>
+                <div>
+                  <CasesTable />
+                </div>
+              </section>
+            ) : (
+              <Empty
+                description={
+                  <div style={{ marginTop: "1em" }}>
+                    <h2>No cases</h2>
+                    <p style={{ fontSize: 16 }}>
+                      <a onClick={() => uiStore!.openCreateCaseModal()}>
+                        Create a case
+                      </a>{" "}
+                      to start tracking and collaborating on an investigation.
+                    </p>
+                  </div>
+                }
+              />
+            )}
           </div>
         );
       }
