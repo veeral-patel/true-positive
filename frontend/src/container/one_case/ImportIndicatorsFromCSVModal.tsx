190c2c2e63827c86c0be38bfe3029d6ecf2144d8
Veeral Patel <vpatel98@gmail.com>
2019-09-22 20:24:27 -0700

    add dragger component

--- frontend/src/container/one_case/ImportIndicatorsFromCSVModal.tsx
+++ frontend/src/container/one_case/ImportIndicatorsFromCSVModal.tsx
@@ -1,7 +1,8 @@
-import { Button, Modal, Steps } from "antd";
+import { Button, Icon, Modal, Steps, Upload } from "antd";
 import React from "react";
 
 const { Step } = Steps;
+const { Dragger } = Upload;
 
 interface Props {
   /* whether this modal is open. */
@@ -41,7 +42,17 @@ class ImportIndicatorsFromCSVModal extends React.Component<Props, State> {
             <Step title="Add indicators" />
           </Steps>
           <div className="steps-content" style={{ marginTop: "1.5em" }}>
-            Content here
+            {currentStep === 0 ? (
+              <Dragger>
+                <p className="ant-upload-drag-icon">
+                  <Icon type="upload" />
+                </p>
+                <p className="ant-upload-text">
+                  Drag a CSV into this area to upload
+                </p>
+                <p className="ant-upload-hint">Or click to select the file</p>
+              </Dragger>
+            ) : null}
           </div>
           <div className="steps-action" style={{ marginTop: "1em" }}>
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
