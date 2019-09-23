3d2aecce21450b02fe23dfa8e2239a5fadb20344
Veeral Patel <vpatel98@gmail.com>
2019-09-22 18:31:51 -0700

    add file upload area

--- frontend/src/container/one_case/AddFileIndicatorModal.tsx
+++ frontend/src/container/one_case/AddFileIndicatorModal.tsx
@@ -1,5 +1,7 @@
-import { Modal } from "antd";
+import { Icon, Modal, Upload } from "antd";
 import React from "react";
+
+const { Dragger } = Upload;
 
 interface Props {
   visible: boolean;
@@ -14,7 +16,17 @@ class AddFileIndicatorModal extends React.Component<Props> {
         title="Add a file indicator"
         visible={visible}
         onCancel={() => handleClose()}
-      ></Modal>
+      >
+        <Dragger>
+          <p className="ant-upload-drag-icon">
+            <Icon type="inbox" />
+          </p>
+          <p className="ant-upload-text">
+            Drag/drop file(s) into this area to upload them
+          </p>
+          <p className="ant-upload-hint">Or click to select file(s)</p>
+        </Dragger>
+      </Modal>
     );
   }
 }
