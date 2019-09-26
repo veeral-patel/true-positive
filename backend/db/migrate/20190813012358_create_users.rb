a945b5a0a36757caead7cdfdd80515722f3bd3aa
Veeral Patel <vpatel98@gmail.com>
2019-09-25 21:37:21 -0700

    consolidate migration for user model

--- backend/db/migrate/20190813012358_create_users.rb
+++ backend/db/migrate/20190813012358_create_users.rb
@@ -3,6 +3,30 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
 
+      t.string :email
+      t.string :password_digest
+      t.string :auth_tokens
+
+      # Confirmable
+      t.string :unconfirmed_email
+      t.string :confirmation_token
+      t.datetime :confirmation_sent_at
+      t.datetime :confirmed_at
+
+      # Recoverable
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      # Trackable
+      t.string :last_sign_in_ip
+      t.datetime :last_sign_in_at
+
+      # Invitable
+      t.string :invitation_token
+      t.datetime :invitation_sent_at
+      t.datetime :invitation_accepted_at
+      t.datetime :invitation_created_at
+
       t.timestamps
     end
   end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_08_024043) do
+ActiveRecord::Schema.define(version: 2019_09_26_043519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
