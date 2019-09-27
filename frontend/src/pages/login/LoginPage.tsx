2e79bcc2da2cef64ff0de2ec8b8e1992e4dcff09
Veeral Patel <vpatel98@gmail.com>
2019-09-26 22:09:15 -0700

    autofocus username input

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -9,6 +9,7 @@ interface Props {
 class NormalLoginForm extends React.Component<Props> {
   handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    this.props.form.validateFields();
   };
 
   render() {
@@ -17,17 +18,18 @@ class NormalLoginForm extends React.Component<Props> {
       <Form onSubmit={this.handleSubmit} className="login-form">
         <Form.Item>
           {getFieldDecorator("username", {
-            rules: [{ required: true, message: "Please input your username!" }]
+            rules: [{ required: true, message: "Please enter your username" }]
           })(
             <Input
               prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
               placeholder="Username"
+              autoFocus
             />
           )}
         </Form.Item>
         <Form.Item>
           {getFieldDecorator("password", {
-            rules: [{ required: true, message: "Please input your Password!" }]
+            rules: [{ required: true, message: "Please enter your password!" }]
           })(
             <Input
               prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
