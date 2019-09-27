33ecbe06ad1f52f26d2adfc0ba801c5d814d438c
Veeral Patel <vpatel98@gmail.com>
2019-09-26 23:05:25 -0700

    added has_many to user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,18 +1,14 @@
 class User < ApplicationRecord
     include RailsJwtAuth::Authenticatable
-    # include RailsJwtAuth::Confirmable
-    # include RailsJwtAuth::Recoverable
-    # include RailsJwtAuth::Trackable
-    # include RailsJwtAuth::Invitable
+
+    has_many :created_cases, foreign_key: "created_by_id", class_name: "Case"
+    has_many :created_tasks, foreign_key: "created_by_id", class_name: "Task"
+    has_many :created_indicators, foreign_key: "created_by_id", class_name: "Indicator"
 
     validates :username, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } , uniqueness: true, presence: true
 
     def to_s
         self.username
-    end
-
-    def created_cases
-        Case.where(created_by: self)
     end
 end
