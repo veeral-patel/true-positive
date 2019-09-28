c614a0261f47831ee88d0d47dc212c4669f7e808
Veeral Patel <vpatel98@gmail.com>
2019-09-28 00:56:50 -0700

    remove mst-gql completely

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,11 @@
 import "antd/dist/antd.css";
 import App from "App";
 import { Provider } from "mobx-react";
-import { RootStore, StoreContext } from "models";
-import { createHttpClient } from "mst-gql";
 import React from "react";
 import ReactDOM from "react-dom";
 import "react-quill/dist/quill.snow.css";
 import store from "stores";
 import * as serviceWorker from "./serviceWorker";
-
-const gqlHttpClient = createHttpClient(
-  `${process.env.REACT_APP_API_ENDPOINT}/graphql`
-);
-
-const rootStore = RootStore.create(undefined, {
-  gqlHttpClient
-});
 
 ReactDOM.render(
   <Provider
@@ -29,9 +19,7 @@ ReactDOM.render(
     allTasksStore={store.allTasksStore}
     authStore={store.authStore}
   >
-    <StoreContext.Provider value={rootStore}>
-      <App />
-    </StoreContext.Provider>
+    <App />
   </Provider>,
   document.getElementById("root")
 );
