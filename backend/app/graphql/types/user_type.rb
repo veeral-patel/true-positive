954ccaff45d25720b1d0a9e77b6ddba86a134adc
Veeral Patel <vpatel98@gmail.com>
2019-09-28 16:21:40 -0700

    be able to query if an user is an admin

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -13,5 +13,9 @@ module Types
         field :email, String, null: false do
             description "The user's email address."
         end
+
+        field :admin, Boolean, null: false do
+            description "Whether this user is an administrator. Administrators can view, edit, or delete any case, manage users, and adjust global settings (like customizing statuses)."
+        end
     end
 end
