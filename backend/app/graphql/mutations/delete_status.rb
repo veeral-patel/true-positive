94b4b6a4029ace5f2787f172d4a6092b3b13f2f9
Veeral Patel <vpatel98@gmail.com>
2019-09-28 22:27:29 -0700

    can delete a status by name

--- backend/app/graphql/mutations/delete_status.rb
+++ backend/app/graphql/mutations/delete_status.rb
@@ -1,20 +1,20 @@
 class Mutations::DeleteStatus < Mutations::BaseMutation
     description "Deletes a status. You cannot delete a status if any cases or tasks have that status."
 
-    argument :id, ID, required: true do
-        description "The ID of the status to delete."
+    argument :name, String, required: true do
+        description "The name of the status to delete."
     end
 
-    field :id, ID, null: false do
-        description "The ID of the status that was just deleted."
+    field :name, String, null: false do
+        description "The name of the status that was just deleted."
     end
 
-    def resolve(id:)
-        status = find_status_or_throw_execution_error(status_id: id)
+    def resolve(name:)
+        status = find_status_by_name_or_throw_execution_error(status_name: name)
 
         if status.destroy
             {
-                "id": id
+                "name": name
             }
         else
             raise GraphQL::ExecutionError, status.errors.full_messages.join(" | ")
