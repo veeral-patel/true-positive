51c7a07a019f679fb669437142264e2dfb176608
Veeral Patel <vpatel98@gmail.com>
2019-09-28 17:00:33 -0700

    create a smart create case component

--- frontend/src/container/all_cases/CreateCaseModal.tsx
+++ frontend/src/container/all_cases/CreateCaseModal.tsx
@@ -1,4 +1,5 @@
 import { AutoComplete, Button, Form, Icon, Input, Modal, Select } from "antd";
+import { WrappedFormUtils } from "antd/lib/form/Form";
 import "container/all_cases/CreateCaseModalP.css";
 import { inject, observer } from "mobx-react";
 import React from "react";
@@ -8,32 +9,95 @@ import UIStore from "stores/UIStore";
 
 const { Option } = Select;
 
-interface Props {
-  uiStore?: UIStore;
+// -----
+
+interface FormProps {
+  form: WrappedFormUtils;
   statusStore?: StatusStore;
   priorityStore?: PriorityStore;
+  uiStore?: UIStore;
 }
 
-export default inject("uiStore", "statusStore", "priorityStore")(
+class DumbCreateCaseForm extends React.Component<FormProps> {
+  componentDidMount() {
+    const { statusStore, priorityStore } = this.props;
+    statusStore!.loadStatuses();
+    priorityStore!.loadPriorities();
+  }
+
+  render() {
+    const { uiStore, statusStore, priorityStore } = this.props;
+
+    const statusOptions = statusStore!.statuses.map(status => (
+      <Option key={status.id}>{status.name}</Option>
+    ));
+
+    const priorityOptions = priorityStore!.priorities.map(priority => (
+      <Option key={priority.id}>{priority.name}</Option>
+    ));
+
+    return (
+      <Form colon={false}>
+        <Form.Item label="Name" required>
+          <Input
+            placeholder="Found Ryuk"
+            ref={input => input && input.focus()}
+          />
+        </Form.Item>
+        <Form.Item label="Status" required>
+          <AutoComplete
+            dataSource={statusOptions}
+            placeholder="Choose a status"
+          >
+            <Input suffix={<Icon type="down" style={{ color: "gray" }} />} />
+          </AutoComplete>
+        </Form.Item>
+        <Form.Item label="Priority" required>
+          <AutoComplete
+            dataSource={priorityOptions}
+            placeholder="Choose a priority"
+          >
+            <Input suffix={<Icon type="down" style={{ color: "gray" }} />} />
+          </AutoComplete>
+        </Form.Item>
+        <Form.Item>
+          <div style={{ float: "right" }}>
+            <Button
+              style={{ marginRight: "0.5em" }}
+              onClick={() => uiStore!.closeModal()}
+            >
+              Cancel
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Create Case
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+// ----
+
+// provide our form with validation abilities and access to our MobX stores
+const CreateCaseForm = Form.create()(
+  inject("statusStore", "priorityStore", "uiStore")(
+    observer(DumbCreateCaseForm)
+  )
+);
+
+// -----
+
+interface ModalProps {
+  uiStore?: UIStore;
+}
+
+export default inject("uiStore")(
   observer(
-    class CreateCaseModal extends React.Component<Props> {
-      componentDidMount() {
-        const { statusStore, priorityStore } = this.props;
-        statusStore!.loadStatuses();
-        priorityStore!.loadPriorities();
-      }
-
+    class CreateCaseModal extends React.Component<ModalProps> {
       render() {
-        const { uiStore, statusStore, priorityStore } = this.props;
-
-        const statusOptions = statusStore!.statuses.map(status => (
-          <Option key={status.id}>{status.name}</Option>
-        ));
-
-        const priorityOptions = priorityStore!.priorities.map(priority => (
-          <Option key={priority.id}>{priority.name}</Option>
-        ));
-
+        const { uiStore } = this.props;
         return (
           <Modal
             visible={uiStore!.openModal === "CREATE_CASE_MODAL"}
@@ -43,47 +107,7 @@ export default inject("uiStore", "statusStore", "priorityStore")(
             style={{ padding: "0px" }}
             destroyOnClose={true}
           >
-            <Form colon={false}>
-              <Form.Item label="Name" required>
-                <Input
-                  placeholder="Found Ryuk"
-                  ref={input => input && input.focus()}
-                />
-              </Form.Item>
-              <Form.Item label="Status" required>
-                <AutoComplete
-                  dataSource={statusOptions}
-                  placeholder="Choose a status"
-                >
-                  <Input
-                    suffix={<Icon type="down" style={{ color: "gray" }} />}
-                  />
-                </AutoComplete>
-              </Form.Item>
-              <Form.Item label="Priority" required>
-                <AutoComplete
-                  dataSource={priorityOptions}
-                  placeholder="Choose a priority"
-                >
-                  <Input
-                    suffix={<Icon type="down" style={{ color: "gray" }} />}
-                  />
-                </AutoComplete>
-              </Form.Item>
-              <Form.Item>
-                <div style={{ float: "right" }}>
-                  <Button
-                    style={{ marginRight: "0.5em" }}
-                    onClick={() => uiStore!.closeModal()}
-                  >
-                    Cancel
-                  </Button>
-                  <Button type="primary" htmlType="submit">
-                    Create Case
-                  </Button>
-                </div>
-              </Form.Item>
-            </Form>
+            <CreateCaseForm />
           </Modal>
         );
       }
