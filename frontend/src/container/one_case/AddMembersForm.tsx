0c44c84e49358f3f1b5573399dc8d5759825cbd9
Veeral Patel <vpatel98@gmail.com>
2019-09-30 23:37:08 -0700

    turn the select into a form

--- frontend/src/container/one_case/AddMembersForm.tsx
+++ frontend/src/container/one_case/AddMembersForm.tsx
@@ -1,23 +1,40 @@
-import { Select } from "antd";
+import { Form, Select } from "antd";
+import { FormComponentProps, WrappedFormUtils } from "antd/lib/form/Form";
 import React from "react";
 
-interface Props {
+interface FormProps {
   userOptions: React.ReactNode[];
+  form: WrappedFormUtils;
 }
 
-class AddMembersForm extends React.Component<Props> {
+class DumbAddMembersForm extends React.Component<
+  FormProps & FormComponentProps
+> {
   render() {
     const { userOptions } = this.props;
+    const { getFieldDecorator } = this.props.form;
+
     return (
-      <Select
-        style={{ width: "100%" }}
-        mode="multiple"
-        placeholder="Add members"
-      >
-        {userOptions}
-      </Select>
+      // add a onSubmit prop to Form below
+      <Form>
+        <Form.Item>
+          {getFieldDecorator("status")(
+            <Select
+              style={{ width: "100%" }}
+              mode="multiple"
+              placeholder="Add members"
+            >
+              {userOptions}
+            </Select>
+          )}
+        </Form.Item>
+      </Form>
     );
   }
 }
+
+const AddMembersForm = Form.create<FormProps & FormComponentProps>()(
+  DumbAddMembersForm
+);
 
 export default AddMembersForm;
