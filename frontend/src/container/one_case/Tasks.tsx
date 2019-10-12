7568d6134657913c5d5ae53d46f7b0ba27eb0060
Veeral Patel <vpatel98@gmail.com>
2019-10-11 21:09:41 -0700

    remove stray brace

--- frontend/src/container/one_case/Tasks.tsx
+++ frontend/src/container/one_case/Tasks.tsx
@@ -96,7 +96,6 @@ export default inject("activeCaseStore")(
                         )
                       }
                     />
-                    }
                   </div>
                 )}
               </Content>
