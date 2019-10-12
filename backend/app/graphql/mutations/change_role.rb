30a42edfb4cbcc333c53db06cb7e4e848c1ac5b9
Veeral Patel <vpatel98@gmail.com>
2019-10-12 15:18:08 -0700

    can change the role of a user

--- backend/app/graphql/mutations/change_role.rb
+++ backend/app/graphql/mutations/change_role.rb
@@ -1,4 +1,4 @@
-class Mutations::ChangeRole
+class Mutations::ChangeRole  < Mutations::BaseMutation
     description "Change a case member's role."
 
     argument :case_id, ID, required: true do
@@ -13,11 +13,27 @@ class Mutations::ChangeRole
         description "The case member's new role."
     end
 
+    field :case, Types::CaseType, null: true do
+        description "The updated case."
+    end
+
     def resolve(case_id:, username:, role:)
         # find the case and the user in question
         the_case = find_case_or_throw_execution_error(case_id: case_id)
         user = find_user_or_throw_execution_error(username: username)
 
-        # TODO: write the rest of the mutation
+        begin
+            member = the_case.case_members.find_by!(user: user)
+            member.role = role
+            if member.save
+                {
+                    "case": the_case
+                }
+            else
+                raise GraphQL::ExecutionError, the_case.errors.full_messages.join(" | ")
+            end
+        rescue ActiveRecord::RecordNotFound
+            raise GraphQL::ExecutionError, "#{username} is not a member of the specified case."
+        end
     end
 end
