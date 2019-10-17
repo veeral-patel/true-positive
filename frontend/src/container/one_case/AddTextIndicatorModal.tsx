8bba168647aabcef023417c5fa2c9958933c5545
Veeral Patel <vpatel98@gmail.com>
2019-10-16 19:29:13 -0700

    validate fields

--- frontend/src/container/one_case/AddTextIndicatorModal.tsx
+++ frontend/src/container/one_case/AddTextIndicatorModal.tsx
@@ -1,15 +1,44 @@
 import { Form, Input, Modal, Typography } from "antd";
+import { WrappedFormUtils } from "antd/lib/form/Form";
 import React from "react";
 
 const { TextArea } = Input;
 const { Paragraph } = Typography;
 
-interface Props {
+// -----
+
+interface FormProps {
+  form: WrappedFormUtils;
+}
+
+class DumbAddIndicatorModal extends React.Component<FormProps> {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form>
+        <Form.Item label="Name">
+          {getFieldDecorator("name", {
+            rules: [{ required: true, message: "Please name your indicator" }]
+          })(<Input placeholder="Name your indicator" />)}
+        </Form.Item>
+        <Form.Item label="Indicator">
+          {getFieldDecorator("indicator", {
+            rules: [{ required: true, message: "Please enter your indicator" }]
+          })(<TextArea placeholder="Enter your indicator here" rows={5} />)}
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+// -----
+
+interface ModalProps {
   visible: boolean;
   handleClose: () => void;
 }
 
-class AddTextIndicatorModal extends React.Component<Props> {
+class AddTextIndicatorModal extends React.Component<ModalProps> {
   render() {
     const { visible, handleClose } = this.props;
     return (
@@ -23,14 +52,6 @@ class AddTextIndicatorModal extends React.Component<Props> {
           Examples of text indicators include Snort rules, Yara signatures, and
           Exif metadata.
         </Paragraph>
-        <Form>
-          <Form.Item>
-            <Input placeholder="Name your indicator" />
-          </Form.Item>
-          <Form.Item>
-            <TextArea placeholder="Enter indicator here" rows={5} />
-          </Form.Item>
-        </Form>
       </Modal>
     );
   }
