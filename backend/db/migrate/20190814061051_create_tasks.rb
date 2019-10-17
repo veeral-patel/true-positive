44307f6045579974082c329f024b2d0a5ca4ea91
Veeral Patel <vpatel98@gmail.com>
2019-10-16 21:31:34 -0700

    remove status and priority from task

--- backend/db/migrate/20190814061051_create_tasks.rb
+++ backend/db/migrate/20190814061051_create_tasks.rb
@@ -3,8 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :name
       t.text :description
-      t.references :status, foreign_key: true
-      t.references :priority, foreign_key: true
+      t.boolean :done, default: false
       t.references :case, foreign_key: true
       t.references :created_by, foreign_key: { to_table: :users }
       t.references :assigned_to, foreign_key: { to_table: :users }
