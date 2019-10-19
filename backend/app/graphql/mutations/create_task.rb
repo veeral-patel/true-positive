051bac114867e2b677e21570f06309e880d5a137
Veeral Patel <vpatel98@gmail.com>
2019-10-19 01:06:08 -0700

    be able to mark a task as done upon creation

--- backend/app/graphql/mutations/create_task.rb
+++ backend/app/graphql/mutations/create_task.rb
@@ -15,6 +15,10 @@ class Mutations::CreateTask < Mutations::BaseMutation
 
     # not required ----
 
+    argument :done, Boolean, required: false do
+        description "Whether you want to mark this task as done once it's created."
+    end
+
     argument :description, String, required: false do
         description "Optional text describing this task."
     end
@@ -31,7 +35,7 @@ class Mutations::CreateTask < Mutations::BaseMutation
         description "The newly created task."
     end
 
-    def resolve(name:, case_id:, description: nil, assigned_to: nil, tags: nil)
+    def resolve(name:, case_id:, done: false, description: nil, assigned_to: nil, tags: nil)
         # find case, assigned user for this new task
         the_case = find_case_or_throw_execution_error(case_id: case_id)
         assigned_user = assigned_to.nil? ? nil : find_user_or_throw_execution_error(username: assigned_to)
@@ -48,7 +52,8 @@ class Mutations::CreateTask < Mutations::BaseMutation
             created_by: context[:current_user],
             description: description,
             assigned_to: assigned_user,
-            tag_list: tags
+            tag_list: tags,
+            done: done
         )
 
         # save it to the database
