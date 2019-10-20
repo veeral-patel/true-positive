c0d7336a79a708faba91f57fdf8e42f45d9f4936
Veeral Patel <vpatel98@gmail.com>
2019-10-20 10:53:56 -0700

    add search input to the top menu

--- frontend/src/container/shared/top_menu/TopMenu.tsx
+++ frontend/src/container/shared/top_menu/TopMenu.tsx
@@ -1,14 +1,12 @@
 import { navigate } from "@reach/router";
-import { Menu } from "antd";
+import { Icon, Input, Menu } from "antd";
 import logo from "logo/tp_logo_lightblue.svg";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import AuthStore from "stores/AuthStore";
-import UIStore from "stores/UIStore";
 import { paths } from "utils/constants";
 
 interface TopMenuProps {
-  uiStore?: UIStore;
   authStore?: AuthStore;
 }
 
@@ -16,7 +14,7 @@ export default inject("uiStore", "authStore")(
   observer(
     class TopMenu extends React.Component<TopMenuProps> {
       render() {
-        const { uiStore, authStore } = this.props;
+        const { authStore } = this.props;
         return (
           <Menu theme="dark" mode="horizontal" selectedKeys={[]}>
             <Menu.Item
@@ -42,6 +40,9 @@ export default inject("uiStore", "authStore")(
               style={{ float: "left" }}
             >
               Manage
+            </Menu.Item>
+            <Menu.Item style={{ float: "left" }}>
+              <Input placeholder="Search" prefix={<Icon type="search" />} />
             </Menu.Item>
             <Menu.Item
               style={{ float: "right" }}
