6f48b258e9845d073ba10c3a5808169d4b87353f
Veeral Patel <vpatel98@gmail.com>
2019-10-21 22:20:45 -0700

    remove edit button

--- frontend/src/presentational/shared/comments/CommentListP.tsx
+++ frontend/src/presentational/shared/comments/CommentListP.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Comment, List } from "antd";
+import { Avatar, Comment, List } from "antd";
 import DeleteCommentButton from "container/shared/comments/DeleteCommentButton";
 import React from "react";
 import IComment from "ts/interfaces/IComment";
@@ -25,10 +25,7 @@ const CommentListP: React.FC<CommentListProps> = ({ comments }) => {
             avatar={<Avatar icon="user" />}
             actions={[
               usernameOfCurrentUser === comment.createdBy.username && (
-                <span>
-                  <Button type="link" icon="edit" />
-                  <DeleteCommentButton comment={comment} />
-                </span>
+                <DeleteCommentButton comment={comment} />
               )
             ]}
           />
