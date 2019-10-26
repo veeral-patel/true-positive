8d8ccd71095acec71ffc936df974422a97a029be
Veeral Patel <vpatel98@gmail.com>
2019-10-25 23:28:00 -0700

    add menu to Search page

--- frontend/src/pages/search/SearchPage.tsx
+++ frontend/src/pages/search/SearchPage.tsx
@@ -1,11 +1,23 @@
 import { RouteComponentProps } from "@reach/router";
+import { Menu } from "antd";
 import React from "react";
 
 interface Props extends RouteComponentProps {}
 
 class SearchPage extends React.Component<Props> {
   render() {
-    return <h3>Search</h3>;
+    return (
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <Menu theme="light" mode="vertical">
+            <Menu.Item>Cases</Menu.Item>
+            <Menu.Item>Tasks</Menu.Item>
+            <Menu.Item>Indicators</Menu.Item>
+          </Menu>
+        </div>
+        <div style={{ flex: 4 }}></div>
+      </div>
+    );
   }
 }
 
