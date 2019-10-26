b336a8f3af350c2e4b7405dcb62d705941598aa3
Veeral Patel <vpatel98@gmail.com>
2019-10-25 23:36:56 -0700

    highlight Cases by default

--- frontend/src/pages/search/SearchPage.tsx
+++ frontend/src/pages/search/SearchPage.tsx
@@ -9,10 +9,10 @@ class SearchPage extends React.Component<Props> {
     return (
       <div style={{ display: "flex" }}>
         <div style={{ flex: 1 }}>
-          <Menu theme="light" mode="vertical">
-            <Menu.Item>Cases</Menu.Item>
-            <Menu.Item>Tasks</Menu.Item>
-            <Menu.Item>Indicators</Menu.Item>
+          <Menu theme="light" mode="vertical" defaultSelectedKeys={["cases"]}>
+            <Menu.Item key="cases">Cases</Menu.Item>
+            <Menu.Item key="tasks">Tasks</Menu.Item>
+            <Menu.Item key="indicators">Indicators</Menu.Item>
           </Menu>
         </div>
         <div style={{ flex: 4 }}></div>
