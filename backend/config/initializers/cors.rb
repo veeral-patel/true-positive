952257cd77ac158bd1bed5da084a948491d2376c
Veeral Patel <vpatel98@gmail.com>
2019-10-26 18:36:57 -0700

    whitelist tryincidents.com

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', '*.tryincidents.com'
 
     resource '*',
       headers: :any,
