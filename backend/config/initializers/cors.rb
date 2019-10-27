7217824070bba8577f12472d08fb8797e2097912
Veeral Patel <vpatel98@gmail.com>
2019-10-26 20:29:46 -0700

    change origin again

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '/^(.*\.|)tryincidents\.com$/'
+    origins 'localhost:3000', 'console.staging.tryincidents.com'
 
     resource '*',
       headers: :any,
