a93fcfe57bd4978f50dea39dddbc202bd1f5f3e0
Veeral Patel <vpatel98@gmail.com>
2019-11-07 23:50:06 -0800

    remove ID from status

--- backend/app/graphql/types/status_type.rb
+++ backend/app/graphql/types/status_type.rb
@@ -2,10 +2,6 @@ module Types
     class StatusType < Types::BaseObject
         description "A state a case or a task can be in."
 
-        field :id, ID, null: false do
-            description "An unique integer identifying this status."
-        end
-
         field :name, String, null: false do
             description "This status's name (such as 'Open')."
         end
