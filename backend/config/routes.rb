8d804896d8ba4f3a509ca426fd8f19b05e75a340
Veeral Patel <vpatel98@gmail.com>
2019-11-07 20:58:27 -0800

    remove commented out routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :session, controller: 'sessions', only: [:create, :destroy]
-  # resources :confirmations, controller: 'rails_jwt_auth/confirmations', only: [:create, :update]
-  # resources :passwords, controller: 'rails_jwt_auth/passwords', only: [:create, :update]
-  # resources :invitations, controller: 'rails_jwt_auth/invitations', only: [:create, :update]
-
   post "/graphql", to: "graphql#execute"
 end
