00f092b95a72871ddc3de972bdacaae7d59f6fd4
Veeral Patel <vpatel98@gmail.com>
2019-11-07 23:48:05 -0800

    remove ID from tag type

--- backend/app/graphql/types/tag_type.rb
+++ backend/app/graphql/types/tag_type.rb
@@ -2,10 +2,6 @@ module Types
     class TagType < Types::BaseObject
         description "An existing tag on a case, task, or indicator."
 
-        field :id, ID, null: false do
-            description "An unique integer identifying this tag."
-        end
-
         field :name, String, null: false do
             description "The tag (such as 'phishing')."
         end
