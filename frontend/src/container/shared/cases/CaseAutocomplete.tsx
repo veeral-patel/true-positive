cd61d022b8bdde088f77c36ef4599dcac0bdc442
Veeral Patel <vpatel98@gmail.com>
2019-11-19 01:21:41 -0800

    remove another unneeded presentational component

--- frontend/src/container/shared/cases/CaseAutocomplete.tsx
+++ frontend/src/container/shared/cases/CaseAutocomplete.tsx
@@ -1,7 +1,8 @@
+import { AutoComplete, Icon, Input } from "antd";
 import { inject, observer } from "mobx-react";
-import CaseAutocompleteP from "presentational/shared/cases/CaseAutocompleteP";
 import React from "react";
 import AllCasesStore from "stores/AllCasesStore";
+const { Option } = AutoComplete;
 
 interface CaseAutocompleteProps {
   allCasesStore?: AllCasesStore;
@@ -18,7 +19,20 @@ export default inject("allCasesStore")(
       render() {
         // One day, handle the case where the cases are loading.
         const { allCasesStore } = this.props;
-        return <CaseAutocompleteP cases={allCasesStore!.cases} />;
+
+        const options = allCasesStore!.cases.map(thecase => (
+          <Option key={thecase.id}>{thecase.name}</Option>
+        ));
+
+        return (
+          <AutoComplete dataSource={options} style={{ width: "100%" }}>
+            <Input
+              prefix={<Icon type="search" />}
+              suffix={<Icon type="arrow-right" />}
+              placeholder="Filter cases"
+            />
+          </AutoComplete>
+        );
       }
     }
   )
