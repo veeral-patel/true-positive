8b394bb654df035399f0c255dd2754eb0ad3bc6b
Veeral Patel <vpatel98@gmail.com>
2019-11-23 12:18:29 -0800

    remove TagStore from EditableTagList file

--- frontend/src/presentational/shared/tags/EditableTagList.tsx
+++ frontend/src/presentational/shared/tags/EditableTagList.tsx
@@ -6,7 +6,6 @@ import ListOfTagsP from "presentational/shared/tags/ListOfTagsP";
 import GET_TAGS from "queries/getTags";
 import React, { useState } from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
-import TagStore from "stores/TagStore";
 import ITag from "ts/interfaces/ITag";
 
 const { Option } = Select;
@@ -16,9 +15,6 @@ const { Option } = Select;
 interface FormProps {
   /* the list of existing tags this case/task/etc posseses */
   existingTags: ITag[];
-
-  /* used to fetch all existing tags for autocomplete */
-  tagStore?: TagStore;
 
   /* used for the 'update tags' operation */
   activeCaseStore?: ActiveCaseStore;
@@ -36,62 +32,6 @@ interface FormProps {
 interface TagData {
   tags: ITag[];
 }
-
-// don't use this component directly
-// class DumbEditTagsForm extends React.Component<FormProps & FormComponentProps> {
-//   componentDidMount() {
-//     const { tagStore } = this.props;
-//     tagStore!.loadTags();
-//   }
-
-//   render() {
-//     const { existingTags, form, tagStore, handleCancel } = this.props;
-//     const { getFieldDecorator } = form;
-
-//     // generate options from our list of all tags
-//     const allTagOptions = tagStore!.tags.map(tag => (
-//       <Option key={tag.name}>{tag.name}</Option>
-//     ));
-
-//     return (
-//       <Form onSubmit={this.handleSubmit.bind(this)}>
-//         <Form.Item>
-//           {getFieldDecorator("tags", {
-//             initialValue: existingTags.map(tag => tag.name)
-//           })(
-//             <Select
-//               mode="tags"
-//               placeholder="Select tags"
-//               tokenSeparators={[","]}
-//               style={{ width: "100%" }}
-//             >
-//               {allTagOptions}
-//             </Select>
-//           )}
-//         </Form.Item>
-//         <Form.Item>
-//           <Button type="link" onClick={handleCancel}>
-//             Cancel
-//           </Button>
-//           <Button htmlType="submit">Save</Button>
-//         </Form.Item>
-//       </Form>
-//     );
-//   }
-
-//   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-//     // prevent page reload
-//     event.preventDefault();
-
-//     // validate fields
-//     const { form, activeCaseStore, objectId, type } = this.props;
-//     form.validateFields((errors, values) => {
-//       if (!errors) {
-//         if (objectId) activeCaseStore!.changeTags(values.tags, objectId, type);
-//       }
-//     });
-//   }
-// }
 
 function DumbEditTagsForm(props: FormProps & FormComponentProps) {
   // runs when form is submitted
@@ -153,7 +93,7 @@ function DumbEditTagsForm(props: FormProps & FormComponentProps) {
 
 // our smart form
 const EditTagsForm = Form.create<FormProps & FormComponentProps>()(
-  inject("tagStore", "activeCaseStore")(observer(DumbEditTagsForm))
+  inject("activeCaseStore")(observer(DumbEditTagsForm))
 );
 
 // -----
