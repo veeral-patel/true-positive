69513d890af2b87fcf386aee1985aa6a5e8f6145
Veeral Patel <vpatel98@gmail.com>
2019-11-23 16:01:01 -0800

    add horizontal tabs

--- frontend/src/container/admin/CustomizeTemplates.tsx
+++ frontend/src/container/admin/CustomizeTemplates.tsx
@@ -1,7 +1,18 @@
+import { Tabs } from "antd";
 import React from "react";
 
+const { TabPane } = Tabs;
+
 function CustomizeTemplates() {
-  return <h3>Templates</h3>;
+  return (
+    <>
+      <h3>Templates</h3>
+      <Tabs defaultActiveKey="task_templates">
+        <TabPane key="task_templates" tab="Task Templates"></TabPane>
+        <TabPane key="case_templates" tab="Case Templates"></TabPane>
+      </Tabs>
+    </>
+  );
 }
 
 export default CustomizeTemplates;
