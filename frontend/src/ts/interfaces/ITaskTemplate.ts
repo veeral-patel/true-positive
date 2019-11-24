caac26f375a164da075c74ef4b9c93c84abbe6ad
Veeral Patel <vpatel98@gmail.com>
2019-11-23 18:17:42 -0800

    update ITaskTemplate type

--- frontend/src/ts/interfaces/ITaskTemplate.ts
+++ frontend/src/ts/interfaces/ITaskTemplate.ts
@@ -1,5 +1,11 @@
+import IUser from "./IUser";
+
 interface ITaskTemplate {
   name: string;
+  createdAt: string;
+  createdBy: IUser;
+  assignedTo: IUser | null;
+  description: string;
 }
 
 export default ITaskTemplate;
