eef52ece7bd384eadbedcc8ad60cd45f515aee3c
Veeral Patel <vpatel98@gmail.com>
2019-11-23 18:15:55 -0800

    add createdAt and createdBy to task template type

--- backend/app/graphql/types/task_template_type.rb
+++ backend/app/graphql/types/task_template_type.rb
@@ -10,6 +10,14 @@ class Types::TaskTemplateType < Types::BaseObject
         description "Default name for tasks created with this template."
     end
 
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false do
+        description "When this template was created (in ISO8601 format)."
+    end
+
+    field :created_by, Types::UserType, null: false do
+        description "The user who created this template."
+    end
+
     # possibly null ---
     field :description, String, null: true do
         description "Default description for tasks created with this template."
