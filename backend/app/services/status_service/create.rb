3c1ff2b305d9e90d83d053044f264d07db4d73d3
Veeral Patel <vpatel98@gmail.com>
2019-11-28 17:50:41 -0800

    remove dummy code from create status service

--- backend/app/services/status_service/create.rb
+++ backend/app/services/status_service/create.rb
@@ -2,10 +2,7 @@ require 'active_interaction'
 
 module StatusService
     class Create < ActiveInteraction::Base
-        float :x
-
         def execute
-            x ** 2
         end
     end
 end
