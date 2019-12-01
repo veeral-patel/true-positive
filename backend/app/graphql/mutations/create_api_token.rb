0c7745aca67733aa962077b2547f905095d56e4d
Veeral Patel <vpatel98@gmail.com>
2019-11-30 21:18:52 -0800

    fix bugs in mutation

--- backend/app/graphql/mutations/create_api_token.rb
+++ backend/app/graphql/mutations/create_api_token.rb
@@ -6,9 +6,9 @@ class Mutations::CreateApiToken < Mutations::BaseMutation
         description "A brief name describing the API token's purpose."
     end
 
-    field :api_token, Types::ApiTokenType, null: true
+    field :api_token, Types::ApiTokenType, null: true do
         description "The newly created API token."
-    do
+    end
 
     def resolve(name:)
     end
