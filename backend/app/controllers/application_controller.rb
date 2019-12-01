f40a981df966e2de46dff5b556c656836f1d9193
Veeral Patel <vpatel98@gmail.com>
2019-11-30 23:20:19 -0800

    add comments to authenticate_user_api method

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -13,12 +13,18 @@ class ApplicationController < ActionController::API
     # end
 
     def authenticate_user_api
+        # extract the API token from the Authorization HTTP header
         token_from_request = request.env['HTTP_AUTHORIZATION']&.split&.last
 
         begin
+            # determine if the provided API token exists in the database
             existing_token = ApiToken.find_by!(api_token: token_from_request)
+
+            # if it exists, set `current_user` to the user who created it
             @current_user = existing_token.user
         rescue ActiveRecord::RecordNotFound
+            # if the provided API token doesn't exist in the database,
+            # then return a 401 HTTP response
             render json: { "error": "API token is invalid" }, status: 401
         end
     end
