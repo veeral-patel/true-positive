67381c34883ff6f4f77dcacba409360a801f12e7
Veeral Patel <vpatel98@gmail.com>
2019-11-30 23:16:31 -0800

    authenticate_user_api method works!

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,13 +2,24 @@ class ApplicationController < ActionController::API
     include RailsJwtAuth::AuthenticableHelper
     include Pundit
 
-    before_action :authenticate_user
+    before_action :authenticate_user_api
 
-    def authenticate_user
+    # def authenticate_user_ui
+    #     begin
+    #         self.authenticate!
+    #     rescue RailsJwtAuth::NotAuthorized
+    #         render json: { "error": "You are not authenticated." }, status: 401
+    #     end
+    # end
+
+    def authenticate_user_api
+        token_from_request = request.env['HTTP_AUTHORIZATION']&.split&.last
+
         begin
-            self.authenticate!
-        rescue RailsJwtAuth::NotAuthorized
-            render json: { "error": "You are not authenticated." }, status: 401
+            existing_token = ApiToken.find_by!(api_token: token_from_request)
+            @current_user = existing_token.user
+        rescue ActiveRecord::RecordNotFound
+            render json: { "error": "API token is invalid" }, status: 401
         end
     end
 end
