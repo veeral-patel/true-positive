159f4733b0f831ee6f259643132176be96e71c3c
Veeral Patel <vpatel98@gmail.com>
2019-11-30 20:17:57 -0800

    add has_many association to user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     has_many :created_indicators, foreign_key: "created_by_id", class_name: "Indicator"
     has_many :created_task_templates, foreign_key: "created_by_id", class_name: "TaskTemplate"
     has_many :comments, foreign_key: "created_by_id", class_name: "Comment"
+    has_many :api_tokens, foreign_key: "user_id", class_name: "ApiToken"
 
     validates :username, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
