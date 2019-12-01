169ab25869d4e340892df070a42798e1af7e947b
Veeral Patel <vpatel98@gmail.com>
2019-11-30 21:49:15 -0800

    write resolver for createApiToken mutation

--- backend/app/graphql/mutations/create_api_token.rb
+++ backend/app/graphql/mutations/create_api_token.rb
@@ -11,5 +11,19 @@ class Mutations::CreateApiToken < Mutations::BaseMutation
     end
 
     def resolve(name:)
+        # create a new task in memory
+        new_token = context[:current_user].api_tokens.new(
+            name: name,
+            api_token: SecureRandom.hex(24)
+        )
+
+        # and save it to the database
+        if new_token.save
+            {
+                "api_token": new_token
+            }
+        else
+            raise GraphQL::ExecutionError, new_token.errors.full_messages.join(" | ") 
+        end
     end
 end
