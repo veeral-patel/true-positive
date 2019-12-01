821e55cb8df6c9ac92aa542540ca2d5e0e68fe07
Veeral Patel <vpatel98@gmail.com>
2019-11-30 23:52:03 -0800

    add comments

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::API
     def authenticate_user
         token_from_request = request.env['HTTP_AUTHORIZATION']&.split&.last
 
+        # Hack: If the provided token is 40 characters, then assume
+        # the user is authenticating via the API, the UI otherwise.
         if token_from_request.size == 40
             authenticate_user_api(token_from_request)
         else
@@ -15,6 +17,8 @@ class ApplicationController < ActionController::API
     end
 
     def authenticate_user_ui
+        # Authenticate and identify the user if they're logging in through the UI
+        # (aka with a JWT).
         begin
             self.authenticate!
         rescue RailsJwtAuth::NotAuthorized
@@ -23,6 +27,7 @@ class ApplicationController < ActionController::API
     end
 
     def authenticate_user_api(token_from_request)
+        # Authenticate and identify the user if they're using the API.
         begin
             # determine if the provided API token exists in the database
             existing_token = ApiToken.find_by!(api_token: token_from_request)
