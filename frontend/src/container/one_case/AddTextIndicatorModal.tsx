5e2859e94cc3e4a7cf41221626f274d11327f0a5
Veeral Patel <vpatel98@gmail.com>
2019-12-08 15:55:33 -0800

    fix AddTextIndicatorModel form

--- frontend/src/container/one_case/AddTextIndicatorModal.tsx
+++ frontend/src/container/one_case/AddTextIndicatorModal.tsx
@@ -1,5 +1,4 @@
 import { Button, Form, Input, Modal, Typography } from "antd";
-import { FormComponentProps, WrappedFormUtils } from "antd/lib/form/Form";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
@@ -10,68 +9,61 @@ const { Paragraph } = Typography;
 // -----
 
 interface FormProps {
-  form: WrappedFormUtils;
   handleClose: () => void;
   activeCaseStore?: ActiveCaseStore;
 }
 
 // Don't use this form directly
-class DumbAddIndicatorForm extends React.Component<
-  FormProps & FormComponentProps
-> {
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    const { handleClose } = this.props;
+const AddIndicatorForm = inject("activeCaseStore")(
+  observer(
+    class InnerForm extends React.Component<FormProps> {
+      render() {
+        const { handleClose, activeCaseStore } = this.props;
 
-    return (
-      <Form colon={false} onSubmit={this.handleSubmit.bind(this)}>
-        <Form.Item label="Name">
-          {getFieldDecorator("name", {
-            rules: [{ required: true, message: "Please name your indicator" }]
-          })(<Input placeholder="Name your indicator" />)}
-        </Form.Item>
-        <Form.Item label="Indicator">
-          {getFieldDecorator("indicator", {
-            rules: [{ required: true, message: "Please enter your indicator" }]
-          })(<TextArea placeholder="Enter your indicator here" rows={5} />)}
-        </Form.Item>
-        <Form.Item>
-          <div style={{ float: "right" }}>
-            <Button style={{ marginRight: "0.5em" }} onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button type="primary" htmlType="submit">
-              Add indicator
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-    );
-  }
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    // prevent page reload
-    event.preventDefault();
-
-    // validate our fields and raise errors if needed
-    const { form, activeCaseStore } = this.props;
-    form.validateFields((errors, values) => {
-      if (!errors && activeCaseStore!.activeCase) {
-        activeCaseStore!.createTextIndicator(
-          activeCaseStore!.activeCase.id,
-          values.name,
-          values.indicator
+        return (
+          <Form
+            colon={false}
+            onFinish={values => {
+              if (activeCaseStore!.activeCase) {
+                activeCaseStore!.createTextIndicator(
+                  activeCaseStore!.activeCase.id,
+                  values.name,
+                  values.indicator
+                );
+              }
+            }}
+          >
+            <Form.Item
+              label="Name"
+              rules={[
+                { required: true, message: "Please name your indicator" }
+              ]}
+            >
+              <Input placeholder="Name your indicator" />
+            </Form.Item>
+            <Form.Item
+              label="Indicator"
+              rules={[
+                { required: true, message: "Please enter your indicator" }
+              ]}
+            >
+              <TextArea placeholder="Enter your indicator here" rows={5} />
+            </Form.Item>
+            <Form.Item>
+              <div style={{ float: "right" }}>
+                <Button style={{ marginRight: "0.5em" }} onClick={handleClose}>
+                  Cancel
+                </Button>
+                <Button type="primary" htmlType="submit">
+                  Add indicator
+                </Button>
+              </div>
+            </Form.Item>
+          </Form>
         );
       }
-    });
-  }
-}
-
-// -----
-
-// Use this form instead
-const AddIndicatorForm = Form.create<FormProps & FormComponentProps>()(
-  inject("activeCaseStore")(observer(DumbAddIndicatorForm))
+    }
+  )
 );
 
 // -----
