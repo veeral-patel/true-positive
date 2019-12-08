62c9e366639556c9e6d80ed13c0f3c90f19e0943
Veeral Patel <vpatel98@gmail.com>
2019-12-08 15:33:40 -0800

    fix more form issues

--- frontend/src/container/all_cases/CreateCaseModal.tsx
+++ frontend/src/container/all_cases/CreateCaseModal.tsx
@@ -1,5 +1,4 @@
 import { Button, Form, Input, Modal, Select } from "antd";
-import { WrappedFormUtils } from "antd/lib/form/Form";
 import "container/shared/modals/FormInModal.css";
 import { inject, observer } from "mobx-react";
 import React from "react";
@@ -13,114 +12,115 @@ const { Option } = Select;
 // -----
 
 interface FormProps {
-  form: WrappedFormUtils;
   statusStore?: StatusStore;
   priorityStore?: PriorityStore;
   uiStore?: UIStore;
   allCasesStore?: AllCasesStore;
 }
 
-class DumbCreateCaseForm extends React.Component<FormProps> {
-  componentDidMount() {
-    const { statusStore, priorityStore } = this.props;
-    statusStore!.loadStatuses();
-    priorityStore!.loadPriorities();
-  }
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    // prevent page reload
-    event.preventDefault();
-
-    // validate our fields and raise errors if needed
-    const { form, allCasesStore } = this.props;
-
-    form.validateFields((errors, values) => {
-      if (!errors) {
-        allCasesStore!.createCase(values.name, values.status, values.priority);
+const CreateCaseForm = inject(
+  "statusStore",
+  "priorityStore",
+  "uiStore",
+  "allCasesStore"
+)(
+  observer(
+    class InnerForm extends React.Component<FormProps> {
+      componentDidMount() {
+        const { statusStore, priorityStore } = this.props;
+        statusStore!.loadStatuses();
+        priorityStore!.loadPriorities();
       }
-    });
-  }
 
-  render() {
-    const { uiStore, statusStore, priorityStore } = this.props;
-    const { getFieldDecorator } = this.props.form;
+      handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        // prevent page reload
+        event.preventDefault();
 
-    const statusOptions = statusStore!.statuses.map(status => (
-      <Option key={status.name}>{status.name}</Option>
-    ));
+        // validate our fields and raise errors if needed
+        const { form, allCasesStore } = this.props;
 
-    const priorityOptions = priorityStore!.priorities.map(priority => (
-      <Option key={priority.name}>{priority.name}</Option>
-    ));
+        form.validateFields((errors, values) => {
+          if (!errors) {
+            allCasesStore!.createCase(
+              values.name,
+              values.status,
+              values.priority
+            );
+          }
+        });
+      }
 
-    return (
-      <Form colon={false} onSubmit={this.handleSubmit.bind(this)}>
-        <Form.Item label="Name">
-          {getFieldDecorator("name", {
-            rules: [
-              { required: true, message: "Please enter a name for this case" }
-            ]
-          })(
-            <Input
-              placeholder="Found Ryuk"
-              ref={input => input && input.focus()}
-            />
-          )}
-        </Form.Item>
-        <Form.Item label="Status">
-          {getFieldDecorator("status", {
-            rules: [{ required: true, message: "Please choose a status" }]
-          })(
-            <Select
-              showSearch
-              placeholder="Choose a status"
-              style={{ minWidth: "200px" }}
+      render() {
+        const { uiStore, statusStore, priorityStore } = this.props;
+
+        const statusOptions = statusStore!.statuses.map(status => (
+          <Option key={status.name} value={status.name}>
+            {status.name}
+          </Option>
+        ));
+
+        const priorityOptions = priorityStore!.priorities.map(priority => (
+          <Option key={priority.name} value={priority.name}>
+            {priority.name}
+          </Option>
+        ));
+
+        return (
+          <Form colon={false} submit={this.handleSubmit.bind(this)}>
+            <Form.Item
+              label="Name"
+              rules={[
+                { required: true, message: "Please enter a name for this case" }
+              ]}
             >
-              {statusOptions}
-            </Select>
-          )}
-        </Form.Item>
-        <Form.Item label="Priority">
-          {getFieldDecorator("priority", {
-            rules: [{ required: true, message: "Please choose a priority" }]
-          })(
-            <Select
-              showSearch
-              placeholder="Choose a priority"
-              style={{ minWidth: "200px" }}
+              <Input
+                placeholder="Found Ryuk"
+                ref={input => input && input.focus()}
+              />
+            </Form.Item>
+            <Form.Item label="Status">
+              {getFieldDecorator("status", {
+                rules: [{ required: true, message: "Please choose a status" }]
+              })(
+                <Select
+                  showSearch
+                  placeholder="Choose a status"
+                  style={{ minWidth: "200px" }}
+                >
+                  {statusOptions}
+                </Select>
+              )}
+            </Form.Item>
+            <Form.Item
+              label="Priority"
+              rules={[{ required: true, message: "Please choose a priority" }]}
             >
-              {priorityOptions}
-            </Select>
-          )}
-        </Form.Item>
-        <Form.Item>
-          <div style={{ float: "right" }}>
-            <Button
-              style={{ marginRight: "0.5em" }}
-              onClick={() => uiStore!.closeModal()}
-            >
-              Cancel
-            </Button>
-            <Button type="primary" htmlType="submit">
-              Create Case
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-    );
-  }
-}
-
-// ----
-
-// provide our form with validation and MobX access
-const CreateCaseForm = Form.create()(
-  inject(
-    "statusStore",
-    "priorityStore",
-    "uiStore",
-    "allCasesStore"
-  )(observer(DumbCreateCaseForm))
+              <Select
+                showSearch
+                placeholder="Choose a priority"
+                style={{ minWidth: "200px" }}
+              >
+                {priorityOptions}
+              </Select>
+            </Form.Item>
+            <Form.Item>
+              <div style={{ float: "right" }}>
+                <Button
+                  style={{ marginRight: "0.5em" }}
+                  onClick={() => uiStore!.closeModal()}
+                >
+                  Cancel
+                </Button>
+                <Button type="primary" htmlType="submit">
+                  Create Case
+                </Button>
+              </div>
+            </Form.Item>
+          </Form>
+        );
+      }
+    }
+  )
 );
 
 // -----
