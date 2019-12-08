fdc8f751bb1101ab83277897bc62414788744cc5
Veeral Patel <vpatel98@gmail.com>
2019-12-08 15:45:58 -0800

    fix AddMembersForm

--- frontend/src/container/one_case/AddMembersForm.tsx
+++ frontend/src/container/one_case/AddMembersForm.tsx
@@ -1,67 +1,50 @@
 import { Button, Form, Select } from "antd";
-import { FormComponentProps, WrappedFormUtils } from "antd/lib/form/Form";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
 
-interface FormProps {
+interface Props {
   userOptions: React.ReactNode[];
-  form: WrappedFormUtils;
   activeCaseStore?: ActiveCaseStore;
 }
 
-class DumbAddMembersForm extends React.Component<
-  FormProps & FormComponentProps
-> {
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    // prevent page reload
-    event.preventDefault();
+const AddMembersForm = inject("activeCaseStore")(
+  observer(
+    class InnerForm extends React.Component<Props> {
+      render() {
+        const { userOptions, activeCaseStore } = this.props;
 
-    // validate fields in our form
-    const { form, activeCaseStore } = this.props;
-    form.validateFields((errors, values) => {
-      if (!errors) {
-        values.members.forEach((username: string) => {
-          activeCaseStore!.addCaseMember(username);
-        });
-      }
-    });
-  }
-
-  render() {
-    const { userOptions } = this.props;
-    const { getFieldDecorator } = this.props.form;
-
-    return (
-      <Form style={{ display: "flex" }} onSubmit={this.handleSubmit.bind(this)}>
-        <Form.Item style={{ flex: "80%" }}>
-          {getFieldDecorator("members", {
-            rules: [
-              {
-                required: true,
-                message: "Please select at least one user"
-              }
-            ]
-          })(
-            <Select
-              style={{ width: "100%" }}
-              mode="multiple"
-              placeholder="Select users to add"
+        return (
+          <Form
+            style={{ display: "flex" }}
+            onFinish={values => {
+              values.members.forEach((username: string) => {
+                activeCaseStore!.addCaseMember(username);
+              });
+            }}
+          >
+            <Form.Item
+              style={{ flex: "80%" }}
+              rules={[
+                { required: true, message: "Please select at least one user" }
+              ]}
             >
-              {userOptions}
-            </Select>
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType="submit">Add Members</Button>
-        </Form.Item>
-      </Form>
-    );
-  }
-}
-
-const AddMembersForm = Form.create<FormProps & FormComponentProps>()(
-  inject("activeCaseStore")(observer(DumbAddMembersForm))
+              <Select
+                style={{ width: "100%" }}
+                mode="multiple"
+                placeholder="Select users to add"
+              >
+                {userOptions}
+              </Select>
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit">Add Members</Button>
+            </Form.Item>
+          </Form>
+        );
+      }
+    }
+  )
 );
 
 export default AddMembersForm;
