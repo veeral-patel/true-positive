47055d76080cd8d42259ce7f2eeecc96f1e78620
Veeral Patel <vpatel98@gmail.com>
2019-12-08 16:56:45 -0800

    fix IndicatorForm

--- frontend/src/container/one_indicator/IndicatorForm.tsx
+++ frontend/src/container/one_indicator/IndicatorForm.tsx
@@ -1,5 +1,4 @@
 import { Button, Form, Input } from "antd";
-import { FormComponentProps, WrappedFormUtils } from "antd/lib/form/Form";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
@@ -7,60 +6,46 @@ import IIndicator from "ts/interfaces/IIndicator";
 
 const { TextArea } = Input;
 
-interface FormProps {
+interface Props {
   activeIndicator: IIndicator;
-  form: WrappedFormUtils;
   activeCaseStore?: ActiveCaseStore;
 }
 
-class DumbIndicatorForm extends React.Component<
-  FormProps & FormComponentProps
-> {
-  render() {
-    const { activeIndicator } = this.props;
-    const { getFieldDecorator } = this.props.form;
+const IndicatorForm = inject("activeCaseStore")(
+  observer(
+    class InnerForm extends React.Component<Props> {
+      render() {
+        const { activeIndicator, activeCaseStore } = this.props;
 
-    return (
-      <Form onSubmit={this.handleSubmit.bind(this)}>
-        <Form.Item>
-          {getFieldDecorator("indicatorValue", {
-            initialValue: activeIndicator.indicator
-          })(
-            <TextArea
-              rows={5}
-              placeholder="Enter your indicator here"
-              style={{ fontFamily: "monospace" }}
-            />
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType="submit" style={{ float: "right" }}>
-            Update
-          </Button>
-        </Form.Item>
-      </Form>
-    );
-  }
-
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    // prevent page reload
-    event.preventDefault();
-
-    // validate fields in our form
-    const { form, activeCaseStore, activeIndicator } = this.props;
-    form.validateFields((errors: any, values: any) => {
-      if (!errors) {
-        activeCaseStore!.changeIndicatorValue(
-          activeIndicator.id,
-          values.indicatorValue
+        return (
+          <Form
+            onFinish={values => {
+              activeCaseStore!.changeIndicatorValue(
+                activeIndicator.id,
+                values.indicatorValue
+              );
+            }}
+            initialValues={{
+              indicatorValue: activeIndicator.indicator
+            }}
+          >
+            <Form.Item label="Indicator" name="indicatorValue">
+              <TextArea
+                rows={5}
+                placeholder="Enter your indicator here"
+                style={{ fontFamily: "monospace" }}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit" style={{ float: "right" }}>
+                Update
+              </Button>
+            </Form.Item>
+          </Form>
         );
       }
-    });
-  }
-}
-
-const IndicatorForm = Form.create<FormProps & FormComponentProps>()(
-  inject("activeCaseStore")(observer(DumbIndicatorForm))
+    }
+  )
 );
 
 export default IndicatorForm;
