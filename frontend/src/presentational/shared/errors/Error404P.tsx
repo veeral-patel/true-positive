34ef190733145aa0011a4f4c0f2c911e6c70b693
Veeral Patel <vpatel98@gmail.com>
2019-12-13 22:02:42 -0800

    make 404 page work in dark mode

--- frontend/src/presentational/shared/errors/Error404P.tsx
+++ frontend/src/presentational/shared/errors/Error404P.tsx
@@ -1,21 +1,24 @@
 import { navigate, RouteComponentProps } from "@reach/router";
 import { Button, Layout, Result } from "antd";
+import { inject, observer } from "mobx-react";
 import React from "react";
+import UIStore from "stores/UIStore";
 import { paths } from "utils/constants";
 
 const { Content } = Layout;
 
 interface Page404Props extends RouteComponentProps {
   showBackButton?: boolean;
+  uiStore?: UIStore;
 }
 
-const Error404P: React.FC<Page404Props> = ({ showBackButton }) => {
+const Error404P: React.FC<Page404Props> = ({ showBackButton, uiStore }) => {
   const showBackButtonReal =
     showBackButton !== undefined ? showBackButton : true;
   return (
     <Content
       style={{
-        backgroundColor: "#fff",
+        backgroundColor: uiStore!.theme === "LIGHT" ? "#fff" : "#141414",
         padding: 24,
         marginLeft: 24,
         height: "100%"
@@ -26,12 +29,10 @@ const Error404P: React.FC<Page404Props> = ({ showBackButton }) => {
         title="404"
         subTitle="Sorry, the page you visited does not exist."
         extra={
-          showBackButtonReal ? (
+          showBackButtonReal && (
             <Button type="primary" onClick={() => navigate(paths.ROOT_PATH)}>
-              Back Home
+              Go Back Home
             </Button>
-          ) : (
-            <span />
           )
         }
       />
@@ -39,4 +40,4 @@ const Error404P: React.FC<Page404Props> = ({ showBackButton }) => {
   );
 };
 
-export default Error404P;
+export default inject("uiStore")(observer(Error404P));
