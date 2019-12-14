08aa42e9708dab865246fa51fefd5c53bbe9b407
Veeral Patel <vpatel98@gmail.com>
2019-12-13 22:23:46 -0800

    refactor to a function

--- frontend/src/pages/search/SearchPage.tsx
+++ frontend/src/pages/search/SearchPage.tsx
@@ -4,55 +4,53 @@ import React from "react";
 
 interface Props extends RouteComponentProps {}
 
-class SearchPage extends React.Component<Props> {
-  render() {
-    const badgeStyle = {
-      backgroundColor: "#262626",
-      color: "#e8e8e8"
-    };
+function SearchPage(props: Props) {
+  const badgeStyle = {
+    backgroundColor: "#262626",
+    color: "#e8e8e8"
+  };
 
-    return (
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 1 }}>
-          <Menu theme="light" mode="vertical" defaultSelectedKeys={["cases"]}>
-            <Menu.Item key="cases">
-              <div>
-                Cases
-                <div style={{ float: "right" }}>
-                  <Badge count={25} style={badgeStyle} />
-                </div>
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: 1 }}>
+        <Menu theme="light" mode="vertical" defaultSelectedKeys={["cases"]}>
+          <Menu.Item key="cases">
+            <div>
+              Cases
+              <div style={{ float: "right" }}>
+                <Badge count={25} style={badgeStyle} />
               </div>
-            </Menu.Item>
-            <Menu.Item key="tasks">
-              <div>
-                Tasks
-                <div style={{ float: "right" }}>
-                  <Badge count={20} style={badgeStyle} />
-                </div>
+            </div>
+          </Menu.Item>
+          <Menu.Item key="tasks">
+            <div>
+              Tasks
+              <div style={{ float: "right" }}>
+                <Badge count={20} style={badgeStyle} />
               </div>
-            </Menu.Item>
-            <Menu.Item key="indicators">
-              <div>
-                Indicators
-                <div style={{ float: "right" }}>
-                  <Badge count={5} style={badgeStyle} />
-                </div>
+            </div>
+          </Menu.Item>
+          <Menu.Item key="indicators">
+            <div>
+              Indicators
+              <div style={{ float: "right" }}>
+                <Badge count={5} style={badgeStyle} />
               </div>
-            </Menu.Item>
-            <Menu.Item key="comments">
-              <div>
-                Comments
-                <div style={{ float: "right" }}>
-                  <Badge count={0} style={badgeStyle} />
-                </div>
+            </div>
+          </Menu.Item>
+          <Menu.Item key="comments">
+            <div>
+              Comments
+              <div style={{ float: "right" }}>
+                <Badge count={0} style={badgeStyle} />
               </div>
-            </Menu.Item>
-          </Menu>
-        </div>
-        <div style={{ flex: 4 }}></div>
+            </div>
+          </Menu.Item>
+        </Menu>
       </div>
-    );
-  }
+      <div style={{ flex: 4 }}></div>
+    </div>
+  );
 }
 
 export default SearchPage;
