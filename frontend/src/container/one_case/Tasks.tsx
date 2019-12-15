cdff5293c88379f666db746326543d4d52d38bc3
Veeral Patel <vpatel98@gmail.com>
2019-12-15 00:36:19 -0800

    refactor Tasks component to a SFC

--- frontend/src/container/one_case/Tasks.tsx
+++ frontend/src/container/one_case/Tasks.tsx
@@ -6,7 +6,7 @@ import CreateTaskModal from "container/one_case/CreateTaskModal";
 import TaskGroup from "container/one_case/TaskGroup";
 import TaskProgress from "container/shared/tasks/TaskProgress";
 import { inject, observer } from "mobx-react";
-import React from "react";
+import React, { useState } from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
 import UIStore from "stores/UIStore";
 
@@ -18,107 +18,86 @@ interface Props extends RouteComponentProps {
   uiStore?: UIStore;
 }
 
-interface State {
-  openModal: "CREATE_TASK" | null;
-}
+function Tasks({ activeCaseStore, uiStore }: Props) {
+  const [openModal, setOpenModal] = useState<"CREATE_TASK" | null>(null);
 
-export default inject(
-  "activeCaseStore",
-  "uiStore"
-)(
-  observer(
-    class Tasks extends React.Component<Props, State> {
-      constructor(props: Props) {
-        super(props);
-        this.state = {
-          openModal: null
-        };
-      }
+  const activeCase = activeCaseStore!.activeCase;
 
-      render() {
-        const { activeCaseStore, uiStore } = this.props;
-        const { openModal } = this.state;
+  // should always render, since we're catching errors and showing
+  // our spinner above this, as a HOC
+  if (activeCase) {
+    const taskGroups = activeCase.taskGroups.map(group => (
+      <TaskGroup
+        name={group.name}
+        tasks={group.tasks}
+        key={group.id}
+        id={group.id}
+      />
+    ));
 
-        const activeCase = activeCaseStore!.activeCase;
-
-        // should always render, since we're catching errors and showing
-        // our spinner above this, as a HOC
-        if (activeCase) {
-          const taskGroups = activeCase.taskGroups.map(group => (
-            <TaskGroup
-              name={group.name}
-              tasks={group.tasks}
-              key={group.id}
-              id={group.id}
+    return (
+      <div>
+        <Content
+          style={{
+            backgroundColor: uiStore!.theme === "LIGHT" ? "#fff" : "#141414",
+            padding: 24,
+            margin: 0,
+            minHeight: 280
+          }}
+        >
+          {activeCase.totalTaskCount === 0 ? (
+            <Empty
+              description={
+                <div style={{ marginTop: "1em" }}>
+                  <h3>No tasks</h3>
+                  <Paragraph>
+                    A task is a piece of work to be completed in a case.
+                  </Paragraph>
+                  <Button
+                    icon={<PlusOutlined />}
+                    onClick={() => setOpenModal("CREATE_TASK")}
+                  >
+                    Create a task
+                  </Button>
+                </div>
+              }
             />
-          ));
-
-          return (
-            <div>
-              <Content
+          ) : (
+            <>
+              <div
                 style={{
-                  backgroundColor:
-                    uiStore!.theme === "LIGHT" ? "#fff" : "#141414",
-                  padding: 24,
-                  margin: 0,
-                  minHeight: 280
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginBottom: "1em"
                 }}
               >
-                {activeCase.totalTaskCount === 0 ? (
-                  <Empty
-                    description={
-                      <div style={{ marginTop: "1em" }}>
-                        <h3>No tasks</h3>
-                        <Paragraph>
-                          A task is a piece of work to be completed in a case.
-                        </Paragraph>
-                        <Button
-                          icon={<PlusOutlined />}
-                          onClick={() =>
-                            this.setState({ openModal: "CREATE_TASK" })
-                          }
-                        >
-                          Create a task
-                        </Button>
-                      </div>
-                    }
-                  />
-                ) : (
-                  <div>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        marginBottom: "1em"
-                      }}
-                    >
-                      <h3>
-                        Tasks ({activeCase.completedTaskCount}/
-                        {activeCase.totalTaskCount})
-                      </h3>
-                      <TaskProgress
-                        completedTaskCount={activeCase.completedTaskCount}
-                        totalTaskCount={activeCase.totalTaskCount}
-                      />
-                    </div>
-                    <CreateTaskInput
-                      handleEnter={event => {
-                        const newTask = event.currentTarget.value;
-                        activeCaseStore!.createTask(newTask, activeCase.id);
-                      }}
-                    />
-                    {taskGroups}
-                  </div>
-                )}
-              </Content>
-              <CreateTaskModal
-                visible={openModal === "CREATE_TASK"}
-                handleClose={() => this.setState({ openModal: null })}
+                <h3>
+                  Tasks ({activeCase.completedTaskCount}/
+                  {activeCase.totalTaskCount})
+                </h3>
+                <TaskProgress
+                  completedTaskCount={activeCase.completedTaskCount}
+                  totalTaskCount={activeCase.totalTaskCount}
+                />
+              </div>
+              <CreateTaskInput
+                handleEnter={event => {
+                  const newTask = event.currentTarget.value;
+                  activeCaseStore!.createTask(newTask, activeCase.id);
+                }}
               />
-            </div>
-          );
-        }
-      }
-    }
-  )
-);
+              {taskGroups}
+            </>
+          )}
+        </Content>
+        <CreateTaskModal
+          visible={openModal === "CREATE_TASK"}
+          handleClose={() => setOpenModal(null)}
+        />
+      </div>
+    );
+  }
+  return null;
+}
+
+export default inject("activeCaseStore", "uiStore")(observer(Tasks));
