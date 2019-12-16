1aab2cb1424e36f7a64e3ad4b5446bacaacec587
Veeral Patel <vpatel98@gmail.com>
2019-12-15 19:55:19 -0800

    add authz check

--- backend/app/graphql/mutations/create_case_template.rb
+++ backend/app/graphql/mutations/create_case_template.rb
@@ -26,7 +26,7 @@ class Mutations::CreateCaseTemplate < Mutations::BaseMutation
         status_record = find_status_by_name_or_throw_execution_error(status_name: status)
         priority_record = find_priority_by_name_or_throw_execution_error(priority_name: priority)
 
-        # create the case template in memory
+        # create a case template in memory
         case_template = CaseTemplate.new(
             name: name,
             created_by: context[:current_user],
@@ -35,8 +35,12 @@ class Mutations::CreateCaseTemplate < Mutations::BaseMutation
             description: description
         )
 
-        # TODO: authorize this action
+        # authorize this action
+        unless CaseTemplatePolicy.new(context[:current_user], template).create_template?
+            raise GraphQL::ExecutionError, "You are not authorized to create case templates."
+        end
 
+        # save it to the database
         if case_template.save
             {
                 "case_template": case_template
