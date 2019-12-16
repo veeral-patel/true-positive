c763654ff6de917ae6aef57877421b9acb4e4c33
Veeral Patel <vpatel98@gmail.com>
2019-12-15 19:07:35 -0800

    add UpdateCaseTemplateDrawer component

---
+++ frontend/src/container/admin/UpdateCaseTemplateDrawer.tsx
@@ -0,0 +1,25 @@
+import { Drawer } from "antd";
+import CaseTemplateForm from "container/admin/CaseTemplateForm";
+import React from "react";
+
+interface Props {
+  visible: boolean;
+  handleClose: () => void;
+}
+
+function UpdateCaseTemplateDrawer({ visible, handleClose }: Props) {
+  return (
+    <Drawer
+      visible={visible}
+      title={<h3>Update a case template</h3>}
+      width={600}
+      maskClosable={false}
+      keyboard={false}
+      onClose={handleClose}
+    >
+      <CaseTemplateForm handleClose={handleClose} />
+    </Drawer>
+  );
+}
+
+export default UpdateCaseTemplateDrawer;
