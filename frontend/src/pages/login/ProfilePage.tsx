5ea95dae20448ab12e43ada57c9e371d5c124b30
Veeral Patel <vpatel98@gmail.com>
2019-12-17 00:26:20 -0800

    add form to profile page

--- frontend/src/pages/login/ProfilePage.tsx
+++ frontend/src/pages/login/ProfilePage.tsx
@@ -1,10 +1,30 @@
 import { RouteComponentProps } from "@reach/router";
+import { Form, Input } from "antd";
 import React from "react";
 
 interface Props extends RouteComponentProps {}
 
 function ProfilePage(props: Props) {
-  return <span />;
+  return (
+    <Form layout="vertical" colon={false}>
+      <Form.Item
+        name="username"
+        label="Username"
+        rules={[{ required: true, message: "You must have an username" }]}
+      >
+        <Input placeholder="john_steinbeck" />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="Email"
+        rules={[
+          { required: true, message: "You must provide an email address" }
+        ]}
+      >
+        <Input placeholder="john@steinbeck.io" />
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default ProfilePage;
