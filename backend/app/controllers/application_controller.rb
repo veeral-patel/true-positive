ca26ea31355d164b700d3a5a49c94e45e7a48717
Veeral Patel <vpatel98@gmail.com>
2019-12-16 20:58:39 -0800

    fix authentication 500 error

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,13 +7,17 @@ class ApplicationController < ActionController::API
     def authenticate_user
         token_from_request = request.env['HTTP_AUTHORIZATION']&.split&.last
 
-        # Hack: If the provided token is 40 characters, then assume
-        # the user is authenticating via the API, the UI otherwise.
-        if token_from_request.size == 40
-            authenticate_user_api(token_from_request)
+        if token_from_request.nil?
+            render json: { "error": "Please provide an API token in your request's Authorization header." }, status: 401
         else
-            authenticate_user_ui
-        end 
+            # Hack: If the provided token is 40 characters, then assume
+            # the user is authenticating via the API, the UI otherwise.
+            if token_from_request.size == 40
+                authenticate_user_api(token_from_request)
+            else
+                authenticate_user_ui
+            end 
+        end
     end
 
     def authenticate_user_ui
