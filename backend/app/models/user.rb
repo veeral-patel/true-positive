7dea56c2748145689da155a610e80e1d2ce271db
Veeral Patel <vpatel98@gmail.com>
2019-12-16 23:21:44 -0800

    add created_forms to User model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     has_many :created_indicators, foreign_key: "created_by_id", class_name: "Indicator"
     has_many :created_task_templates, foreign_key: "created_by_id", class_name: "TaskTemplate"
     has_many :created_case_templates, foreign_key: "created_by_id", class_name: "CaseTemplate"
+    has_many :created_forms, foreign_key: "created_by_id", class_name: "Form"
     has_many :comments, foreign_key: "created_by_id", class_name: "Comment"
     has_many :api_tokens, foreign_key: "user_id", class_name: "ApiToken"
 
