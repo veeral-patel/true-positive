10a1f87e2b5a2bb040584c77bee3d8b21f12aafb
Veeral Patel <vpatel98@gmail.com>
2019-12-21 15:45:41 -0800

    adjust site settings

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,8 +11,8 @@
 const siteConfig = {
   title: "True Positive", // Title for your website.
   tagline: "",
-  url: "https://truepositive.app/docs", // Your website URL
-  baseUrl: "/", // Base URL for your project */
+  url: "https://truepositive.app", // Your website URL
+  baseUrl: "/docs", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
