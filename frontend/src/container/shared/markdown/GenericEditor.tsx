f48273473a6469f0a8c6db8a4894b0d3d39e7334
Veeral Patel <vpatel98@gmail.com>
2019-12-22 15:27:55 -0800

    fetch users from the server

--- frontend/src/container/shared/markdown/GenericEditor.tsx
+++ frontend/src/container/shared/markdown/GenericEditor.tsx
@@ -1,8 +1,11 @@
 import converter from "container/shared/markdown/converter";
+import client from "createApolloClient";
 import { inject, observer } from "mobx-react";
+import GET_USERS from "queries/getUsers";
 import * as React from "react";
 import ReactMde, { Suggestion } from "react-mde";
 import UIStore from "stores/UIStore";
+import IUser from "ts/interfaces/IUser";
 
 interface Props {
   value?: string;
@@ -10,30 +13,38 @@ interface Props {
   uiStore?: UIStore;
 }
 
+interface IUserData {
+  users: IUser[];
+}
+
+// loadSuggestions retrieves a list of users from the server and formats
+// the list into a format react-mde can use
 function loadSuggestions(text: string): Promise<Suggestion[]> {
-  return new Promise((accept, reject) => {
-    setTimeout(() => {
-      const suggestions = [
-        {
-          preview: "Andre",
-          value: "@andre"
-        },
-        {
-          preview: "Angela",
-          value: "@angela"
-        },
-        {
-          preview: "David",
-          value: "@david"
-        },
-        {
-          preview: "Louise",
-          value: "@louise"
+  return (
+    client
+      .query<IUserData>({ query: GET_USERS })
+      .then(result => {
+        // Get the list of all the users, from our GraphQL query
+        const allUsers = result.data.users;
+
+        // Format the list of users into a list of suggestions
+        var formattedUsers = [];
+        for (const user of allUsers) {
+          formattedUsers.push({
+            value: `@${user.username}`,
+            preview: user.username
+          });
         }
-      ].filter(i => i.value.toLowerCase().includes(text.toLowerCase()));
-      accept(suggestions);
-    }, 100);
-  });
+
+        // Return the suggestions where the user's username includes the typed in text
+        // as a substring
+        return formattedUsers.filter(user =>
+          user.preview.toLowerCase().includes(text.toLowerCase())
+        );
+      })
+      // If we couldn't get the list of users, provide no suggestions to the user
+      .catch(error => [])
+  );
 }
 
 function GenericEditor({ value, onChange, uiStore }: Props) {
