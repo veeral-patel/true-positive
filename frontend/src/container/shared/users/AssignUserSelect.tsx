523f2317ff3fb3ebbddd36d48838d3e84efbe215
Veeral Patel <vpatel98@gmail.com>
2019-12-25 14:59:10 -0800

    refactor AssignUserSelect to hooks

--- frontend/src/container/shared/users/AssignUserSelect.tsx
+++ frontend/src/container/shared/users/AssignUserSelect.tsx
@@ -1,52 +1,62 @@
+import { useQuery } from "@apollo/react-hooks";
 import { Select } from "antd";
-import { inject, observer } from "mobx-react";
+import GET_USERS from "queries/getUsers";
 import React from "react";
-import UserStore from "stores/UserStore";
+import IUser from "ts/interfaces/IUser";
 import { NO_ASSIGNED_USER } from "utils/constants";
 
 const { Option } = Select;
 
 interface Props {
   handleSelect: (userId: any) => void;
-  userStore?: UserStore;
 }
 
-export default inject("userStore")(
-  observer(
-    class AssignUserSelect extends React.Component<Props> {
-      componentDidMount() {
-        const { userStore } = this.props;
-        userStore!.loadUsers();
-      }
+interface UserData {
+  users: IUser[];
+}
 
-      render() {
-        const { userStore, handleSelect } = this.props;
+function AssignUserSelect({ handleSelect }: Props) {
+  const { loading, error, data } = useQuery<UserData>(GET_USERS);
 
-        // generate a list of options
-        const options = userStore!.users
-          .map(user => (
-            <Option key={user.username} value={user.username}>
-              {user.username}
-            </Option>
-          ))
-          .concat(
-            <Option key={NO_ASSIGNED_USER} value={NO_ASSIGNED_USER}>
-              N/A
-            </Option>
-          );
+  // generate a list of options
+  var options: Object[] = [];
+  if (loading) {
+    options = [
+      <Option key="loading" value="loading">
+        Loading...
+      </Option>
+    ];
+  } else if (data) {
+    options = data.users
+      .map(user => (
+        <Option key={user.username} value={user.username}>
+          {user.username}
+        </Option>
+      ))
+      .concat(
+        <Option key={NO_ASSIGNED_USER} value={NO_ASSIGNED_USER}>
+          N/A
+        </Option>
+      );
+  } else {
+    options = [
+      <Option key="error" value="error">
+        Error loading users
+      </Option>
+    ];
+  }
 
-        // render our component
-        return (
-          <Select
-            showSearch
-            placeholder="Choose a user"
-            style={{ minWidth: "200px" }}
-            onSelect={userId => handleSelect(userId)}
-          >
-            {options}
-          </Select>
-        );
-      }
-    }
-  )
-);
+  // render our component
+  return (
+    <Select
+      showSearch
+      placeholder="Choose a user"
+      style={{ minWidth: "200px" }}
+      onSelect={userId => handleSelect(userId)}
+    >
+      {options}
+    </Select>
+  );
+}
+
+export default AssignUserSelect;
