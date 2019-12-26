882e3153295a1e3d94e5c45cdc07ca061b295236
Veeral Patel <vpatel98@gmail.com>
2019-12-25 21:58:59 -0800

    add commentCount field to search mutation

--- backend/app/graphql/mutations/search.rb
+++ backend/app/graphql/mutations/search.rb
@@ -5,6 +5,10 @@ class Mutations::Search < Mutations::BaseMutation
         description "Your search query."
     end
 
+    field :comment_count, Int, null: true do
+        description "The number of comments matching this search query."
+    end
+
     field :comments, [Types::CommentType], null: true do
         description "Comments matching this search query."
     end
@@ -14,6 +18,7 @@ class Mutations::Search < Mutations::BaseMutation
         filtered_comments = CommentPolicy::Scope.new(context[:current_user], matching_comments).resolve
 
         {
+            "comment_count": filtered_comments.count,
             "comments": filtered_comments
         }
     end
