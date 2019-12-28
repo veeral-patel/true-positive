721446ed80ff7e68d840172f78876b46298fb38b
Veeral Patel <vpatel98@gmail.com>
2019-12-27 23:02:36 -0800

    remove half-done audits section

--- frontend/src/pages/cases/CasePage.tsx
+++ frontend/src/pages/cases/CasePage.tsx
@@ -1,5 +1,5 @@
 import { RouteComponentProps, Router } from "@reach/router";
-import { Empty, Layout, Typography } from "antd";
+import { Layout, Typography } from "antd";
 import CaseSider from "container/one_case/CaseSider";
 import Forms from "container/one_case/Forms";
 import HandleErrorAndLoading from "container/one_case/HandleErrorAndLoading";
@@ -19,7 +19,6 @@ import Page404 from "presentational/shared/errors/Error404P";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
 import UIStore from "stores/UIStore";
-import formatISO8601 from "utils/formatISO8601";
 
 const { Text } = Typography;
 const { Content } = Layout;
@@ -110,72 +109,18 @@ export default inject(
                     </Router>
                   )}
                 </div>
-                <div style={{ display: "flex" }}>
-                  <div
-                    style={{ flex: 5, overflow: "hidden", borderRadius: "8px" }}
-                  >
-                    <Router>
-                      <Info path="/" />
-                      <Info path="/info" />
-                      <Members path="/members" />
-                      <Indicators path="/indicators" />
-                      <Forms path="/forms" />
-                      <Tasks path="/tasks" />
-                      <OneTask path="/tasks/:taskId" />
-                      <OneIndicator path="/indicators/:indicatorId" />
-                      <Page404 default showBackButton={false} />
-                    </Router>
-                  </div>
-                  <div className="activityPaneContainer">
-                    <Content
-                      style={{
-                        backgroundColor:
-                          uiStore!.theme === "LIGHT" ? "#fff" : "#141414",
-                        padding: 16,
-                        marginLeft: 24,
-                        height: "100%",
-                        borderRadius: "8px"
-                      }}
-                    >
-                      <Text
-                        type="secondary"
-                        style={{ textTransform: "uppercase" }}
-                      >
-                        History
-                      </Text>
-                      {activeCaseStore!.activeCase &&
-                        (activeCaseStore!.activeCase.audits.length === 0 ? (
-                          <Empty
-                            description={
-                              <div>
-                                <p>No History</p>
-                                <p style={{ color: "#bfbfbf" }}>
-                                  Changes you make to this case will appear
-                                  here.
-                                </p>
-                              </div>
-                            }
-                          />
-                        ) : (
-                          <div style={{ marginTop: "1em" }}>
-                            {/* Only render the first 15 audits  */}
-                            {activeCaseStore!.activeCase.audits
-                              .filter((_, index) => index < 15)
-                              .map(audit => (
-                                <div style={{ marginBottom: "1.25em" }}>
-                                  <Text type="secondary">
-                                    {audit.readableMessage}
-                                  </Text>
-                                  &nbsp;&nbsp;
-                                  <Text disabled>
-                                    {formatISO8601(audit.createdAt)}
-                                  </Text>
-                                </div>
-                              ))}
-                          </div>
-                        ))}
-                    </Content>
-                  </div>
+                <div style={{ overflow: "hidden", borderRadius: "8px" }}>
+                  <Router>
+                    <Info path="/" />
+                    <Info path="/info" />
+                    <Members path="/members" />
+                    <Indicators path="/indicators" />
+                    <Forms path="/forms" />
+                    <Tasks path="/tasks" />
+                    <OneTask path="/tasks/:taskId" />
+                    <OneIndicator path="/indicators/:indicatorId" />
+                    <Page404 default showBackButton={false} />
+                  </Router>
                 </div>
               </HandleErrorAndLoading>
             </Layout>
