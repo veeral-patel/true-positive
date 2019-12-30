a131a0a16fa3aade344efcefc336d10358bb91f0
Veeral Patel <vpatel98@gmail.com>
2019-12-29 16:52:07 -0800

    whitelist lvh.me:3000

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'console.staging.truepositive.app'
+    origins 'localhost:3000', 'console.staging.truepositive.app', /^(.*\.|)lvh\.me:3000$/
 
     resource '*',
       headers: :any,
