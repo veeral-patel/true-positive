2ac1f86f139808e65f0ad942b99374bc2c424db8
Veeral Patel <vpatel98@gmail.com>
2019-12-31 23:49:13 -0800

    remove redundant migrations

--- backend/db/migrate/20200101055354_add_identifier_to_tenants.rb
+++
@@ -1,5 +0,0 @@
-class AddIdentifierToTenants < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tenants, :identifier, :string
-  end
-end
--- backend/db/migrate/20200101074521_remove_identifier_from_tenants.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIdentifierFromTenants < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tenants, :identifier, :string
-  end
-end
