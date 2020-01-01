42406bba3cd418840e49c72becc1c3763ff72450
Veeral Patel <vpatel98@gmail.com>
2020-01-01 11:49:51 -0800

    delete seeds

--- backend/db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Status.create(name: "Open")
-Status.create(name: "In Progress")
-Status.create(name: "Closed")
-
-Priority.create(name: "High")
-Priority.create(name: "Medium")
-Priority.create(name: "Low")
