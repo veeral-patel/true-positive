b7bfdc8c627ee5a6dcb1916586cd2448746d0f4b
Veeral Patel <vpatel98@gmail.com>
2020-01-01 12:09:03 -0800

    add else clause

--- frontend/src/utils/getApiEndpoint.ts
+++ frontend/src/utils/getApiEndpoint.ts
@@ -1,6 +1,6 @@
 function getApiEndpoint(): string {
   if (process.env.NODE_ENV === "development") return "http://localhost:4000";
-  return "https://api.truepositive.app";
+  else return "https://api.truepositive.app";
 }
 
 export { getApiEndpoint };
