5909d99744879402690791400e75a083af4e73da
Veeral Patel <vpatel98@gmail.com>
2020-01-01 12:08:41 -0800

    finish getApiEndpoint

--- frontend/src/utils/getApiEndpoint.ts
+++ frontend/src/utils/getApiEndpoint.ts
@@ -1,12 +1,6 @@
-function getApiEndpointInDev(): string {
-  return "http://localhost:4000";
-}
-
 function getApiEndpoint(): string {
-  if (process.env.NODE_ENV === "development") return getApiEndpointInDev();
-  else {
-    throw "Update getApiEndpoint in getApiEndpoint.ts to support non-development environments!";
-  }
+  if (process.env.NODE_ENV === "development") return "http://localhost:4000";
+  return "https://api.truepositive.app";
 }
 
 export { getApiEndpoint };
