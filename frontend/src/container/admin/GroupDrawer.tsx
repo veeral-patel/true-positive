03dd2d96affc184fc4b88c8dfc126824b9c842f2
Veeral Patel <vpatel98@gmail.com>
2020-01-02 00:22:38 -0800

    retrieve data to populate drawer

--- frontend/src/container/admin/GroupDrawer.tsx
+++ frontend/src/container/admin/GroupDrawer.tsx
@@ -1,5 +1,18 @@
-import { Drawer } from "antd";
+import { useQuery } from "@apollo/react-hooks";
+import { Drawer, Spin } from "antd";
+import gql from "graphql-tag";
+import Error from "presentational/shared/errors/Error";
 import React from "react";
+import IGroup from "ts/interfaces/IGroup";
+
+const GET_ONE_GROUP = gql`
+  query getOneGroup($id: ID!) {
+    group(id: $id) {
+      id
+      name
+    }
+  }
+`;
 
 interface Props {
   visible: boolean;
@@ -8,7 +21,33 @@ interface Props {
 }
 
 function GroupDrawer({ visible, onClose, groupId }: Props) {
-  return <Drawer visible={visible} onClose={onClose} width={600} />;
+  const { loading, error, data } = useQuery<IGroup>(GET_ONE_GROUP, {
+    variables: {
+      id: groupId
+    }
+  });
+
+  let drawerContent: React.ReactNode = null;
+
+  if (loading) drawerContent = <Spin />;
+  else if (error) {
+    drawerContent = (
+      <Error title="Could not retrieve case" subtitle={error.message} />
+    );
+  } else if (data) {
+    drawerContent = null;
+  }
+
+  return (
+    <Drawer
+      visible={visible}
+      onClose={onClose}
+      width={600}
+      title={<h3>Update group</h3>}
+    >
+      {drawerContent}
+    </Drawer>
+  );
 }
 
 export default GroupDrawer;
