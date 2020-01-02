2ccf005c30c3f5b1af76a9ef79b6aaa6c6d4af43
Veeral Patel <vpatel98@gmail.com>
2020-01-01 20:46:11 -0800

    fix cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'console.staging.truepositive.app', /^(.*\.|)lvh\.me:3000$/
+    origins 'localhost:3000', 'console.truepositive.app'
 
     resource '*',
       headers: :any,
