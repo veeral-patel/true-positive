2aa1a5ab8bcf2da128cc07623f99a9e2d4507724
Veeral Patel <vpatel98@gmail.com>
2020-01-02 01:03:25 -0800

    add empty state

--- frontend/src/container/admin/GroupDrawer.tsx
+++ frontend/src/container/admin/GroupDrawer.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/react-hooks";
-import { Drawer, Form, Input, Spin } from "antd";
+import { Drawer, Empty, Form, Input, List, Spin } from "antd";
+import Paragraph from "antd/lib/typography/Paragraph";
 import gql from "graphql-tag";
 import Error from "presentational/shared/errors/Error";
 import React from "react";
@@ -10,6 +11,11 @@ const GET_ONE_GROUP = gql`
     group(id: $id) {
       id
       name
+      userCount
+      users {
+        username
+        email
+      }
     }
   }
 `;
@@ -40,15 +46,47 @@ function GroupDrawer({ visible, onClose, groupId }: Props) {
     );
   } else if (data) {
     drawerContent = (
-      <Form
-        colon={false}
-        layout="vertical"
-        initialValues={{ name: data.group.name }}
-      >
-        <Form.Item label="Name" name="name">
-          <Input placeholder="AppSec" disabled />
-        </Form.Item>
-      </Form>
+      <>
+        <div>
+          <Form
+            colon={false}
+            layout="vertical"
+            initialValues={{ name: data.group.name }}
+          >
+            <Form.Item label="Name" name="name">
+              <Input placeholder="AppSec" disabled />
+            </Form.Item>
+          </Form>
+        </div>
+        <div>
+          <Paragraph>Users ({data.group.userCount})</Paragraph>
+          {data.group.userCount === 0 ? (
+            <Empty
+              description={
+                <div style={{ marginTop: "2em" }}>
+                  <h3>No users</h3>
+                  <Paragraph>Add users to this group above</Paragraph>
+                </div>
+              }
+            />
+          ) : (
+            <List
+              bordered
+              dataSource={data.group.users}
+              itemLayout="horizontal"
+              pagination={{ position: "bottom" }}
+              renderItem={user => (
+                <List.Item>
+                  <List.Item.Meta
+                    title={user.username}
+                    description={user.email}
+                  />
+                </List.Item>
+              )}
+            />
+          )}
+        </div>
+      </>
     );
   }
 
