bd04e1582887ab50ee44029f54769731144a4118
Veeral Patel <vpatel98@gmail.com>
2020-01-01 21:44:54 -0800

    make tenant a foreign key

--- backend/db/migrate/20200102054040_add_tenant_to_cases.rb
+++ backend/db/migrate/20200102054040_add_tenant_to_cases.rb
@@ -1,5 +1,5 @@
 class AddTenantToCases < ActiveRecord::Migration[5.2]
   def change
-    add_reference :cases, :tenant
+    add_reference :cases, :tenant, foreign_key: { to_table: :tenants }
   end
 end
