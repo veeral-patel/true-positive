cae6c36d68d1dea5fac1c8646b18164be1e6fe04
Veeral Patel <vpatel98@gmail.com>
2020-01-02 22:29:13 -0800

    can delete groups now

--- backend/app/models/group.rb
+++ backend/app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ApplicationRecord
 
     validates :name, presence: true
 
-    has_many :group_users
+    has_many :group_users, dependent: :destroy
     has_many :users, through: :group_users
 
     def user_count
