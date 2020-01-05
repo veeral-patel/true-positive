94a5a71b00d294b4b77ba59866b0a1a5adb70d23
Veeral Patel <vpatel98@gmail.com>
2020-01-04 18:38:29 -0800

    add resolve method

--- backend/app/graphql/mutations/remove_user_from_case_template.rb
+++ backend/app/graphql/mutations/remove_user_from_case_template.rb
@@ -8,4 +8,19 @@ class Mutations::RemoveUserFromCaseTemplate < Mutations::BaseMutation
     argument :case_template_id, ID, required: true do
         description "ID of the case template to add this user to."
     end
+
+    field :case_template, Types::CaseTemplateType, null: true do
+        description "The updated case template."
+    end
+
+    def resolve(username:, case_template_id:)
+        # find the user and case template
+        user = find_user_or_throw_execution_error(username: username)
+        case_template = find_case_template_or_throw_execution_error(id: case_template_id)
+
+        # authorize this action
+        unless CaseTemplatePolicy.new(context[:current_user], case_template).update_template?
+            raise GraphQL::ExecutionError, "You are not authorized to update this template."
+        end
+    end
 end
