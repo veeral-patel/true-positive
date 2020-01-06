23e444113c08ffe28225c5d77f5f0cad79d23beb
Veeral Patel <vpatel98@gmail.com>
2020-01-06 00:16:58 -0800

    set up set_as_primary

--- backend/app/models/status.rb
+++ backend/app/models/status.rb
@@ -1,11 +1,19 @@
 class Status < ApplicationRecord
     acts_as_tenant :tenant
 
+    include SetAsPrimary
+    set_as_primary :default_status, owner_key: :tenant, force_primary: false
+
     validates :name, presence: true
 
     validates_uniqueness_to_tenant :name
 
     def to_s
         self.name
+    end
+
+    def self.default_status
+        # Retrieves our tenant's default tenant (or returns nil if there's no default status)
+        Status.find_by(default_status: true)
     end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_04_210900) do
+ActiveRecord::Schema.define(version: 2020_01_06_073340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -210,6 +210,7 @@ ActiveRecord::Schema.define(version: 2020_01_04_210900) do
     t.bigint "tenant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "default_status", default: false, null: false
     t.index ["tenant_id"], name: "index_statuses_on_tenant_id"
   end
 
