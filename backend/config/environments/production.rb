d18e2b5fa4017619813fad1fc6fb6454c1b0e008
Veeral Patel <vpatel98@gmail.com>
2020-01-05 21:34:41 -0800

    add Sendgrid settings

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -54,7 +54,23 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "v1_#{Rails.env}"
 
+  # Settings for ActionMailer
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'truepositive.app',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
