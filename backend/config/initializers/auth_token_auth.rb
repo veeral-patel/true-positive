b2707c7bfefc404601206ac8e7400e35a4ac8a9d
Veeral Patel <vpatel98@gmail.com>
2020-01-05 18:23:15 -0800

    set invitations url

--- backend/config/initializers/auth_token_auth.rb
+++ backend/config/initializers/auth_token_auth.rb
@@ -2,4 +2,10 @@ RailsJwtAuth.setup do |config|
     config.auth_field_name = 'username'
 
     config.simultaneous_sessions = 1000
+
+    if Rails.env === "development"
+        config.invitations_url = "http://localhost:3000"
+    elsif Rails.env === "production"
+        config.invitations_url = "https://console.truepositive.app"
+    end
 end
