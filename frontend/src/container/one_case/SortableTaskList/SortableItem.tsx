20cfc98ab135d3593fe8d67e51d31b74ad650eda
Veeral Patel <vpatel98@gmail.com>
2020-01-10 22:55:19 -0800

    update UI after deleting/updating a task

--- frontend/src/container/one_case/SortableTaskList/SortableItem.tsx
+++ frontend/src/container/one_case/SortableTaskList/SortableItem.tsx
@@ -19,10 +19,12 @@ import {
 } from "antd";
 import { ApolloError } from "apollo-boost";
 import UserSelect from "container/shared/users/UserSelect";
+import { inject, observer } from "mobx-react";
 import DELETE_A_TASK from "mutations/deleteTask";
 import UPDATE_TASK from "mutations/updateTask";
 import React, { useState } from "react";
 import { SortableElement } from "react-sortable-hoc";
+import ActiveCaseStore from "stores/ActiveCaseStore";
 import ITask from "ts/interfaces/ITask";
 import { getPathToATask } from "utils/pathHelpers";
 import truncateString from "utils/truncateString";
@@ -30,117 +32,126 @@ import truncateString from "utils/truncateString";
 interface Props {
   task: ITask;
   markTaskAsDone: (taskId: number, done: boolean) => void;
+  activeCaseStore?: ActiveCaseStore;
 }
 
-const SortableItem = SortableElement(({ task, markTaskAsDone }: Props) => {
-  const [expanded, setExpanded] = useState(false);
+const SortableItem = SortableElement(
+  ({ task, markTaskAsDone, activeCaseStore }: Props) => {
+    const [expanded, setExpanded] = useState(false);
 
-  const [deleteTask] = useMutation(DELETE_A_TASK, {
-    onCompleted: function() {
-      message.success("Deleted the task");
-    },
-    onError: function(error: ApolloError) {
-      notification.error({
-        message: "Could not delete the task",
-        description: error.message
-      });
-    }
-  });
+    const [deleteTask] = useMutation(DELETE_A_TASK, {
+      onCompleted: function() {
+        message.success("Deleted the task");
+        activeCaseStore!.loadActiveCase();
+      },
+      onError: function(error: ApolloError) {
+        notification.error({
+          message: "Could not delete the task",
+          description: error.message
+        });
+      }
+    });
 
-  const [updateTask] = useMutation(UPDATE_TASK, {
-    onCompleted: function() {
-      message.success("Updated task");
-    },
-    onError: function(error: ApolloError) {
-      notification.error({
-        message: "Failed to update task",
-        description: error.message
-      });
-    }
-  });
+    const [updateTask] = useMutation(UPDATE_TASK, {
+      onCompleted: function() {
+        message.success("Updated task");
+        activeCaseStore!.loadActiveCase();
+      },
+      onError: function(error: ApolloError) {
+        notification.error({
+          message: "Failed to update task",
+          description: error.message
+        });
+      }
+    });
 
-  return (
-    <List.Item
-      style={{ cursor: "grab" }}
-      actions={[
-        <div>
-          {expanded ? (
-            <Tooltip title="Collapse row">
-              <Button
-                icon={<MinusSquareOutlined />}
-                style={{ border: "none" }}
-                onClick={() => setExpanded(false)}
-              />
-            </Tooltip>
-          ) : (
-            <Tooltip title="Expand row">
-              <Button
-                icon={<PlusSquareOutlined />}
-                style={{ border: "none" }}
-                onClick={() => setExpanded(true)}
-              />
-            </Tooltip>
-          )}
-        </div>,
-        <Tooltip title={`${task.commentCount} comment(s)`}>
-          <MessageOutlined /> {task.commentCount}
-        </Tooltip>,
-        <Popover
-          title="Assign task"
-          content={
-            <UserSelect
-              forAssigning={true}
-              value={task.assignedTo && task.assignedTo.username}
-              onChange={username =>
-                updateTask({
-                  variables: {
-                    input: {
-                      taskId: task.id,
-                      assignedTo: username
+    return (
+      <List.Item
+        style={{ cursor: "grab" }}
+        actions={[
+          <div>
+            {expanded ? (
+              <Tooltip title="Collapse row">
+                <Button
+                  icon={<MinusSquareOutlined />}
+                  style={{ border: "none" }}
+                  onClick={() => setExpanded(false)}
+                />
+              </Tooltip>
+            ) : (
+              <Tooltip title="Expand row">
+                <Button
+                  icon={<PlusSquareOutlined />}
+                  style={{ border: "none" }}
+                  onClick={() => setExpanded(true)}
+                />
+              </Tooltip>
+            )}
+          </div>,
+          <Tooltip title={`${task.commentCount} comment(s)`}>
+            <MessageOutlined /> {task.commentCount}
+          </Tooltip>,
+          <Popover
+            title="Assign task"
+            content={
+              <UserSelect
+                forAssigning={true}
+                value={task.assignedTo && task.assignedTo.username}
+                onChange={username =>
+                  updateTask({
+                    variables: {
+                      input: {
+                        taskId: task.id,
+                        assignedTo: username
+                      }
                     }
-                  }
-                })
-              }
-            />
-          }
-        >
-          <Button style={{ border: "none" }} icon={<UserAddOutlined />} />
-        </Popover>,
-        <Popconfirm
-          title="Delete this task?"
-          onConfirm={() =>
-            deleteTask({
-              variables: {
-                input: {
-                  id: task.id
+                  })
                 }
-              }
-            })
+              />
+            }
+          >
+            <Button style={{ border: "none" }} icon={<UserAddOutlined />} />
+          </Popover>,
+          <Popconfirm
+            title="Delete this task?"
+            onConfirm={() =>
+              deleteTask({
+                variables: {
+                  input: {
+                    id: task.id
+                  }
+                }
+              })
+            }
+          >
+            <Button icon={<DeleteOutlined />} style={{ border: "none" }} />
+          </Popconfirm>
+        ]}
+      >
+        <List.Item.Meta
+          title={
+            <>
+              <Checkbox
+                style={{ marginRight: "1.0em" }}
+                defaultChecked={task.done}
+                onChange={event =>
+                  markTaskAsDone(task.id, event.target.checked)
+                }
+              />
+              <a
+                onClick={() => navigate(getPathToATask(task.case.id, task.id))}
+              >
+                {truncateString(task.name, 75)}
+              </a>
+            </>
           }
-        >
-          <Button icon={<DeleteOutlined />} style={{ border: "none" }} />
-        </Popconfirm>
-      ]}
-    >
-      <List.Item.Meta
-        title={
-          <>
-            <Checkbox
-              style={{ marginRight: "1.0em" }}
-              defaultChecked={task.done}
-              onChange={event => markTaskAsDone(task.id, event.target.checked)}
-            />
-            <a onClick={() => navigate(getPathToATask(task.case.id, task.id))}>
-              {truncateString(task.name, 75)}
-            </a>
-          </>
-        }
-        description={
-          task.assignedTo && `Assigned to ${task.assignedTo.username}`
-        }
-      />
-    </List.Item>
-  );
-});
+          description={
+            task.assignedTo && `Assigned to ${task.assignedTo.username}`
+          }
+        />
+      </List.Item>
+    );
+  }
+);
 
-export default SortableItem;
+export default inject("activeCaseStore")(observer(SortableItem));
