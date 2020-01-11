fa99209e8118c58564423ed479b945e96a0d904c
Veeral Patel <vpatel98@gmail.com>
2020-01-10 21:51:56 -0800

    add expand icon

--- frontend/src/container/one_case/SortableTaskList/SortableItem.tsx
+++ frontend/src/container/one_case/SortableTaskList/SortableItem.tsx
@@ -1,6 +1,7 @@
 import {
   DeleteOutlined,
   MessageOutlined,
+  MinusSquareOutlined,
   PlusSquareOutlined
 } from "@ant-design/icons";
 import { useMutation } from "@apollo/react-hooks";
@@ -8,7 +9,6 @@ import { navigate } from "@reach/router";
 import {
   Button,
   Checkbox,
-  Divider,
   List,
   message,
   notification,
@@ -17,7 +17,7 @@ import {
 } from "antd";
 import { ApolloError } from "apollo-boost";
 import DELETE_A_TASK from "mutations/deleteTask";
-import React from "react";
+import React, { useState } from "react";
 import { SortableElement } from "react-sortable-hoc";
 import ITask from "ts/interfaces/ITask";
 import { getPathToATask } from "utils/pathHelpers";
@@ -29,6 +29,8 @@ interface Props {
 }
 
 const SortableItem = SortableElement(({ task, markTaskAsDone }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+
   const [deleteTask] = useMutation(DELETE_A_TASK, {
     onCompleted: function() {
       message.success("Deleted the task");
@@ -42,7 +44,47 @@ const SortableItem = SortableElement(({ task, markTaskAsDone }: Props) => {
   });
 
   return (
-    <List.Item style={{ cursor: "grab" }}>
+    <List.Item
+      style={{ cursor: "grab" }}
+      actions={[
+        <div>
+          {expanded ? (
+            <Tooltip title="Collapse row">
+              <Button
+                icon={<MinusSquareOutlined />}
+                style={{ border: "none" }}
+                onClick={() => setExpanded(false)}
+              />
+            </Tooltip>
+          ) : (
+            <Tooltip title="Expand row">
+              <Button
+                icon={<PlusSquareOutlined />}
+                style={{ border: "none" }}
+                onClick={() => setExpanded(true)}
+              />
+            </Tooltip>
+          )}
+        </div>,
+        <Tooltip title={`${task.commentCount} comment(s)`}>
+          <MessageOutlined /> {task.commentCount}
+        </Tooltip>,
+        <Popconfirm
+          title="Delete this task?"
+          onConfirm={() =>
+            deleteTask({
+              variables: {
+                input: {
+                  id: task.id
+                }
+              }
+            })
+          }
+        >
+          <Button icon={<DeleteOutlined />} style={{ border: "none" }} />
+        </Popconfirm>
+      ]}
+    >
       <List.Item.Meta
         title={
           <>
@@ -60,28 +102,6 @@ const SortableItem = SortableElement(({ task, markTaskAsDone }: Props) => {
           task.assignedTo && `Assigned to ${task.assignedTo.username}`
         }
       />
-      <Tooltip title="Expand row">
-        <Button icon={<PlusSquareOutlined />} style={{ border: "none" }} />
-      </Tooltip>
-      <Divider type="vertical" />
-      <Tooltip title={`${task.commentCount} comment(s)`}>
-        <MessageOutlined /> {task.commentCount}
-      </Tooltip>
-      <Divider type="vertical" />
-      <Popconfirm
-        title="Delete this task?"
-        onConfirm={() =>
-          deleteTask({
-            variables: {
-              input: {
-                id: task.id
-              }
-            }
-          })
-        }
-      >
-        <Button icon={<DeleteOutlined />} style={{ border: "none" }} />
-      </Popconfirm>
     </List.Item>
   );
 });
