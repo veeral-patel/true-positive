597f8d9c90787e19c65efb0c4151456e83bee7ea
Veeral Patel <vpatel98@gmail.com>
2020-01-12 12:20:31 -0800

    require case_template in CreateCaseEmailAddress model

--- backend/app/models/create_case_email_address.rb
+++ backend/app/models/create_case_email_address.rb
@@ -2,4 +2,7 @@ class CreateCaseEmailAddress < ApplicationRecord
   acts_as_tenant :tenant
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+  validates :case_template, presence: true
+
+  belongs_to :case_template
 end
