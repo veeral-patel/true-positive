b907ddfe7646c470e0eb81f249f8983d288e7fee
Veeral Patel <vpatel98@gmail.com>
2020-01-11 22:15:55 -0800

    run prettier

--- frontend/src/container/one_case/TaskGroup/Heading.tsx
+++ frontend/src/container/one_case/TaskGroup/Heading.tsx
@@ -1,16 +1,14 @@
-import { DeleteOutlined, UserAddOutlined } from "@ant-design/icons";
+import { DeleteOutlined } from "@ant-design/icons";
 import { useMutation } from "@apollo/react-hooks";
 import {
   Button,
   message,
   Modal,
   notification,
-  Popover,
   Tooltip,
   Typography
 } from "antd";
 import { ApolloError } from "apollo-boost";
-import UserSelect from "container/shared/users/UserSelect";
 import { inject, observer } from "mobx-react";
 import DELETE_A_TASK_GROUP from "mutations/deleteTaskGroup";
 import UPDATE_TASK_GROUP from "mutations/updateTaskGroup";
