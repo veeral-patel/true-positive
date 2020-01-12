f5c5d083cdce8566c1cf0567b7f3aa33c178f1f6
Veeral Patel <vpatel98@gmail.com>
2020-01-11 21:09:10 -0800

    reenable api only

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -32,6 +32,6 @@ module V1
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
+    config.api_only = true
   end
 end
