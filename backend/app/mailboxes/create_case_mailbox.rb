36b55750e865eaba31f28711d9f8000dc00dcd8a
Veeral Patel <vpatel98@gmail.com>
2020-01-12 12:11:50 -0800

    start writing CreateCaseMailbox

--- backend/app/mailboxes/create_case_mailbox.rb
+++ backend/app/mailboxes/create_case_mailbox.rb
@@ -1,27 +1,12 @@
 class CreateCaseMailbox < ApplicationMailbox
   def process
-    binding.pry
-    # get the recipient of the email
-
-    # look for the recipient in our inbound_addresses database table
-
-    # if we find it, then ---
-
-      # get the raw email from the email.
-      # questions: is the raw email a file? do I need to upload it somewhere to avoid incineration? should I never incinerate?
-
-      # create a new case
-
-      # attach the raw email to the case
-
-      # get a link to the case
-
-      # reply to the email saying thank you for your email, here is the
-      # link to the case that's been created, and add a line saying "Powered by True Positive"
-
-    # if we don't find it, then ---
-
-        # reply to the email saying the email address you sent to doesn't correspond
-        # to any inbound email address in our database
+    # Look for each recipient email address in our list of inbound addresses
+    mail.recipients.each do |recipient|
+      inbound_address = InboundEmailAddress.find_by(email: recipient)
+      if not inbound_address.nil?
+        # Create a case from the template associated with inbound_address
+        return
+      end
+    end
   end
 end
