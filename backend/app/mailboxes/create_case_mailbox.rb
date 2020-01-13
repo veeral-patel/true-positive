25342895d52d5bb7af21719191b880624fb44f5f
Veeral Patel <vpatel98@gmail.com>
2020-01-12 17:25:25 -0800

    actually create a case upon receiving inbound mail

--- backend/app/mailboxes/create_case_mailbox.rb
+++ backend/app/mailboxes/create_case_mailbox.rb
@@ -1,11 +1,13 @@
 class CreateCaseMailbox < ApplicationMailbox
   def process
-    # Look for each recipient email address in our list of create case addresses
+    # For each recipient email address
     mail.recipients.each do |recipient|
-      address = CreateCaseEmailAddress.find_by(email: recipient)
-      if not address.nil?
-        # Create a case from the template associated with address
-        return
+      # If the recipient email is the email of an inbound create case email address...
+      inbound_address = CreateCaseEmailAddress.find_by(email: recipient)
+      if not inbound_address.nil?
+        # Then create a case from the template associated with the inbound address
+        # Note that created_by is set to the creator of the template
+        CaseService::CreateCaseFromTemplate.run(template: inbound_address.template, created_by: inbound_address.case_template.created_by)
       end
     end
   end
