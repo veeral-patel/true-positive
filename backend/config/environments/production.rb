e1e38d68f0e24e7c31a62b17fd84a8cf6772b3e4
Veeral Patel <vpatel98@gmail.com>
2020-01-12 19:02:10 -0800

    set ingress to sendgrid

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Prepare the ingress controller used to receive mail
-  # config.action_mailbox.ingress = :relay
+  config.action_mailbox.ingress = :sendgrid
 
   # Settings specified here will take precedence over those in config/application.rb.
 
