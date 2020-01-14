52590e7374da5936762a50b4e6eb323a22cad89c
Veeral Patel <vpatel98@gmail.com>
2020-01-13 21:13:14 -0800

    mention time is in UTC

--- backend/app/mailboxes/create_case_mailbox.rb
+++ backend/app/mailboxes/create_case_mailbox.rb
@@ -15,7 +15,7 @@ class CreateCaseMailbox < ApplicationMailbox
         # Add a comment to the case indicating it was created via email
         new_case.comments.create(
           created_by: inbound_address.case_template.created_by,
-          comment: "This case was created from an inbound email received by #{inbound_address.email} at #{Time.now.strftime("%m/%d/%Y %I:%M %p")}."\
+          comment: "This case was created from an inbound email received by #{inbound_address.email} at #{Time.now.strftime("%m/%d/%Y %I:%M %p")} UTC."\
                    "\n\n"\
                    "(True Positive added this comment automatically when the case was created.)"
         )
