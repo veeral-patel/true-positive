b087f98df6a5438dbbc4b7afba8c6a0ea711fddf
Veeral Patel <vpatel98@gmail.com>
2020-01-14 20:30:25 -0800

    remove comment

--- backend/app/mailboxes/application_mailbox.rb
+++ backend/app/mailboxes/application_mailbox.rb
@@ -1,5 +1,3 @@
 class ApplicationMailbox < ActionMailbox::Base
   routing /inbound-cases.truepositive.app$/ => :create_case
 end
-
-# CreateCaseEmailAddress.create(email: "l3amar23@inbound-cases.truepositive.app", case_template: CaseTemplate.first)
