93105b503fea476c7f688ca0868cb2d1aa8dc7f4
Veeral Patel <vpatel98@gmail.com>
2020-01-14 22:38:10 -0800

    block requests from disabled users

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
     include Pundit
 
     before_action :authenticate_user
+    before_action :handle_disabled
 
     def authenticate_user
         token_from_request = request.env['HTTP_AUTHORIZATION']&.split&.last
@@ -12,8 +13,8 @@ class ApplicationController < ActionController::API
         if token_from_request.nil?
             render json: { "message": "Please provide an API token in your request's Authorization header." }, status: 401
         else
-            # Hack: If the provided token is 40 characters, then assume
-            # the user is authenticating via the API, the UI otherwise.
+            # Hack: If the provided token is 40 characters, then assume the user is
+            # authenticating via the API, the UI otherwise.
             if token_from_request.size == 40
                 authenticate_user_api(token_from_request)
             else
@@ -22,9 +23,23 @@ class ApplicationController < ActionController::API
         end
     end
 
+    def handle_disabled
+        if @current_user.disabled
+            render json: {
+                "errors": [
+                    {
+                        "message": "Your user account has been disabled.",
+                    }
+                ]
+            }
+            return
+        end
+    end
+
     def authenticate_user_ui
         # Authenticate and identify the user if they're logging in through the UI
         # (aka with a JWT).
+
         begin
             self.authenticate!
 
