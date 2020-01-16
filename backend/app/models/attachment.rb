d6f24e6d5ea02ade10473460b73b0bee6d55aaaa
Veeral Patel <vpatel98@gmail.com>
2020-01-15 18:29:41 -0800

    write Attachment model

--- backend/app/models/attachment.rb
+++ backend/app/models/attachment.rb
@@ -1,4 +1,11 @@
 class Attachment < ApplicationRecord
-  belongs_to :created_by
-  belongs_to :tenant
+  acts_as_tenant :tenant
+
+  belongs_to :attachable, polymorphic: true
+  belongs_to :created_by, :class_name => 'User', optional: false
+  
+  validates :created_by, presence: true
+  validates :attachable, presence: true
+
+  has_one_attachment :file
 end
