d77fce1408ea469fbd1535aa37fbe724fd246580
Veeral Patel <vpatel98@gmail.com>
2020-01-16 22:54:20 -0800

    remove foreign_key argument

--- backend/app/models/case_template.rb
+++ backend/app/models/case_template.rb
@@ -11,8 +11,8 @@ class CaseTemplate < ApplicationRecord
   belongs_to :status
   belongs_to :priority
 
-  has_many :default_members, foreign_key: "caseable_id", class_name: "CaseMember", as: :caseable, dependent: :destroy
-  has_many :default_groups, foreign_key: "caseable_id", class_name: "CaseGroup", as: :caseable, dependent: :destroy
+  has_many :default_members, class_name: "CaseMember", as: :caseable, dependent: :destroy
+  has_many :default_groups, class_name: "CaseGroup", as: :caseable, dependent: :destroy
   has_many :task_groups, as: :caseable, dependent: :destroy
 
   acts_as_taggable_on :tags
