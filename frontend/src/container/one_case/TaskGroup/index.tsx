00c4f628c6fca3860655c36d665f9a4b0f75f215
Veeral Patel <vpatel98@gmail.com>
2020-01-17 22:06:35 -0800

    remove unneeded component

--- frontend/src/container/one_case/TaskGroup/index.tsx
+++ frontend/src/container/one_case/TaskGroup/index.tsx
@@ -1,13 +1,25 @@
+import { DeleteOutlined } from "@ant-design/icons";
 import { useMutation } from "@apollo/react-hooks";
-import { message, notification } from "antd";
+import {
+  Button,
+  message,
+  Modal,
+  notification,
+  Tooltip,
+  Typography
+} from "antd";
+import { ApolloError } from "apollo-boost";
 import CreateTaskInput from "container/one_case/CreateTaskInput";
 import SortableTaskList from "container/one_case/SortableTaskList";
-import Heading from "container/one_case/TaskGroup/Heading";
 import { inject, observer } from "mobx-react";
 import CREATE_A_TASK from "mutations/createTask";
+import DELETE_A_TASK_GROUP from "mutations/deleteTaskGroup";
+import UPDATE_TASK_GROUP from "mutations/updateTaskGroup";
 import React from "react";
 import ActiveCaseStore from "stores/ActiveCaseStore";
 import ITask from "ts/interfaces/ITask";
+
+const { Text } = Typography;
 
 interface Props {
   name: string;
@@ -40,14 +52,75 @@ function TaskGroup({
     }
   });
 
+  const [updateTaskGroup] = useMutation(UPDATE_TASK_GROUP, {
+    onCompleted: function() {
+      message.success("Updated the task group");
+      if (!forCaseTemplate) activeCaseStore!.loadActiveCase();
+    },
+    onError: function(error: ApolloError) {
+      notification.error({
+        message: "An error occurred while updating the task group",
+        description: error
+      });
+    }
+  });
+
+  const [deleteTaskGroup] = useMutation(DELETE_A_TASK_GROUP, {
+    onCompleted: function() {
+      message.success("Deleted task group");
+      if (!forCaseTemplate) activeCaseStore!.loadActiveCase();
+    },
+    onError: function(error: ApolloError) {
+      notification.error({
+        message: "Could not delete this task group",
+        description: error.message
+      });
+    }
+  });
+
   return (
     <div style={{ marginBottom: "3em" }}>
       <div>
-        <Heading
-          heading={name}
-          id={taskGroupId}
-          forCaseTemplate={forCaseTemplate}
-        />
+        <Text
+          type="secondary"
+          style={{ textTransform: "uppercase" }}
+          editable={{
+            onChange: newName => {
+              updateTaskGroup({
+                variables: {
+                  input: {
+                    id: taskGroupId,
+                    name: newName
+                  }
+                }
+              });
+            }
+          }}
+        >
+          {name}
+        </Text>
+        <Tooltip title="Delete">
+          <Button
+            icon={<DeleteOutlined />}
+            type="link"
+            onClick={() => {
+              Modal.confirm({
+                title: "Delete this task group?",
+                content:
+                  "This will also delete all the tasks in this task group.",
+                onOk: () => {
+                  deleteTaskGroup({
+                    variables: {
+                      input: {
+                        id: taskGroupId
+                      }
+                    }
+                  });
+                }
+              });
+            }}
+          />
+        </Tooltip>
       </div>
       <div style={{ marginTop: "0.5em" }}>
         <CreateTaskInput
