77885430c95216aa1f1d627611925fe5e1b73ee2
Veeral Patel <vpatel98@gmail.com>
2020-01-19 13:53:30 -0800

    can create CC email addresses now

--- backend/app/graphql/mutations/create_create_case_email_address.rb
+++ backend/app/graphql/mutations/create_create_case_email_address.rb
@@ -1,13 +1,18 @@
 class Mutations::CreateCreateCaseEmailAddress < Mutations::BaseMutation
     description "Create an inbound email address used to create cases."
 
+    argument :case_template_id, ID, required: true do
+        description "ID of the case template to use to initialize cases created from emails sent to this address."
+    end
+
     field :create_case_email_address, Types::CreateCaseEmailAddressType, null: true do
         description "The newly created inbound email address."
     end
 
-    def resolve
+    def resolve(case_template_id:)
         # create the address in memory
-        address = CreateCaseEmailAddress.new(email: "#{SecureRandom.alphanumeric(20)}@inbound-cases.truepositive.app")
+        case_template = find_case_template_or_throw_execution_error(id: case_template_id)
+        address = CreateCaseEmailAddress.new(email: "#{SecureRandom.alphanumeric(20)}@inbound-cases.truepositive.app", case_template: case_template)
 
         # authorize this action
         unless CreateCaseEmailAddressPolicy.new(context[:current_user], address).create?
