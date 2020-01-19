2b62d7591145d98140bb8b80b94f863cee8997d5
Veeral Patel <vpatel98@gmail.com>
2020-01-19 15:59:14 -0800

    case should be created by default_creator

--- backend/app/mailboxes/create_case_mailbox.rb
+++ backend/app/mailboxes/create_case_mailbox.rb
@@ -2,19 +2,21 @@ class CreateCaseMailbox < ApplicationMailbox
   def process
     # For each recipient email address
     mail.recipients.each do |recipient|
+
       # If the recipient email is the email of an inbound create case email address...
       inbound_address = CreateCaseEmailAddress.find_by(email: recipient)
+
       if not inbound_address.nil?
         # Set the current tenant based upon the inbound address
         ActsAsTenant.current_tenant = inbound_address.tenant
 
         # Then create a case from the template associated with the inbound address
         # Note that created_by is set to the creator of the template
-        new_case = CaseService::CreateCaseFromTemplate.run(template: inbound_address.case_template, created_by: inbound_address.case_template.created_by)
+        new_case = CaseService::CreateCaseFromTemplate.run(template: inbound_address.case_template, created_by: inbound_address.default_creator)
 
         # Add a comment to the case indicating it was created via email
         new_case.comments.create(
-          created_by: inbound_address.case_template.created_by,
+          created_by: inbound_address.default_creator,
           comment: "This case was created from an inbound email received by #{inbound_address.email} at #{Time.now.strftime("%m/%d/%Y %I:%M %p")} UTC."\
                    "\n\n"\
                    "(True Positive added this comment automatically when the case was created.)"
