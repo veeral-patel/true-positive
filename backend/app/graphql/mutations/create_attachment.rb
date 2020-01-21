1e2a95d4eac2120a50e1e662c9c5de2e011c4add
Veeral Patel <vpatel98@gmail.com>
2020-01-20 18:09:15 -0800

    can attach a attachment to a case now

--- backend/app/graphql/mutations/create_attachment.rb
+++ backend/app/graphql/mutations/create_attachment.rb
@@ -9,21 +9,27 @@ class Mutations::CreateAttachment < Mutations::BaseMutation
         description "The file's filename."
     end
 
+    argument :case_id, ID, required: true do
+        description "ID of the case to add this attachment to."
+    end
+
     # TODO: remove the URL field
     field :url, String, null: true do
         description "A URL to access the newly uploaded file."
     end
 
-    def resolve(blob:, filename:)
+    def resolve(blob:, filename:, case_id:)
+        the_case = find_case_or_throw_execution_error(case_id: case_id)
+
         decoded = Base64.decode64(blob)
 
-        Case.last.attachments.create(file: {
+        at = the_case.attachments.create(file: {
             io: StringIO.new(decoded),
             filename: filename
-        })
+        }, created_by: context[:current_user])
 
         {
-            "url": Attachment.last.url
+            "url": at.url
         }
     end
 end
