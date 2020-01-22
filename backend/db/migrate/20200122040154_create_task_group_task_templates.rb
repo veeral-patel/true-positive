4ba5ead64d0ada825c87936a37176f681bf85e66
Veeral Patel <vpatel98@gmail.com>
2020-01-21 20:03:04 -0800

    rollback again

--- backend/db/migrate/20200122040154_create_task_group_task_templates.rb
+++
@@ -1,8 +0,0 @@
-class CreateTaskGroupTaskTemplates < ActiveRecord::Migration[6.0]
-  def change
-    create_table :task_group_task_templates do |t|
-      t.references :task_group, foreign_key: true
-      t.references :task_template, foreign_key: true
-    end
-  end
-end
