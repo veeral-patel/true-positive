f16efe8f3bf070836200c50a7a8b4a91863bee04
Veeral Patel <vpatel98@gmail.com>
2020-01-21 20:27:12 -0800

    add taskTemplates to TS definition

--- frontend/src/ts/interfaces/ITaskGroup.ts
+++ frontend/src/ts/interfaces/ITaskGroup.ts
@@ -1,9 +1,11 @@
 import ITask from "./ITask";
+import ITaskTemplate from "./ITaskTemplate";
 
 interface ITaskGroup {
   id: number;
   name: string;
   tasks: ITask[];
+  taskTemplates: ITaskTemplate[];
 }
 
 export default ITaskGroup;
