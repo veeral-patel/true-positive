118d9f8e4715ca2e839dc04981b47d78b62bb168
Veeral Patel <vpatel98@gmail.com>
2020-01-22 18:45:52 -0800

    load data in TaskTemplateSelect

--- frontend/src/container/admin/TaskTemplateSelect.tsx
+++ frontend/src/container/admin/TaskTemplateSelect.tsx
@@ -1,18 +1,52 @@
+import { useQuery } from "@apollo/react-hooks";
 import { Select } from "antd";
+import GET_TASK_TEMPLATES from "queries/getTaskTemplates";
 import React from "react";
+import ITaskTemplate from "ts/interfaces/ITaskTemplate";
+
+const { Option } = Select;
+
+interface TaskTemplateData {
+  taskTemplates: ITaskTemplate[];
+}
 
 interface Props {
   placeholder: string;
 }
 
 function TaskTemplateSelect({ placeholder }: Props) {
+  const { loading, data } = useQuery<TaskTemplateData>(GET_TASK_TEMPLATES);
+
+  var options: Object[] = [];
+  if (loading) {
+    options = [
+      <Option key="loading" value="loading">
+        Loading...
+      </Option>
+    ];
+  } else if (data) {
+    options = data.taskTemplates.map(taskTemplate => (
+      <Option key={taskTemplate.id} value={taskTemplate.id}>
+        {taskTemplate.name}
+      </Option>
+    ));
+  } else {
+    options = [
+      <Option key="error" value="error">
+        Error loading task templates
+      </Option>
+    ];
+  }
+
   return (
     <Select
       placeholder={placeholder}
       mode="multiple"
-      options={[]}
       style={{ width: "100%" }}
-    />
+      optionFilterProp="children"
+    >
+      {options}
+    </Select>
   );
 }
 
