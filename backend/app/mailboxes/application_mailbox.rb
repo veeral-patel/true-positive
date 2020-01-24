9d73f13f0515ffefeb0c938faded8c90f0de44b7
Veeral Patel <vpatel98@gmail.com>
2020-01-24 00:01:38 -0800

    route forward emails too

--- backend/app/mailboxes/application_mailbox.rb
+++ backend/app/mailboxes/application_mailbox.rb
@@ -1,3 +1,15 @@
 class ApplicationMailbox < ActionMailbox::Base
   routing /inbound-cases.truepositive.app$/ => :create_case
+  routing ->(inbound_email) { self.forwarded_to_an_inbound_address(inbound_email) } => :create_case
+
+  private
+    # Returns true if the inbound email was received because it was forwarded to an inbound
+    # address (using Gmail, at least)
+    def self.forwarded_to_an_inbound_address(inbound_email)
+      if inbound_email.mail['X-Forwarded-To'].nil?
+        false
+      else
+        inbound_email.mail['X-Forwarded-To'].value) =~ /inbound-cases.truepositive.app$/
+      end
+    end
 end
