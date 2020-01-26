479bbd444279a5e32221841dc55c560cd85818e4
Veeral Patel <vpatel98@gmail.com>
2020-01-25 20:40:30 -0800

    get ChangeTaskTemplatePosition mutation working

--- backend/app/graphql/mutations/change_task_template_position.rb
+++ backend/app/graphql/mutations/change_task_template_position.rb
@@ -1,8 +1,12 @@
 class Mutations::ChangeTaskTemplatePosition < Mutations::BaseMutation
     description "Updates the position of a task template in a case template."
 
-    argument :id, ID, required: true do
+    argument :task_template_id, ID, required: true do
         description "ID of the task template to move."
+    end
+
+    argument :task_group_id, ID, required: true do
+        description "ID of the group this task template is in."
     end
 
     argument :position, Int, required: true do
@@ -13,17 +17,26 @@ class Mutations::ChangeTaskTemplatePosition < Mutations::BaseMutation
         description "The updated task template."
     end
 
-    def resolve(id:, position:)
+    def resolve(task_template_id:, task_group_id:, position:)
         # find the task template
-        task_template = find_task_template_or_throw_execution_error(id: id)
+        task_template = find_task_template_or_throw_execution_error(id: task_template_id)
+        task_group = find_task_group_or_throw_execution_error(id: task_group_id)
 
         # authorize this action
         unless TaskTemplatePolicy.new(context[:current_user], task_template).update_template?
             raise GraphQL::ExecutionError, "You are not authorized to update this template."
         end
 
-        # and update its position
-        if task_template.insert_at(position)
+        # find the TaskGroupTaskTemplate with the provided TT and TG. and raise an error
+        # if no such record exists
+        begin
+            task_group_task_template = TaskGroupTaskTemplate.find_by!(task_group: task_group, task_template: task_template)
+        rescue ActiveRecord::RecordNotFound
+            raise GraphQL::ExecutionError, "The task group you specified doesn't have a task template with name #{task_template.name}"
+        end
+
+        # update its position
+        if task_group_task_template.insert_at(position)
             { task_template: task_template }
         else
             raise GraphQL::ExecutionError, task_template.errors.full_messages.join(" | ")
