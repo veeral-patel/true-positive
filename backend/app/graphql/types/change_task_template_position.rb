ef11e5c6fd0915e0331e1dd66c251708d88c3f10
Veeral Patel <vpatel98@gmail.com>
2020-01-25 17:48:22 -0800

    finish ChangeTaskTemplatePosition mutation

--- backend/app/graphql/types/change_task_template_position.rb
+++ backend/app/graphql/types/change_task_template_position.rb
@@ -15,9 +15,18 @@ class Mutations::ChangeTaskTemplatePosition < Mutations::BaseMutation
 
     def resolve(id:, position:)
         # find the task template
+        task_template = find_task_template_or_throw_execution_error(id: id)
 
         # authorize this action
+        unless TaskTemplatePolicy.new(context[:current_user], task_template).update_template?
+            raise GraphQL::ExecutionError, "You are not authorized to update this template."
+        end
 
         # and update its position
+        if task_template.insert_at(position)
+            { task_template: task_template }
+        else
+            raise GraphQL::ExecutionError, task_template.errors.full_messages.join(" | ")
+        end
     end
 end
