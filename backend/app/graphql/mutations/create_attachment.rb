c579ff2b2d97a83a681b2926bdca86c2baa85a34
Veeral Patel <vpatel98@gmail.com>
2020-01-27 20:56:05 -0800

    can add attachments to cases again

--- backend/app/graphql/mutations/create_attachment.rb
+++ backend/app/graphql/mutations/create_attachment.rb
@@ -1,6 +1,8 @@
 class Mutations::CreateAttachment < Mutations::BaseMutation
     description "Attach a file to a case, task, indicator, or another object."
 
+    # required ---
+
     argument :blob, String, required: true do
         description "The file, base64-encoded."
     end
@@ -9,37 +11,59 @@ class Mutations::CreateAttachment < Mutations::BaseMutation
         description "The file's filename."
     end
 
-    argument :case_id, ID, required: true do
+    # not required ---
+
+    # exactly one of the arguments below is required
+    argument :case_id, ID, required: false do
         description "ID of the case to add this attachment to."
     end
+
+    argument :task_id, ID, required: false do
+        description "ID of the task to add this attachment to."
+    end
+
+    # output ---
 
     field :attachment, Types::AttachmentType, null: true do
         description "The newly created attachment."
     end
 
-    def resolve(blob:, filename:, case_id:)
-        # find the case to add this attachment to
-        the_case = find_case_or_throw_execution_error(case_id: case_id)
+    # code ---
 
-        # authorize this action
-        # TODO: when I allow file uploads to tasks and indicators, this call needs to change
-        unless CasePolicy.new(context[:current_user], the_case).update_case?
-            raise GraphQL::ExecutionError, "You are not authorized to add an attachment to this case."
+    
+    def resolve(blob:, filename:, case_id: nil, task_id: nil)
+        # ensure exactly one of case_id and task_id is provided
+        if not [case_id, task_id].one? { |id| not id.nil? } 
+            raise GraphQL::ExecutionError, "Please provide a caseId or a taskId argument, but not both."
         end
 
-        # create a new attachment in memory
-        new_attachment = the_case.attachments.new(
-            file: { io: StringIO.new(Base64.decode64(blob)), filename: filename },
-            created_by: context[:current_user]
-        )
-
-        # and save it to the database
-        if new_attachment.save
-            {
-                "attachment": new_attachment
-            }
-        else
-            raise GraphQL::ExecutionError, attachment.errors.full_messages.join(" | ")
+        # if case_id is provided, attach the file to the corresponding case
+        if not case_id.nil?
+            attach_file_to_a_case(blob: blob, filename: filename, case_id: case_id)
         end
     end
+
+    private
+        def attach_file_to_a_case(blob:, filename:, case_id:)
+            # find the case to add this attachment to
+            the_case = find_case_or_throw_execution_error(case_id: case_id)
+
+            # authorize this action
+            unless CasePolicy.new(context[:current_user], the_case).update_case?
+                raise GraphQL::ExecutionError, "You are not authorized to add an attachment to this case."
+            end
+
+            # create a new attachment in memory
+            new_attachment = the_case.attachments.new(
+                file: { io: StringIO.new(Base64.decode64(blob)), filename: filename },
+                created_by: context[:current_user]
+            )
+
+            # and save it to the database
+            if new_attachment.save
+                { attachment: new_attachment }
+            else
+                raise GraphQL::ExecutionError, attachment.errors.full_messages.join(" | ")
+            end
+        end
 end
