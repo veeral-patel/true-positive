ff6b33f540f06338a56ce20d1a68891ec7e42f9f
Veeral Patel <vpatel98@gmail.com>
2020-01-28 21:39:01 -0800

    refactor top menu to SFC

--- frontend/src/container/shared/top_menu/TopMenu.tsx
+++ frontend/src/container/shared/top_menu/TopMenu.tsx
@@ -14,97 +14,87 @@ import AuthStore from "stores/AuthStore";
 import { paths } from "utils/constants";
 import getUsernameOfCurrentUser from "utils/currentUser";
 import { getApiEndpoint } from "utils/getApiEndpoint";
+import GlobalAutocomplete from "./GlobalAutocomplete";
 
 const { SubMenu } = Menu;
 
-interface TopMenuProps {
+interface Props {
   authStore?: AuthStore;
 }
 
-export default inject(
-  "uiStore",
-  "authStore"
-)(
-  observer(
-    class TopMenu extends React.Component<TopMenuProps> {
-      render() {
-        const { authStore } = this.props;
-        const usernameOfCurrentUser = getUsernameOfCurrentUser();
+function TopMenu({ authStore }: Props) {
+  const usernameOfCurrentUser = getUsernameOfCurrentUser();
 
-        return (
-          <Menu theme="dark" mode="horizontal" selectedKeys={[]}>
-            <Menu.Item
-              style={{ float: "left" }}
-              onClick={() => navigate(paths.CASES_PATH)}
-            >
-              <img
-                src={logo}
-                style={{ verticalAlign: "middle", height: "20px" }}
-              />
-            </Menu.Item>
-            <Menu.Item
-              onClick={() => navigate(paths.CASES_PATH)}
-              style={{ float: "left" }}
-            >
-              Cases
-            </Menu.Item>
-            <Menu.Item
-              style={{ float: "left" }}
-              onClick={() => navigate(paths.TASKS_PATH)}
-            >
-              Tasks
-            </Menu.Item>
-            <Menu.Item
-              onClick={() => navigate(paths.MANAGE_PATH)}
-              style={{ float: "left" }}
-            >
-              Manage
-            </Menu.Item>
-            <SubMenu
-              title={
-                <div>
-                  API
-                  {"  "}
-                  <DownOutlined />
-                </div>
-              }
-              style={{ float: "left" }}
-            >
-              <Menu.Item onClick={() => navigate(paths.API_TOKENS_PATH)}>
-                <KeyOutlined />
-                Tokens
-              </Menu.Item>
-              <Menu.Item onClick={() => navigate(getApiEndpoint())}>
-                <DeploymentUnitOutlined />
-                Playground
-              </Menu.Item>
-            </SubMenu>
-            {/* <Menu.Item style={{ float: "left" }}>
-              <GlobalAutocomplete />
-            </Menu.Item> */}
-            <SubMenu
-              title={
-                <div>
-                  {usernameOfCurrentUser}
-                  {"  "}
-                  <DownOutlined />
-                </div>
-              }
-              style={{ float: "right" }}
-            >
-              <Menu.Item onClick={() => navigate(paths.PROFILE_PATH)}>
-                <UserOutlined />
-                Profile
-              </Menu.Item>
-              <Menu.Divider />
-              <Menu.Item onClick={() => authStore!.logout()}>
-                <LogoutOutlined />
-                Log Out
-              </Menu.Item>
-            </SubMenu>
-          </Menu>
-        );
-      }
-    }
-  )
-);
+  return (
+    <Menu theme="dark" mode="horizontal" selectedKeys={[]}>
+      <Menu.Item
+        style={{ float: "left" }}
+        onClick={() => navigate(paths.CASES_PATH)}
+      >
+        <img src={logo} style={{ verticalAlign: "middle", height: "20px" }} />
+      </Menu.Item>
+      <Menu.Item
+        onClick={() => navigate(paths.CASES_PATH)}
+        style={{ float: "left" }}
+      >
+        Cases
+      </Menu.Item>
+      <Menu.Item
+        style={{ float: "left" }}
+        onClick={() => navigate(paths.TASKS_PATH)}
+      >
+        Tasks
+      </Menu.Item>
+      <Menu.Item
+        onClick={() => navigate(paths.MANAGE_PATH)}
+        style={{ float: "left" }}
+      >
+        Manage
+      </Menu.Item>
+      <SubMenu
+        title={
+          <div>
+            API
+            {"  "}
+            <DownOutlined />
+          </div>
+        }
+        style={{ float: "left" }}
+      >
+        <Menu.Item onClick={() => navigate(paths.API_TOKENS_PATH)}>
+          <KeyOutlined />
+          Tokens
+        </Menu.Item>
+        <Menu.Item onClick={() => navigate(getApiEndpoint())}>
+          <DeploymentUnitOutlined />
+          Playground
+        </Menu.Item>
+      </SubMenu>
+      <Menu.Item style={{ float: "left" }}>
+        <GlobalAutocomplete />
+      </Menu.Item>
+      <SubMenu
+        title={
+          <div>
+            {usernameOfCurrentUser}
+            {"  "}
+            <DownOutlined />
+          </div>
+        }
+        style={{ float: "right" }}
+      >
+        <Menu.Item onClick={() => navigate(paths.PROFILE_PATH)}>
+          <UserOutlined />
+          Profile
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item onClick={() => authStore!.logout()}>
+          <LogoutOutlined />
+          Log Out
+        </Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+}
+
+export default inject("uiStore", "authStore")(observer(TopMenu));
