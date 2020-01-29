e6b741bd0224a9ee7d8ac7757f4ecbbdc6d8803e
Veeral Patel <vpatel98@gmail.com>
2020-01-28 23:24:08 -0800

    clean up code

--- backend/app/graphql/mutations/change_role_in_case.rb
+++ backend/app/graphql/mutations/change_role_in_case.rb
@@ -38,29 +38,29 @@ class Mutations::ChangeRoleInCase  < Mutations::BaseMutation
             raise GraphQL::ExecutionError, "You are not authorized to change a case member's role."
         end
 
+        # find the case member to update
         begin
-            # find the case member to update
             member = the_case.case_members.find_by!(user: user)
-
-            # if this is the last CAN_EDIT member in the case, you cannot make him CAN_VIEW
-            members_who_can_edit = the_case.case_members.where(role: "CAN_EDIT")
-            if members_who_can_edit.count == 1  && member.role == "CAN_EDIT"  && role == "CAN_VIEW"
-                raise GraphQL::ExecutionError, "You can't make a case's last 'Can Edit' member 'Can View'."
-            end
-
-            # update the case member in memory
-            member.role = role
-
-            # and save to the database
-            if member.save
-                {
-                    "case": the_case
-                }
-            else
-                raise GraphQL::ExecutionError, the_case.errors.full_messages.join(" | ")
-            end
         rescue ActiveRecord::RecordNotFound
             raise GraphQL::ExecutionError, "#{username} is not a member of the specified case."
+        end
+
+        # if this is the last CAN_EDIT member in the case, you cannot make him CAN_VIEW
+        number_of_editors = the_case.case_members.where(role: "CAN_EDIT").count
+        if number_of_editors == 1 && member.role == "CAN_EDIT"  && role == "CAN_VIEW"
+            raise GraphQL::ExecutionError, "Each case must have at least one user with edit access."
+        end
+
+        # update the case member in memory
+        member.role = role
+
+        # and save to the database
+        if member.save
+            {
+                "case": the_case
+            }
+        else
+            raise GraphQL::ExecutionError, the_case.errors.full_messages.join(" | ")
         end
     end
 end
